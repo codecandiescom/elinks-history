1b887d3359dddef8b06e832595a1fe779f7c8c24
Petr Baudis <pasky@ucw.cz>
2002-01-05 21:39:41 +0000

    first stage of get_home() cleanup - needs to be done w/o so much gotos probably

--- default.c
+++ default.c
@@ -250,37 +250,50 @@ int write_to_config_file(unsigned char *name, unsigned char *c)
 	return 0;
 }
 
-unsigned char *get_home(int *n)
+unsigned char *get_home(int *new)
 {
 	struct stat st;
 	unsigned char *home = stracpy(getenv("HOME"));
 	unsigned char *home_links;
 	unsigned char *config_dir = stracpy(getenv("CONFIG_DIR"));
 
-	if (n) *n = 1;
+	if (new) *new = 1;
+	
 	if (!home) {
 		int i;
+		
 		home = stracpy(path_to_exe);
 		if (!home) {
 			if (config_dir) mem_free(config_dir);
 			return NULL;
 		}
-		for (i = strlen(home) - 1; i >= 0; i--) if (dir_sep(home[i])) {
-			home[i + 1] = 0;
-			goto br;
+		
+		for (i = strlen(home) - 1; i >= 0; i--) {
+			if (dir_sep(home[i])) {
+				home[i + 1] = 0;
+				break;
+			}
 		}
-		home[0] = 0;
-		br:;
+		
+		if (i < 0) home[0] = 0;
 	}
-	while (home[0] && dir_sep(home[strlen(home) - 1])) home[strlen(home) - 1] = 0;
+	
+	while (home[0] && dir_sep(home[strlen(home) - 1]))
+		home[strlen(home) - 1] = 0;
+	
 	if (home[0]) add_to_strn(&home, "/");
+	
 	home_links = stracpy(home);
-	if (config_dir) 	
-	{
+	
+	if (config_dir) {
 		add_to_strn(&home_links, config_dir);
-		while (home_links[0] && dir_sep(home_links[strlen(home_links) - 1])) home_links[strlen(home_links) - 1] = 0;
+		
+		while (home_links[0] && dir_sep(home_links[strlen(home_links) - 1]))
+			home_links[strlen(home_links) - 1] = 0;
+		
 		if (stat(home_links, &st) != -1 && S_ISDIR(st.st_mode)) {
 			add_to_strn(&home_links, "/links");
+			
 	    	} else {
 			fprintf(stderr, "CONFIG_DIR set to %s. But directory %s doesn't exist.\n\007", config_dir, home_links);
 			sleep(3);
@@ -288,36 +301,55 @@ unsigned char *get_home(int *n)
 			home_links = stracpy(home);
 			add_to_strn(&home_links, ".links");		
 		}
+		
 		mem_free(config_dir);
-	} else add_to_strn(&home_links, ".links");
+		
+	} else {
+		add_to_strn(&home_links, ".links");
+	}
+	
 	if (stat(home_links, &st)) {
-		if (!mkdir(home_links, 0777)) goto home_creat;
-		if (config_dir) goto failed;
+		if (!mkdir(home_links, 0777))
+			goto home_creat;
+		if (config_dir)
+			goto failed;
 		goto first_failed;
 	}
-	if (S_ISDIR(st.st_mode)) goto home_ok;
-	first_failed:
+	
+	if (S_ISDIR(st.st_mode))
+		goto home_ok;
+	
+first_failed:
 	mem_free(home_links);
+	
 	home_links = stracpy(home);
 	add_to_strn(&home_links, "links");
+	
 	if (stat(home_links, &st)) {
-		if (!mkdir(home_links, 0777)) goto home_creat;
+		if (mkdir(home_links, 0777) == 0)
+			goto home_creat;
 		goto failed;
 	}
-	if (S_ISDIR(st.st_mode)) goto home_ok;
-	failed:
+	
+	if (S_ISDIR(st.st_mode))
+		goto home_ok;
+	
+failed:
 	mem_free(home_links);
 	mem_free(home);
+	
 	return NULL;
 
-	home_ok:
-	if (n) *n = 0;
-	home_creat:
+home_ok:
+	if (new) *new = 0;
+	
+home_creat:
 #ifdef HAVE_CHMOD
 	chmod(home_links, 0700);
 #endif
 	add_to_strn(&home_links, "/");
 	mem_free(home);
+	
 	return home_links;
 }
 

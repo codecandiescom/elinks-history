bfd9aea1fd5fa51e6e35c7553226f957ddbb5318
Petr Baudis <pasky@ucw.cz>
2002-01-17 19:14:37 +0000

    more sane passing of parameters ;)

--- autocommit.sh
+++ autocommit.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cvs ci -m "$1" -f configure.in acconfig.h aclocal.m4 config.h.in Makefile.in configure stamp-h.in
+cvs ci $1 -f configure.in acconfig.h aclocal.m4 config.h.in Makefile.in configure stamp-h.in

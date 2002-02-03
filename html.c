1c12ddfc01dadc0cf276bd4d23f65e980196b7e5
Petr Baudis <pasky@ucw.cz>
2002-02-03 07:50:40 +0000

    forgot one line in querystring patch

--- html.c
+++ html.c
@@ -1039,6 +1039,7 @@ void get_html_form(unsigned char *a, struct form *form)
 		form->action = join_urls(format.href_base, all);
 		mem_free(al);
 	} else {
+		if ((ch = strchr(form->action = stracpy(format.href_base), POST_CHAR))) *ch = 0;
 		if ((ch = strchr(form->action, '?'))) *ch = 0;
 	}
 	if ((al = get_target(a))) {

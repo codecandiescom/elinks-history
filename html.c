cf04124ab1ce6a4a3e33e33cff3f28124c2be976
Petr Baudis <pasky@ucw.cz>
2002-03-10 19:58:00 +0000

    reformat bit

--- html.c
+++ html.c
@@ -1037,7 +1037,8 @@ void get_html_form(unsigned char *a, struct form *form)
 		form->action = join_urls(format.href_base, all);
 		mem_free(al);
 	} else {
-		if ((ch = strchr(form->action = stracpy(format.href_base), POST_CHAR))) *ch = 0;
+		form->action = stracpy(format.href_base);
+		if ((ch = strchr(form->action, POST_CHAR))) *ch = 0;
 		if ((ch = strchr(form->action, '?'))) *ch = 0;
 	}
 	if ((al = get_target(a))) {

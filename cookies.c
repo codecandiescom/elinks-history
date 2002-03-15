7b774c92256a9d78027432382de10402a9803593
Petr Baudis <pasky@ucw.cz>
2002-03-15 19:08:42 +0000

    fix for broken sites - parse correctly cookie values containing '=' - patch by Stephane Chazelas <stephane.chazelas@free.fr>

--- cookies.c
+++ cookies.c
@@ -136,8 +136,12 @@ int set_cookie(struct terminal *term, unsigned char *url, unsigned char *str)
 
 		if (*pos == '=') {
 			/* End of name reached */
-			if (!nam_end) nam_end = pos;
-			last_was_eq = 1;
+			if (!nam_end) {
+				nam_end = pos;
+				/* This inside the if is protection against
+				 * broken sites sending '=' inside values. */
+				last_was_eq = 1;
+			}
 			
 		} else if (WHITECHAR(*pos)) {
 			if (!nam_end) {
@@ -152,12 +156,10 @@ int set_cookie(struct terminal *term, unsigned char *url, unsigned char *str)
 			last_was_eq = 0;
 			last_was_ws = 0;
 			
-		} else {
-			if (last_was_ws) {
-				/* Non-whitespace after whitespace and not just
-				 * after '=' - error */
-				return 0;
-			}
+		} else if (last_was_ws) {
+			/* Non-whitespace after whitespace and not just after
+			 * '=' - error */
+			return 0;
 		}
 	}
 	

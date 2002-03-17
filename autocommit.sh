87f916e8c5c57af41411312c148512e04e1cc1df
Petr Baudis <pasky@ucw.cz>
2002-03-17 11:54:41 +0000

    Part III. of source layout change, stage 2. Improved autofiles, so that ./autogen actually works now ;)

--- autocommit.sh
+++ autocommit.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cvs ci $* -f configure.in acconfig.h aclocal.m4 config.h.in Makefile.in configure stamp-h.in
+cvs ci $* -f configure.in acconfig.h aclocal.m4 config.h.in Makefile.in src/Makefile.in src/*/Makefile.in src/*/*/Makefile.in configure stamp-h.in

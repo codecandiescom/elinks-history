b7f8328e6a57d99a43d16e0d2592085f19cefda1
Petr Baudis <pasky@ucw.cz>
2002-03-30 21:52:28 +0000

    Cleanup of main.c, patch by Zas and me.

--- src/main/main.c
+++ src/main/main.c
@@ -1,5 +1,5 @@
 /* The main program - startup */
-/* $Id: main.c,v 1.10 2002/03/29 10:36:56 pasky Exp $ */
+/* $Id: main.c,v 1.11 2002/03/30 21:52:28 pasky Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -51,14 +51,17 @@ enum retval retval = RET_OK;
 
 void unhandle_basic_signals(struct terminal *);
 
-void sig_terminate(struct terminal *t)
+
+void
+sig_terminate(struct terminal *t)
 {
 	unhandle_basic_signals(t);
 	terminate = 1;
 	retval = RET_SIGNAL;
 }
 
-void sig_intr(struct terminal *t)
+void
+sig_intr(struct terminal *t)
 {
 	if (!t) {
 		unhandle_basic_signals(t);
@@ -69,19 +72,23 @@ void sig_intr(struct terminal *t)
 	}
 }
 
-void sig_ctrl_c(struct terminal *t)
+void
+sig_ctrl_c(struct terminal *t)
 {
 	if (!is_blocked()) kbd_ctrl_c();
 }
 
-void sig_ign(void *x)
+void
+sig_ign(void *x)
 {
 }
 
-void sig_tstp(struct terminal *t)
+void
+sig_tstp(struct terminal *t)
 {
 #ifdef SIGSTOP
 	int pid = getpid();
+
 	block_itrm(0);
 #if defined (SIGCONT) && defined(SIGTTOU)
 	if (!fork()) {
@@ -94,13 +101,20 @@ void sig_tstp(struct terminal *t)
 #endif
 }
 
-void sig_cont(struct terminal *t)
+void
+sig_cont(struct terminal *t)
 {
-	if (!unblock_itrm(0)) /*redraw_terminal_cls(t)*/resize_terminal();
-	/*else register_bottom_half(raise, SIGSTOP);*/
+	if (!unblock_itrm(0)) {
+		/* redraw_terminal_cls(t); */
+		resize_terminal();
+	} /* else {
+		register_bottom_half(raise, SIGSTOP);
+	} */
 }
 
-void handle_basic_signals(struct terminal *term)
+
+void
+handle_basic_signals(struct terminal *term)
 {
 	install_signal_handler(SIGHUP, (void (*)(void *))sig_intr, term, 0);
 	install_signal_handler(SIGINT, (void (*)(void *))sig_ctrl_c, term, 0);
@@ -119,7 +133,8 @@ void handle_basic_signals(struct terminal *term)
 #endif
 }
 
-/*void handle_slave_signals(struct terminal *term)
+#if 0
+void handle_slave_signals(struct terminal *term)
 {
 	install_signal_handler(SIGHUP, (void (*)(void *))sig_terminate, term, 0);
 	install_signal_handler(SIGINT, (void (*)(void *))sig_terminate, term, 0);
@@ -136,9 +151,12 @@ void handle_basic_signals(struct terminal *term)
 #ifdef SIGCONT
 	install_signal_handler(SIGCONT, (void (*)(void *))sig_cont, term, 0);
 #endif
-}*/
+}
+#endif
 
-void unhandle_terminal_signals(struct terminal *term)
+
+void
+unhandle_terminal_signals(struct terminal *term)
 {
 	install_signal_handler(SIGHUP, NULL, NULL, 0);
 	install_signal_handler(SIGINT, NULL, NULL, 0);
@@ -156,7 +174,8 @@ void unhandle_terminal_signals(struct terminal *term)
 #endif
 }
 
-void unhandle_basic_signals(struct terminal *term)
+void
+unhandle_basic_signals(struct terminal *term)
 {
 	install_signal_handler(SIGHUP, NULL, NULL, 0);
 	install_signal_handler(SIGINT, NULL, NULL, 0);
@@ -175,54 +194,97 @@ void unhandle_basic_signals(struct terminal *term)
 #endif
 }
 
-int attach_terminal(int in, int out, int ctl, void *info, int len)
+
+int
+attach_terminal(int in, int out, int ctl, void *info, int len)
 {
 	struct terminal *term;
 	int fd[2];
+	
 	if (c_pipe(fd)) {
 		mem_free(info);
 		error("ERROR: can't create pipe for internal communication");
 		return -1;
 	}
+
 	fcntl(fd[0], F_SETFL, O_NONBLOCK);
 	fcntl(fd[1], F_SETFL, O_NONBLOCK);
 	handle_trm(in, out, out, fd[1], ctl, info, len);
 	mem_free(info);
-	if ((term = init_term(fd[0], out, win_func))) {
-		handle_basic_signals(term);	/* OK, this is race condition, but it must be so; GPM installs it's own buggy TSTP handler */
+
+	term = init_term(fd[0], out, win_func);
+	if (term) {
+		/* OK, this is race condition, but it must be so; GPM installs
+		 * it's own buggy TSTP handler. */
+		handle_basic_signals(term);
+
 		return fd[1];
 	}
+
 	close(fd[0]);
 	close(fd[1]);
+
 	return -1;
 }
 
+
 int ac;
 unsigned char **av;
-
 unsigned char *path_to_exe;
-
 int init_b = 0;
 
-void init()
+
+void
+init()
 {
 	int uh;
 	void *info;
 	int len;
 	unsigned char *u;
+	
 	init_trans();
 	set_sigcld();
 	init_home();
 	init_keymaps();
-	if (!(u = parse_options(ac - 1, av + 1))) goto ttt;
-	if (!no_connect && (uh = bind_to_af_unix()) != -1) {
-		if (!(info = create_session_info(base_session, u, &len))) goto ttt;
-		handle_trm(get_input_handle(), get_output_handle(), uh, uh, get_ctl_handle(), info, len);
-		handle_basic_signals(NULL);	/* OK, this is race condition, but it must be so; GPM installs it's own buggy TSTP handler */
-		mem_free(info);
+
+	/* Parsing command line options */
+	u = parse_options(ac - 1, av + 1);
+	if (!u) {
+		retval = RET_SYNTAX;
+		terminate = 1;
 		return;
 	}
-	if ((dds.assume_cp = get_cp_index("ISO-8859-1")) == -1) dds.assume_cp = 0;
+	
+	/* If there's no -no-connect option, check if there's no other ELinks
+	 * running. If we found any, open socket and act as a slave for it. */
+	if (!no_connect) { 
+		uh = bind_to_af_unix();
+		if (uh != -1) {
+			info = create_session_info(base_session, u, &len);
+			if (!info) {
+				retval = RET_FATAL;
+				terminate = 1;
+				return;
+			}
+
+			handle_trm(get_input_handle(), get_output_handle(),
+				   uh, uh, get_ctl_handle(), info, len);
+
+			/* OK, this is race condition, but it must be so; GPM
+			 * installs it's own buggy TSTP handler. */
+			handle_basic_signals(NULL);
+			mem_free(info);
+
+			return;
+		}
+	}
+
+	/* Default codepage settings may be overridden by config file or
+	 * command line, so we need to setup them now. XXX: This is a hack,
+	 * as we can't do this while initializing the options array. */
+	dds.assume_cp = get_cp_index("ISO-8859-1");
+	if (dds.assume_cp == -1) dds.assume_cp = 0;
+
 	load_config();
 	init_b = 1;
 	read_bookmarks();
@@ -231,24 +293,41 @@ void init()
 #ifdef HAVE_LUA
     	init_lua();
 #endif
+	/* Parse commandline options again, in order to override any config
+	 * file options. */
 	u = parse_options(ac - 1, av + 1);
 	if (!u) {
-		ttt:
-		terminate = 1;
 		retval = RET_SYNTAX;
+		terminate = 1;
 		return;
 	}
-	if (!dmp) {
-		if (!((info = create_session_info(base_session, u, &len)) && attach_terminal(get_input_handle(), get_output_handle(), get_ctl_handle(), info, len) != -1)) {
+	
+	if (dmp) {
+		dump_start(u);
+		return;
+
+	} else {
+		int attached;
+
+		info = create_session_info(base_session, u, &len);
+		if (!info) goto fatal_error;
+		
+		attached = attach_terminal(get_input_handle(),
+					   get_output_handle(),
+					   get_ctl_handle(), info, len);
+		
+		if (attached == -1) {
+fatal_error:
 			retval = RET_FATAL;
 			terminate = 1;
+			return;
 		}
-	} else {
-		dump_start(u);
 	}
 }
 
-void terminate_all_subsystems()
+
+void
+terminate_all_subsystems()
 {
 	af_unix_close();
 	destroy_all_sessions();
@@ -286,7 +365,9 @@ void terminate_all_subsystems()
 	terminate_osdep();
 }
 
-int main(int argc, char *argv[])
+
+int
+main(int argc, char *argv[])
 {
 	path_to_exe = argv[0];
 	ac = argc;
@@ -301,7 +382,9 @@ int main(int argc, char *argv[])
 	return retval;
 }
 
-void shrink_memory(int u)
+
+void
+shrink_memory(int u)
 {
 	shrink_dns_cache(u);
 	shrink_format_cache(u);

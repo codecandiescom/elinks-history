07e85b0ad27133fc70df3785d8544fe64ea2b603
Laurent Monin <zas@norz.org>
2002-04-29 17:28:01 +0000

    Tidy up. A call to parse_url() was replaced by calls to wrapping functions.

--- src/protocol/mailto.c
+++ src/protocol/mailto.c
@@ -1,5 +1,5 @@
 /* Internal "mailto", "telnet", "tn3270" and misc. protocol implementation */
-/* $Id: mailto.c,v 1.6 2002/03/29 10:36:57 pasky Exp $ */
+/* $Id: mailto.c,v 1.7 2002/04/29 17:28:01 zas Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -17,10 +17,14 @@
 #include <protocol/types.h>
 #include <protocol/url.h>
 
-void prog_func(struct terminal *term, struct list_head *list, unsigned char *param, unsigned char *name)
+void
+prog_func(struct terminal *term, struct list_head *list,
+	  unsigned char *param, unsigned char *name)
 {
-	unsigned char *prog, *cmd;
-	if (!(prog = get_prog(list)) || !*prog) {
+	unsigned char *cmd;
+	unsigned char *prog = get_prog(list);
+
+	if (!prog || !*prog) {
 		msg_box(term, NULL,
 			TEXT(T_NO_PROGRAM), AL_CENTER | AL_EXTD_TEXT,
 			TEXT(T_NO_PROGRAM_SPECIFIED_FOR), " ", name, ".", NULL,
@@ -28,31 +32,44 @@ void prog_func(struct terminal *term, struct list_head *list, unsigned char *par
 			TEXT(T_CANCEL), NULL, B_ENTER | B_ESC);
 		return;
 	}
-	if ((cmd = subst_file(prog, param))) {
+
+	cmd = subst_file(prog, param);
+	if (cmd) {
 		exec_on_terminal(term, cmd, "", 1);
 		mem_free(cmd);
 	}
 }
 
-void mailto_func(struct session *ses, unsigned char *url)
+
+void
+mailto_func(struct session *ses, unsigned char *url)
 {
-	unsigned char *user, *host, *m;
+	unsigned char *user, *host, *param;
 	int f = 1;
-	if (!(user = get_user_name(url))) goto fail;
-	if (!(host = get_host_name(url))) goto fail1;
-	if (!(m = mem_alloc(strlen(user) + strlen(host) + 2))) goto fail2;
+
+	user = get_user_name(url);
+	if (!user) goto fail;
+
+	host = get_host_name(url);
+	if (!host) goto fail1;
+
+	param = straconcat(user, "@", host, NULL);
+	if (!param) goto fail2;
+
+	check_shell_security(&param);
+
 	f = 0;
-	strcpy(m, user);
-	strcat(m, "@");
-	strcat(m, host);
-	check_shell_security(&m);
-	prog_func(ses->term, &mailto_prog, m, TEXT(T_MAIL));
-	mem_free(m);
-	fail2:
+	prog_func(ses->term, &mailto_prog, param, TEXT(T_MAIL));
+
+	mem_free(param);
+
+fail2:
 	mem_free(host);
-	fail1:
+
+fail1:
 	mem_free(user);
-	fail:
+
+fail:
 	if (f) {
 		msg_box(ses->term, NULL,
 			TEXT(T_BAD_URL_SYNTAX), AL_CENTER,
@@ -62,52 +79,53 @@ void mailto_func(struct session *ses, unsigned char *url)
 	}
 }
 
-void tn_func(struct session *ses, unsigned char *url, struct list_head *prog, unsigned char *t1, unsigned char *t2)
+
+void
+tn_func(struct session *ses, unsigned char *url, struct list_head *prog,
+	unsigned char *t1, unsigned char *t2)
 {
-	unsigned char *m;
-	unsigned char *h, *p;
-	int hl, pl;
-	unsigned char *hh, *pp;
-	int f = 1;
+	unsigned char *host, *port, *param;
 
-	/* TODO: We shouldn't use parse_url() but some wrapper. */
+	host = get_host_name(url);
+	if (!host) goto fail;
+	check_shell_security(&host);
 
-	if (parse_url(url, NULL, NULL, NULL, NULL, NULL, &h, &hl, &p, &pl, NULL, NULL, NULL) || !hl) goto fail;
-	if (!(hh = memacpy(h, hl))) goto fail;
-	if (pl && !(pp = memacpy(p, pl))) goto fail1;
-	check_shell_security(&hh);
-	if (pl) check_shell_security(&pp);
-	if (!(m = mem_alloc(strlen(hh) + (pl ? strlen(pp) : 0) + 2))) goto fail2;
-	f = 0;
-	strcpy(m, hh);
-	if (pl) {
-		strcat(m, " ");
-		strcat(m, pp);
-		m[hl + 1 + pl] = 0;
+	port = get_port_str(url);
+
+	if (port) {
+		check_shell_security(&port);
+		param = straconcat(host, " ", port, NULL);
+		mem_free(port);
+	} else {
+		param = stracpy(host);
 	}
-	prog_func(ses->term, prog, m, t1);
-	mem_free(m);
-	fail2:
-	if (pl) mem_free(pp);
-	fail1:
-	mem_free(hh);
-	fail:
-	if (f) {
-		msg_box(ses->term, NULL,
-			TEXT(T_BAD_URL_SYNTAX), AL_CENTER,
-			t2,
-			NULL, 1,
-			TEXT(T_CANCEL), NULL, B_ENTER | B_ESC);
-	}
+
+	mem_free(host);
+
+	if (!param) goto fail;
+
+	prog_func(ses->term, prog, param, t1);
+	mem_free(param);
+
+	return;
+fail:
+	msg_box(ses->term, NULL,
+		TEXT(T_BAD_URL_SYNTAX), AL_CENTER,
+		t2,
+		NULL, 1,
+		TEXT(T_CANCEL), NULL, B_ENTER | B_ESC);
 }
 
 
-void telnet_func(struct session *ses, unsigned char *url)
+void
+telnet_func(struct session *ses, unsigned char *url)
 {
 	tn_func(ses, url, &telnet_prog, TEXT(T_TELNET), TEXT(T_BAD_TELNET_URL));
 }
 
-void tn3270_func(struct session *ses, unsigned char *url)
+
+void
+tn3270_func(struct session *ses, unsigned char *url)
 {
 	tn_func(ses, url, &tn3270_prog, TEXT(T_TN3270), TEXT(T_BAD_TN3270_URL));
 }

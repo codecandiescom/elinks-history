d3a45333d11c6f6304e784301de9ed576abef6b4
Laurent Monin <zas@norz.org>
2002-05-10 09:03:33 +0000

    Tidy up.

--- src/intl/language.c
+++ src/intl/language.c
@@ -1,5 +1,5 @@
 /* Support for multiple languages */
-/* $Id: language.c,v 1.2 2002/05/08 13:55:04 pasky Exp $ */
+/* $Id: language.c,v 1.3 2002/05/10 09:03:33 zas Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -30,87 +30,126 @@ unsigned char **translation_array[N_LANGUAGES][N_CODEPAGES];
 int current_language;
 int current_lang_charset;
 
-void init_trans()
+
+void
+init_trans()
 {
 	int i, j;
+
 	for (i = 0; i < N_LANGUAGES; i++)
 		for (j = 0; j < N_CODEPAGES; j++)
 			translation_array[i][j] = NULL;
+
 	current_language = 0;
 	current_lang_charset = 0;
 }
 
-void shutdown_trans()
+void
+shutdown_trans()
 {
 	int i, j, k;
+
 	for (i = 0; i < N_LANGUAGES; i++)
-		for (j = 0; j < N_CODEPAGES; j++) if (translation_array[i][j]) {
-			for (k = 0; k < T__N_TEXTS; k++) if (translation_array[i][j][k])
+		for (j = 0; j < N_CODEPAGES; j++)
+			if (translation_array[i][j]) {
+				for (k = 0; k < T__N_TEXTS; k++)
+					if (translation_array[i][j][k])
 				mem_free(translation_array[i][j][k]);
-			mem_free(translation_array[i][j]);
-		}
+				mem_free(translation_array[i][j]);
+			}
 }
 
-unsigned char *get_text_translation(unsigned char *text, struct terminal *term)
+unsigned char *
+get_text_translation(unsigned char *text, struct terminal *term)
 {
 	unsigned char **current_tra;
 	struct conv_table *conv_table;
 	unsigned char *trn;
-	if (text < dummyarray || text > dummyarray + T__N_TEXTS) return text;
-	if ((current_tra = translation_array[current_language][term->spec->charset])) {
+
+	if (text < dummyarray || text > dummyarray + T__N_TEXTS)
+		return text;
+
+	current_tra = translation_array[current_language][term->spec->charset];
+	if (current_tra) {
 		unsigned char *tt;
-		if ((trn = current_tra[text - dummyarray])) return trn;
-		tr:
-		if (!(tt = translations[current_language].t[text - dummyarray].name)) {
+
+		trn = current_tra[text - dummyarray];
+		if (trn) return trn;
+
+tr:
+		tt = translations[current_language].t[text - dummyarray].name;
+		if (!tt) {
 			trn = stracpy(translation_english[text - dummyarray].name);
 		} else {
-			conv_table = get_translation_table(current_lang_charset, term->spec->charset);
+			conv_table = get_translation_table(current_lang_charset,
+							   term->spec->charset);
 			trn = convert_string(conv_table, tt, strlen(tt));
 		}
 		current_tra[text - dummyarray] = trn;
+
 	} else {
-		if (current_lang_charset && term->spec->charset != current_lang_charset) {
-			if ((current_tra = translation_array[current_language][term->spec->charset] = mem_alloc(sizeof (unsigned char **) * T__N_TEXTS))) {
+		if (current_lang_charset
+		    && term->spec->charset != current_lang_charset) {
+			current_tra = mem_alloc(sizeof (unsigned char **) * T__N_TEXTS);
+			translation_array[current_language][term->spec->charset] = current_tra;
+
+			if (current_tra) {
 				memset(current_tra, 0, sizeof (unsigned char **) * T__N_TEXTS);
 				goto tr;
 			}
 		}
-		if (!(trn = translations[current_language].t[text - dummyarray].name)) {
-			trn = translations[current_language].t[text - dummyarray].name = translation_english[text - dummyarray].name;	/* modifying translation structure */
+
+		trn = translations[current_language].t[text - dummyarray].name;
+		if (!trn) {
+			trn = translation_english[text - dummyarray].name;
+			translations[current_language].t[text - dummyarray].name = trn;	/* modifying translation structure */
 		}
 	}
+
 	return trn;
 }
 
-unsigned char *get_english_translation(unsigned char *text)
+unsigned char *
+get_english_translation(unsigned char *text)
 {
-	if (text < dummyarray || text > dummyarray + T__N_TEXTS) return text;
+	if (text < dummyarray || text > dummyarray + T__N_TEXTS)
+		return text;
+
 	return translation_english[text - dummyarray].name;
 }
 
-int n_languages()
+int
+n_languages()
 {
 	return N_LANGUAGES;
 }
 
-unsigned char *language_name(int l)
+unsigned char *
+language_name(int l)
 {
 	return translations[l].t[T__LANGUAGE].name;
 }
 
-void set_language(int l)
+void
+set_language(int l)
 {
 	int i;
 	unsigned char *cp;
-	for (i = 0; i < T__N_TEXTS; i++) if (translations[l].t[i].code != i) {
-		internal("Bad table for language %s. Run script synclang.", translations[l].t[T__LANGUAGE].name);
-		return;
-	}
+
+	for (i = 0; i < T__N_TEXTS; i++)
+		if (translations[l].t[i].code != i) {
+			internal("Bad table for language %s. Run script synclang.",
+				 translations[l].t[T__LANGUAGE].name);
+			return;
+		}
+
 	current_language = l;
 	cp = translations[l].t[T__CHAR_SET].name;
 	i = get_cp_index(cp);
+
 	if (i == -1) {
-		internal("Unknown charset for language %s.", translations[l].t[T__LANGUAGE].name);
+		internal("Unknown charset for language %s.",
+			 translations[l].t[T__LANGUAGE].name);
 		i = 0;
 	}
 	current_lang_charset = i;

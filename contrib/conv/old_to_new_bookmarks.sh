44112d86e0c9bd9698dbc1b39bdd61064c992bf5
Petr Baudis <pasky@ucw.cz>
2002-07-02 21:54:14 +0000

    old_to_new_bookmarks.sh still goes to ~/.links

--- contrib/conv/old_to_new_bookmarks.sh
+++ contrib/conv/old_to_new_bookmarks.sh
@@ -4,11 +4,11 @@
 # WARNING: Close all ELinks sessions before running this script.
 # This script converts ELinks bookmarks file with '|' as separator to new
 # bookmarks format where separator is tab char. It saves old file to
-# ~/.elinks/bookmarks.with_pipes. --Zas
+# ~/.links/bookmarks.with_pipes. --Zas
 
 # Script by Stephane Chazelas :)
 
-BMFILE=$HOME/.elinks/bookmarks
+BMFILE=$HOME/.links/bookmarks
 if [ ! -r "$BMFILE" ]; then
 	echo "$BMFILE does not exist or is not readable!" >&2
 	exit 1
@@ -25,6 +25,7 @@ if cp -f "$BMFILE" "${BMFILE}.with_pipes" \
 then
   echo "Bookmarks file converted."
   echo "Old file was saved as ${BMFILE}.with_pipes."
+  echo "You may want to copy ~/.links/bookmarks to ~/.elinks/bookmarks now."
 else
   echo "Conversion failure" >&2
   exit 1

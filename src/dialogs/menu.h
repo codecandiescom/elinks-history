460e8d01b0c23f61d2bb83a2e1b96d62165df1db
Petr Baudis <pasky@ucw.cz>
2002-07-04 12:07:26 +0000

    Stale prototype removed.

--- src/dialogs/menu.h
+++ src/dialogs/menu.h
@@ -1,4 +1,4 @@
-/* $Id: menu.h,v 1.3 2002/05/08 13:55:02 pasky Exp $ */
+/* $Id: menu.h,v 1.4 2002/07/04 12:07:26 pasky Exp $ */
 
 #ifndef EL__DIALOG_MENU_H
 #define EL__DIALOG_MENU_H
@@ -11,7 +11,6 @@ void activate_bfu_technology(struct session *, int);
 
 void dialog_goto_url(struct session *ses, char *url);
 /* void dialog_save_url(struct session *ses); */
-void dialog_lua_console(struct session *ses);
 
 void free_history_lists();
 

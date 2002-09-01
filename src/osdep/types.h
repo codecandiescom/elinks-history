5b04e66405739eca7d2b532eaf0f3e4a200c5469
Petr Baudis <pasky@ucw.cz>
2002-09-01 11:37:48 +0000

    I'm stupid - sizeof obviously returns number of bytes, not bits ;-)).

--- src/osdep/types.h
+++ src/osdep/types.h
@@ -1,4 +1,4 @@
-/* $Id: types.h,v 1.1 2002/08/27 02:13:57 pasky Exp $ */
+/* $Id: types.h,v 1.2 2002/09/01 11:37:48 pasky Exp $ */
 
 #ifndef EL__UTIL_TYPES_H
 #define EL__UTIL_TYPES_H
@@ -16,15 +16,15 @@
 #endif
 
 #ifndef HAVE_INT32_T
-#if SIZEOF_CHAR == 32
+#if SIZEOF_CHAR == 4
 typedef char int32_t;
-#elif SIZEOF_SHORT == 32
+#elif SIZEOF_SHORT == 4
 typedef short int32_t;
-#elif SIZEOF_INT == 32
+#elif SIZEOF_INT == 4
 typedef int int32_t;
-#elif SIZEOF_LONG == 32
+#elif SIZEOF_LONG == 4
 typedef long int32_t;
-#elif defined(HAVE_LONG_LONG) && SIZEOF_LONG_LONG == 32
+#elif defined(HAVE_LONG_LONG) && SIZEOF_LONG_LONG == 4
 typedef long long int32_t;
 #else
 #error You have no 32-bit integer type. Get in touch with reality.
@@ -32,15 +32,15 @@ typedef long long int32_t;
 #endif
 
 #ifndef HAVE_UINT32_T
-#if SIZEOF_CHAR == 32
+#if SIZEOF_CHAR == 4
 typedef unsigned char uint32_t;
-#elif SIZEOF_SHORT == 32
+#elif SIZEOF_SHORT == 4
 typedef unsigned short uint32_t;
-#elif SIZEOF_INT == 32
+#elif SIZEOF_INT == 4
 typedef unsigned int uint32_t;
-#elif SIZEOF_LONG == 32
+#elif SIZEOF_LONG == 4
 typedef unsigned long uint32_t;
-#elif defined(HAVE_LONG_LONG) && SIZEOF_LONG_LONG == 32
+#elif defined(HAVE_LONG_LONG) && SIZEOF_LONG_LONG == 4
 typedef unsigned long long uint32_t;
 #else
 #error You have no 32-bit integer type. Get in touch with reality.

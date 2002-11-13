f3e123622c227ff15cc15c4e7f5e1aae7f104c12
Petr Baudis <pasky@ucw.cz>
2002-11-13 22:12:25 +0000

    Introduced minimal height 6 of the box, requested by zim and reasonable minimum, it looks..

--- src/bfu/listbox.c
+++ src/bfu/listbox.c
@@ -1,5 +1,5 @@
 /* Listbox widget implementation. */
-/* $Id: listbox.c,v 1.42 2002/10/30 13:31:49 zas Exp $ */
+/* $Id: listbox.c,v 1.43 2002/11/13 22:12:25 pasky Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -34,6 +34,7 @@ dlg_format_box(struct terminal *term, struct terminal *t2,
 	/* (*y) += item->item->gid; */
 	/* This is only weird heuristic, it could scale well I hope. */
 	item->h = (term ? term->y : t2 ? t2->y : 25) * 2 / 3 - 2 * DIALOG_TB - 8;
+	if (item->h < 6) item->h = 6;
 	/* debug("::%d(%d)::%d::%d::", term?term->y:t2->y, term?1:2, item->h, *y); */
 	(*y) += item->h;
 }

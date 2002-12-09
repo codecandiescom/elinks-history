b0ec39748ff165dd44a941f5471271900b99b36b
Petr Baudis <pasky@ucw.cz>
2002-12-09 20:14:42 +0000

    Fixed another critical bug and working on cygwin - we forgot to export the handlers table properly. Thanks to Paul G. Barnett for report, debugging and fix.

--- src/bookmarks/backend/default.h
+++ src/bookmarks/backend/default.h
@@ -1,10 +1,10 @@
-/* $Id: default.h,v 1.1 2002/12/01 19:02:09 pasky Exp $ */
+/* $Id: default.h,v 1.2 2002/12/09 20:14:42 pasky Exp $ */
 
 #ifndef EL__BOOKMARKS_BACKEND_DEFAULT_H
 #define EL__BOOKMARKS_BACKEND_DEFAULT_H
 
 #include "bookmarks/backend/common.h"
 
-struct bookmarks_backend default_bookmarks_backend;
+extern struct bookmarks_backend default_bookmarks_backend;
 
 #endif

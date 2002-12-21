fde52b18afdb9b26de2717ea900345569252d712
Laurent Monin <zas@norz.org>
2002-12-21 19:58:20 +0000

    Minor tidyup.

--- src/protocol/http/header.c
+++ src/protocol/http/header.c
@@ -1,5 +1,5 @@
 /* Parser of HTTP headers */
-/* $Id: header.c,v 1.9 2002/12/07 20:05:57 pasky Exp $ */
+/* $Id: header.c,v 1.10 2002/12/21 19:58:20 zas Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -18,25 +18,29 @@ unsigned char *
 parse_http_header(unsigned char *head, unsigned char *item,
 		  unsigned char **ptr)
 {
-	unsigned char *i, *f, *g, *h;
+	unsigned char *i, *f, *g, *h = NULL;
 
 	if (!head) return NULL;
-	h = NULL;
+
 	for (f = head; *f; f++) {
-		if (*f != 10) continue;
+		if (*f != '\n') continue;
+
 		f++;
 		for (i = item; *i && *f; i++, f++)
 			if (upcase(*i) != upcase(*f)) goto cont;
 		if (!*f) break;
+
 		if (f[0] == ':') {
 			while (f[1] == ' ') f++;
 			for (g = ++f; *g >= ' '; g++);
 			while (g > f && g[-1] == ' ') g--;
+
 			if (h) mem_free(h);
 			h = mem_alloc(g - f + 1);
+
 			if (h) {
 				memcpy(h, f, g - f);
-				h[g - f] = 0;
+				h[g - f] = '\0';
 				if (ptr) {
 					*ptr = f;
 					break;
@@ -58,17 +62,23 @@ parse_http_header_param(unsigned char *x, unsigned char *e)
 	int lp;
 	unsigned char *y = x;
 
-a:
+again:
 	y = strchr(y, ';');
 	if (!y) return NULL;
+
 	while (*y && (*y == ';' || *y <= ' ')) y++;
 	if (strlen(y) < le) return NULL;
-	if (strncasecmp(y, e, le)) goto a;
+
+	if (strncasecmp(y, e, le)) goto again;
+
 	y += le;
+
 	while (*y && (*y <= ' ' || *y == '=')) y++;
 	if (!*y) return stracpy("");
+
 	lp = 0;
 	while (y[lp] >= ' ' && y[lp] != ';') lp++;
+
 	return memacpy(y, lp);
 }
 
@@ -83,17 +93,21 @@ get_http_header_param(unsigned char *e, unsigned char *name)
 again:
 	while (*e && upcase(*e++) != upcase(*name));
 	if (!*e) return NULL;
+
 	n = name + 1;
 	while (*n && upcase(*e) == upcase(*n)) e++, n++;
 	if (*n) goto again;
+
 	while (WHITECHAR(*e)) e++;
 	if (*e++ != '=') return NULL;
-	while (WHITECHAR(*e)) e++;
 
+	while (WHITECHAR(*e)) e++;
 	start = e;
-	if (!IS_QUOTE(*e)) while (*e && !WHITECHAR(*e)) e++;
-	else {
-		char uu = *e++;
+
+	if (!IS_QUOTE(*e)) {
+		while (*e && !WHITECHAR(*e)) e++;
+	} else {
+		unsigned char uu = *e++;
 
 		start++;
 		while (*e != uu) {
@@ -108,12 +122,13 @@ again:
 
 	n = mem_alloc(e - start + 1);
 	if (!n) return NULL;
+
 	while (start < e) {
 		if (*start < ' ') n[i] = '.';
 		else n[i] = *start;
 		i++; start++;
 	}
-	n[i] = 0;
+	n[i] = '\0';
 
 	return n;
 }

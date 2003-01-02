3c580f07d72c34678443bcb160c97424434bd8e8
Petr Baudis <pasky@ucw.cz>
2003-01-02 15:45:38 +0000

    autogen.sh now also creates acinclude.m4 from m4/*.m4 and removes config.cache.

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
+
+echo acinclude.m4...
+cat m4/*.m4 > acinclude.m4
+
+echo aclocal...
 aclocal
+
+echo autoheader...
 autoheader
+
+echo automake...
 automake -a -c
+
+echo autoconf...
 autoconf
+
+echo config.cache...
+rm -f config.cache
+
+echo done

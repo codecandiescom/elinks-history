66f2ff3bc795a2cf18c7d6b8ca189c9dd17ad07d
Laurent Monin <zas@norz.org>
2003-01-23 13:14:46 +0000

    Compilation fix: warning: pointer type mismatch in conditional expression.

--- src/sched/download.c
+++ src/sched/download.c
@@ -1,5 +1,5 @@
 /* Downloads managment */
-/* $Id: download.c,v 1.8 2003/01/23 13:07:03 pasky Exp $ */
+/* $Id: download.c,v 1.9 2003/01/23 13:14:46 zas Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -692,7 +692,7 @@ lookup_unique_name(struct terminal *term, unsigned char *ofile, int resume,
 		N_("File exists"), AL_CENTER | AL_EXTD_TEXT,
 		N_("This file already exists:\n"),
 		lun_hop->ofile ? lun_hop->ofile : (unsigned char *) "", "\n\n",
-		N_("The alternative filename is:\n"), file ? file : "", NULL,
+		N_("The alternative filename is:\n"), file ? file : (unsigned char *) "", NULL,
 		lun_hop, 4,
 		N_("Save under the alternative name"), lun_alternate, B_ENTER,
 		N_("Overwrite the original file"), lun_overwrite, 0,

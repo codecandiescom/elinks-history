024554df99b51070bf3d78e0724e7e6112b1dd7a
Laurent Monin <zas@norz.org>
2003-05-05 14:08:02 +0000

    Minor cleanup (2) ;)

--- src/terminal/tab.h
+++ src/terminal/tab.h
@@ -1,4 +1,4 @@
-/* $Id: tab.h,v 1.3 2003/05/05 14:06:47 zas Exp $ */
+/* $Id: tab.h,v 1.4 2003/05/05 14:08:02 zas Exp $ */
 
 #ifndef EL__TERMINAL_TAB_H
 #define EL__TERMINAL_TAB_H
@@ -7,14 +7,14 @@
 #include "terminal/window.h"
 #include "util/lists.h"
 
-#define get_current_tab(term) get_tab_by_number((term), (term)->current_tab)
-#define IF_ACTIVE(win,term) if (!(win)->type || (win) == get_current_tab((term)))
-
 struct window *init_tab(struct terminal *);
 int number_of_tabs(struct terminal *);
 int get_tab_number(struct window *);
 struct window *get_tab_by_number(struct terminal *, int);
 void switch_to_tab(struct terminal *, int);
 void close_tab(struct terminal *);
+
+#define get_current_tab(term) get_tab_by_number((term), (term)->current_tab)
+#define IF_ACTIVE(win, term) if (!(win)->type || (win) == get_current_tab((term)))
 
 #endif

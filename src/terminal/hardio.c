285dbec15595aaf69cd10f37d5b6728e0fad05d1
Laurent Monin <zas@norz.org>
2003-05-08 01:23:15 +0000

    Debugging code is now cleaner and is useable with hard_read() too.

--- src/terminal/hardio.c
+++ src/terminal/hardio.c
@@ -1,5 +1,5 @@
 /* Low-level terminal-suitable I/O routines */
-/* $Id: hardio.c,v 1.5 2003/05/07 23:04:06 zas Exp $ */
+/* $Id: hardio.c,v 1.6 2003/05/08 01:23:15 zas Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -17,7 +17,72 @@
 #include "terminal/hardio.h"
 
 /* Define it to have data written to stderr */
-#undef HW_DEBUG
+/* #define HW_DEBUG */
+
+#undef debug_open
+#undef debug_flush
+#undef debug_write
+
+#ifdef HW_DEBUG
+static void
+hw_debug_open(unsigned char *name, int fd, unsigned char *p, int l)
+{
+	fprintf(stderr, "[%s (fd=%d, p=%p, l=%d)]\n",name, fd, p, l);
+}
+
+static void
+hw_debug_flush() {
+	fputs("\n\n", stderr);
+	fflush(stderr);
+}
+
+static void
+hw_debug_write(unsigned char *p, int w)
+{
+	int hex = 0;
+	int i = 0;
+
+	if (!w) return;
+
+	for (; i < w; i++) {
+		if (p[i] == ' ') {
+			int c = i;
+
+			while (i < w && p[++i] == ' ');
+
+			if (i - c - 1 > 1) {
+				fprintf(stderr, "[+ %d spaces]\n", i - c - 1);
+				if (i == w) break;
+				c = 0;
+				continue;
+			}
+			c = 0;
+			i--;
+		}
+
+		if (p[i] >= ' ' && p[i] < 127 && p[i] != '|') {
+			if (hex) {
+				fputc('|', stderr);
+				hex = 0;
+			}
+			fputc(p[i], stderr);
+		} else {
+			if (!hex) {
+				fputc('|', stderr);
+				hex = 1;
+			}
+			fprintf(stderr,"%02x", p[i]);
+		}
+	}
+}
+#define debug_open(n, fd, p, l) hw_debug_open(n, fd, p, l)
+#define debug_flush() hw_debug_flush()
+#define debug_write(p, l) hw_debug_write(p, l)
+#else
+#define debug_open(n, fd, p, l)
+#define debug_flush()
+#define debug_write(p, l)
+#endif
 
 
 int
@@ -26,9 +91,7 @@ hard_write(int fd, unsigned char *p, int l)
 	int w = 1;
 	int t = 0;
 
-#ifdef HW_DEBUG
-	fprintf(stderr, "[hard_write(fd=%d, p=%p, l=%d)]\n", fd, p, l);
-#endif
+	debug_open("hard_write", fd, p, l);
 
 	while (l > 0 && w) {
 		w = write(fd, p, l);
@@ -36,52 +99,16 @@ hard_write(int fd, unsigned char *p, int l)
 			if (errno == EINTR) continue;
 			return -1;
 		}
-#ifdef HW_DEBUG
-		if (w) {
-			int hex = 0;
-			int i = 0;
 
-			for (; i < w; i++) {
-				if (p[i] == ' ') {
-					int c = i;
+		debug_write(p, w);
 
-					while (i < w && p[++i] == ' ');
-
-					if (i - c - 1 > 1) {
-						fprintf(stderr, "[+ %d spaces]\n", i - c - 1);
-						if (i == w) break;
-						c = 0;
-						continue;
-					}
-					c = 0;
-					i--;
-				}
-
-				if (p[i] >= ' ' && p[i] < 127 && p[i] != '|') {
-					if (hex) {
-						fputc('|', stderr);
-						hex = 0;
-					}
-					fputc(p[i], stderr);
-				} else {
-					if (!hex) {
-						fputc('|', stderr);
-						hex = 1;
-					}
-					fprintf(stderr,"%02x", p[i]);
-				}
-			}
-		}
-#endif
 		t += w;
 		p += w;
 		l -= w;
 	}
 
-#ifdef HW_DEBUG
-	fputs("\n\n", stderr);
-	fflush(stderr);
-#endif
+	debug_flush();
+
 	return t;
 }
 
@@ -91,25 +118,25 @@ hard_read(int fd, unsigned char *p, int l)
 	int r = 1;
 	int t = 0;
 
+	debug_open("hard_read", fd, p, l);
+
 	while (l > 0 && r) {
 		r = read(fd, p, l);
 		if (r < 0) {
 			if (errno == EINTR) continue;
 			return -1;
 		}
-#if 0 /* for debugging purpose */
-		{
-		 int ww;
 
-		 for (ww = 0; ww < r; ww++)
-			 fprintf(stderr, " %02x", (int) p[ww]);
-		 fflush(stderr);
-		}
-#endif
-	 	t += r;
+		debug_write(p, r);
+
+		t += r;
 		p += r;
 		l -= r;
 	}
 
+	debug_flush();
+
 	return t;
 }
+
+

bea88a547795aac456315848effcc56beacefed2
Laurent Monin <zas@norz.org>
2003-05-17 10:28:11 +0000

    An helper was added to detect redundant calls to _(). Should help with bug 141. To enable it, define DEBUG_IT in libintl.h

--- src/intl/gettext/libintl.h
+++ src/intl/gettext/libintl.h
@@ -1,4 +1,4 @@
-/* $Id: libintl.h,v 1.12 2003/05/08 21:50:08 zas Exp $ */
+/* $Id: libintl.h,v 1.13 2003/05/17 10:28:11 zas Exp $ */
 
 #ifndef EL__INTL_GETTEXT_LIBINTL_H
 #define EL__INTL_GETTEXT_LIBINTL_H
@@ -17,11 +17,16 @@
 #include "config/options.h"
 #include "intl/charsets.h"
 #include "terminal/terminal.h"
+#include "util/error.h"
+
+/* Define it to find redundant useless calls */
+/* #define DEBUG_IT */
 
 /* TODO: Ideally, we should internally work only in Unicode - then the need for
  * charsets multiplexing would cease. That'll take some work yet, though.
  * --pasky */
 
+#ifndef DEBUG_IT
 /* Wraps around gettext(), employing charset multiplexing. If you don't care
  * about charset (usually during initialization or when you don't use terminals
  * at all), use gettext() directly. */
@@ -45,6 +50,63 @@ _(unsigned char *msg, struct terminal *term)
 
 	return (unsigned char *) gettext(msg);
 }
+#else
+
+/* This one will emit errors on null/empty msgs and when multiple calls are
+ * done for the same result in the same function. Some noise is possible,
+ * when a function is called twice or more, but then we should cache msg,
+ * in function caller. --Zas */
+
+/* __FUNCTION__ isn't supported by all, but it's debugging code. */
+#define _(m, t) __(__FILE__, __LINE__, __FUNCTION__, m, t)
+
+/* overflows are theorically possible here. Debug purpose only. */
+static inline unsigned char *
+__(unsigned char *file, unsigned int line, unsigned char *func,
+   unsigned char *msg, struct terminal *term)
+{
+	static unsigned char last_file[512] = "";
+	static unsigned int last_line = 0;
+	static unsigned char last_func[1024] = "";
+	static unsigned char last_result[16384] = "";
+	static int current_charset = -1;
+	int new_charset;
+	unsigned char *result;
+
+
+	/* Prevent useless (and possibly dangerous) calls. */
+	if (!msg || !*msg) {
+		error("%s:%d %s msg parameter", file, line, msg ? "empty": "NULL");
+		return msg;
+	}
+
+	/* Prevent useless switching. */
+	new_charset = get_opt_int_tree(term->spec, "charset");
+	if (current_charset != new_charset) {
+		current_charset = new_charset;
+		bind_textdomain_codeset( /* PACKAGE */ "elinks",
+					get_cp_mime_name(current_charset));
+	}
+
+	result = (unsigned char *) gettext(msg);
+
+	if (!strcmp(result, last_result)
+	    && !strcmp(file, last_file)
+	    && !strcmp(func, last_func)) {
+		error("%s:%d Duplicate call to _() in %s() (previous at line %d)",
+		      file, line, func, last_line);
+	}
+
+	/* Risky ;) */
+	strcpy(last_file, file);
+	strcpy(last_func, func);
+	strcpy(last_result, result);
+	last_line = line;
+
+	return result;
+}
+
+#endif
 
 /* no-op - just for marking */
 #define N_(msg) (gettext_noop(msg))

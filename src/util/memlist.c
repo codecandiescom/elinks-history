91a8dc72d301a2d7524ff95e41a5a94477b68dfd
Laurent Monin <zas@norz.org>
2003-06-04 23:23:18 +0000

    Almost complete rewrite of memory list routines. Useless allocation for empty lists were removed. Comments were added.

--- src/util/memlist.c
+++ src/util/memlist.c
@@ -1,5 +1,5 @@
-/* This routines represent handling of struct memory_list. */
-/* $Id: memlist.c,v 1.6 2002/12/07 20:05:57 pasky Exp $ */
+/* These routines represent handling of struct memory_list. */
+/* $Id: memlist.c,v 1.7 2003/06/04 23:23:18 zas Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -21,73 +21,100 @@
  * back in order to free it himself.
  */
 
-struct memory_list *getml(void *p, ...)
+#define ML_SIZE(n) (sizeof(struct memory_list) + (n) * sizeof(void *))
+
+/* Create a memory list. If p is NULL or allocation fails, it will
+ * returns NULL.
+ * It always stops at first NULL element. */
+struct memory_list *
+getml(void *p, ...)
 {
 	struct memory_list *ml;
 	va_list ap;
 	void *q;
-	int i;
+	int n = 1;
 
+	/* If first element is NULL, there's no need to allocate memory, so
+	 * just return. */
+	if (!p) return NULL;
+
+	/* How many elements ? */
 	va_start(ap, p);
-	for (i = 0, q = p; q; i++) {
-		q = va_arg(ap, void *);
-	}
-
-	ml = mem_alloc(sizeof(struct memory_list) + i * sizeof(void *));
-	if (!ml) {
-		va_end(ap);
-		return NULL;
-	}
-
-	ml->n = i;
+	while ((q = va_arg(ap, void *))) n++;
 	va_end(ap);
 
+	/* Allocate space for memory list. */
+	ml = mem_alloc(ML_SIZE(n));
+	if (!ml) return NULL;
+
+	/* First element. */
+	ml->n = 1;
+	ml->p[0] = p;
+
+	/* Next ones. */
 	va_start(ap, p);
-	for (i = 0, q = p; q; i++) {
-		ml->p[i] = q;
-		q = va_arg(ap, void *);
-	}
+	while ((q = va_arg(ap, void *))) ml->p[ml->n++] = q;
 	va_end(ap);
 
+	/* The end. */
 	return ml;
 }
 
-void add_to_ml(struct memory_list **ml, ...)
+/* Add elements to a memory list.
+ * If memory list exists, it enlarges it, else it creates it.
+ * if there's no elements or first element is NULL, it does nothing.
+ * It always stops at first NULL element. */
+void
+add_to_ml(struct memory_list **ml, ...)
 {
-	struct memory_list *nml;
 	va_list ap;
 	void *q;
 	int n = 0;
 
-	if (!*ml) {
-		*ml = mem_alloc(sizeof(struct memory_list));
-		if (!*ml) return;
-
-		(*ml)->n = 0;
-	}
-
+	/* How many new elements ? */
 	va_start(ap, ml);
 	while ((q = va_arg(ap, void *))) n++;
 	va_end(ap);
 
-	nml = mem_realloc(*ml, sizeof(struct memory_list) + (n + (*ml)->n) * sizeof(void *));
-	if (!nml)
-		return;
+	/* None, so just return. */
+	if (!n) return;
 
+	if (!*ml) {
+		/* If getml() wasn't called before or returned NULL,
+		 * then we create it. */
+		*ml = mem_alloc(ML_SIZE(n));
+		if (!*ml) return;
+
+		(*ml)->n = 0;
+	} else {
+		/* Enlarge existing ml. */
+		struct memory_list *nml;
+
+		nml = mem_realloc(*ml, ML_SIZE(n + (*ml)->n));
+		if (!nml) return;
+
+		*ml = nml;
+	}
+
+	/* Set ml with new elements and update count. */
 	va_start(ap, ml);
-	while ((q = va_arg(ap, void *))) nml->p[nml->n++] = q;
+	while ((q = va_arg(ap, void *))) (*ml)->p[(*ml)->n++] = q;
 	va_end(ap);
-
-	*ml = nml;
 }
 
-void freeml(struct memory_list *ml)
+/* Free elements and memory list.
+ * It ignores NULL pointers. */
+void
+freeml(struct memory_list *ml)
 {
-	int i;
+	if (ml) {
+		int i;
 
-	if (!ml) return;
+		for (i = 0; i < ml->n; i++)
+			mem_free(ml->p[i]);
 
-	for (i = 0; i < ml->n; i++) mem_free(ml->p[i]);
-
-	mem_free(ml);
+		mem_free(ml);
+	}
 }
+
+#undef ML_SIZE

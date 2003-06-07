6ec6b77cbe502f5ad807eccd38b749c076d76571
Laurent Monin <zas@norz.org>
2003-06-07 08:37:21 +0000

    Test exists in configure script, so just use HAVE_VARIADIC_MACROS.

--- src/util/memlist.h
+++ src/util/memlist.h
@@ -1,4 +1,4 @@
-/* $Id: memlist.h,v 1.2 2003/06/05 16:39:25 zas Exp $ */
+/* $Id: memlist.h,v 1.3 2003/06/07 08:37:21 zas Exp $ */
 
 #ifndef EL__UTIL_MEMLIST_H
 #define EL__UTIL_MEMLIST_H
@@ -9,12 +9,8 @@ struct memory_list {
 };
 
 #undef DEBUG_MEMLIST
-/* variadic macros are available in compilers conforming to the ISO C99 standard
- * We test stdc version and enable memlist debug only if a recent one is
- * found. Not perfect but should work. --Zas */
-#if defined(DEBUG) && \
-    ((defined(__GNUC__) && __GNUC__ >= 2) || \
-     (defined(__STDC_VERSION__) && __STDC_VERSION__ >= 199901L))
+
+#if defined(DEBUG) && defined(HAVE_VARIADIC_MACROS)
 struct memory_list *debug_getml(unsigned char *file, int line, void *p, ...);
 void debug_add_to_ml(unsigned char *file, int line, struct memory_list **ml, ...);
 #define getml(...) debug_getml(__FILE__, __LINE__, __VA_ARGS__)

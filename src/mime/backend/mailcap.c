fd7458679bcc31c52b872d58768dada15041e75b
Jonas Fonseca <fonseca@diku.dk>
2003-06-08 02:00:55 +0000

    Destroy file_read_line() and integrate it in parse_mailcap_file() all cleaned up. Also makes parsing more tolerant and optimized.

--- src/mime/backend/mailcap.c
+++ src/mime/backend/mailcap.c
@@ -1,5 +1,5 @@
 /* RFC1524 (mailcap file) implementation */
-/* $Id: mailcap.c,v 1.31 2003/06/07 23:42:31 jonas Exp $ */
+/* $Id: mailcap.c,v 1.32 2003/06/08 02:00:55 jonas Exp $ */
 
 /* This file contains various functions for implementing a fair subset of
  * rfc1524.
@@ -271,12 +271,21 @@ parse_mailcap_file(unsigned char *filename, unsigned int priority)
 {
 	FILE *file = fopen(filename, "r");
 	unsigned char *line = NULL;
-	size_t linelen;
-	int lineno = 0;
+	size_t linelen = MAX_STR_LEN;
+	size_t offset = 0;
+	int lineno = 1;
 
 	if (!file) return;
 
-	while ((line = file_read_line(line, &linelen, file, &lineno))) {
+	line = mem_alloc(linelen);
+	if (!line) {
+		fclose(file);
+		return;
+	}
+
+	/* Read the file line by line. If a line ends with '\' the next line is
+	 * read too increasing @line if necessary. */
+	for (; fgets(line + offset, linelen - offset, file) || offset; lineno++) {
 		struct mailcap_entry *entry;
 		unsigned char *type;
 		unsigned char *command;
@@ -284,6 +293,41 @@ parse_mailcap_file(unsigned char *filename, unsigned int priority)
 
 		/* Ignore comments */
 		if (*line == '#') continue;
+
+		linepos = strchr(line + offset, '\n');
+		if (linepos) {
+			/* A whole line was read. Fetch next into the buffer if
+			 * the line is 'continued'. */
+			unsigned char *end = linepos - 1;
+
+			while (line < end && isspace(*end))
+				end--;
+
+			if (*end == '\\') {
+				offset = linepos - line - 1;
+				continue;
+			} else {
+				offset = 0;
+				*(end+1) = '\0';
+			}
+		} else {
+			/* Test if the line buffer should be increase because
+			 * it was continued and could not fit. */
+			int c = getc(file);
+
+			if (c != EOF) {
+				/* Not on last line. */
+				ungetc(c, file);
+				offset = linelen - 1;
+				linelen += MAX_STR_LEN;
+
+				linepos = mem_realloc(line, linelen);
+				if (!linepos)
+					break;
+				line = linepos;
+				continue;
+			}
+		}
 
 		linepos = line;
 

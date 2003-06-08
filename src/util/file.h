7bfeda0fefbced3fca487c63834f2f86c917cd11
Jonas Fonseca <fonseca@diku.dk>
2003-06-08 13:58:14 +0000

    Documented functions.

--- src/util/file.h
+++ src/util/file.h
@@ -1,14 +1,31 @@
-/* $Id: file.h,v 1.3 2003/01/13 11:18:15 zas Exp $ */
+/* $Id: file.h,v 1.4 2003/06/08 13:58:14 jonas Exp $ */
 
 #ifndef EL__UTIL_FILE_H
 #define EL__UTIL_FILE_H
 
 #include <stdio.h>
 
-int file_exists(const unsigned char *);
-unsigned char *expand_tilde(unsigned char *);
-unsigned char *get_unique_name(unsigned char *);
-unsigned char *file_read_line(unsigned char *, size_t *, FILE *, int *);
-unsigned char *safe_fgets(unsigned char *, int, FILE *);
+int file_exists(const unsigned char *filename);
+
+/* Tilde is only expanded for the current users homedir (~/). */
+/* The returned file name is allocated. */
+unsigned char *expand_tilde(unsigned char *filename);
+
+/* Generate a unique file name by trial and error based on the @fileprefix by
+ * adding suffix counter (e.g. '.42'). */
+/* The returned file name is allocated. */
+unsigned char *get_unique_name(unsigned char *fileprefix);
+
+/* Read a line from @file into the dynamically allocated @line, increasing
+ * @line if necessary. The ending "\n" or "\r\n" is removed.  If a line ends
+ * with "\", this char and the linefeed is removed, and the next line is read
+ * too. */
+/* If @line is NULL the returned line is allocated and if file reading fails
+ * @line is free()d. */
+unsigned char *file_read_line(unsigned char *line, size_t *linesize,
+			      FILE *file, int *linenumber);
+
+/* Wrapper around fgets() that ensures that @buffer is NUL terminated. */
+unsigned char *safe_fgets(unsigned char *buffer, int size, FILE *file);
 
 #endif

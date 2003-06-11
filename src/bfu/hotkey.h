de0516671dcd78c4fad9f7a2c1999bfceb7df597
Petr Baudis <pasky@ucw.cz>
2003-06-11 16:20:02 +0000

    C++ style comments -> C comments

--- src/bfu/hotkey.h
+++ src/bfu/hotkey.h
@@ -1,18 +1,18 @@
 /* Hotkeys handling. */
-/* $Id: hotkey.h,v 1.1 2003/05/21 10:08:43 zas Exp $ */
+/* $Id: hotkey.h,v 1.2 2003/06/11 16:20:02 pasky Exp $ */
 
 #ifndef __EL_HOTKEY_H
 #define __EL_HOTKEY_H
 
 #include "bfu/menu.h"
 
-//int find_hotkey_pos(unsigned char *text);
+/* int find_hotkey_pos(unsigned char *text); */
 void init_hotkeys(struct terminal *term, struct menu_item *items, int ni, int hotkeys);
 #ifdef ENABLE_NLS
 void clear_hotkeys_cache(struct menu_item *items, int ni, int hotkeys);
 #endif
 void refresh_hotkeys(struct terminal *term, struct menu *menu);
-//int is_hotkey(struct menu_item *item, unsigned char key, struct terminal *term);
+/* int is_hotkey(struct menu_item *item, unsigned char key, struct terminal *term); */
 int check_hotkeys(struct menu_head *menu, unsigned char hotkey, struct terminal *term);
 int check_not_so_hot_keys(struct menu_head *menu, unsigned char key, struct terminal *term);
 

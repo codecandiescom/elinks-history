058465bfff10adb6cec02be8c4a7c3f26a4efd4d
Petr Baudis <pasky@ucw.cz>
2003-06-11 22:42:46 +0000

    Reordering of declarations.

--- src/sched/history.h
+++ src/sched/history.h
@@ -1,17 +1,11 @@
-/* $Id: history.h,v 1.2 2003/06/11 22:42:04 pasky Exp $ */
+/* $Id: history.h,v 1.3 2003/06/11 22:42:46 pasky Exp $ */
 
 #ifndef EL__SCHED_HISTORY_H
 #define EL__SCHED_HISTORY_H
 
 #include "sched/location.h"
 #include "sched/session.h"
-#include "util/lists.h"
 
-void go_back(struct session *);
-void go_unback(struct session *);
-
-void ses_back(struct session *);
-void ses_unback(struct session *);
 
 void create_history(struct session *);
 void destroy_history(struct session *);
@@ -21,5 +15,12 @@ static inline void
 add_to_history(struct session *ses, struct location *loc) {
 	add_to_list(ses->history, loc);
 }
+
+
+void go_back(struct session *);
+void go_unback(struct session *);
+
+void ses_back(struct session *);
+void ses_unback(struct session *);
 
 #endif

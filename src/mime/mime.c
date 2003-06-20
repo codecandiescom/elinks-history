1ffba140f521d938ec0eacea57ea12750886a07e
Jonas Fonseca <fonseca@diku.dk>
2003-06-20 17:28:02 +0000

    The quick hack version of using encoding extensions in content type resolving (enhance bug 26)

--- src/mime/mime.c
+++ src/mime/mime.c
@@ -1,5 +1,5 @@
 /* Functionality for handling mime types */
-/* $Id: mime.c,v 1.18 2003/06/20 13:50:32 pasky Exp $ */
+/* $Id: mime.c,v 1.19 2003/06/20 17:28:02 jonas Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -14,6 +14,7 @@
 #include "mime/mime.h"
 #include "protocol/http/header.h"	/* For parse_http_header() */
 #include "protocol/url.h"
+#include "util/encoding.h"
 #include "util/memory.h"
 #include "util/string.h"
 
@@ -29,13 +30,14 @@ done_mime(void)
 	done_mime_backends();
 }
 
-/* Trim the extension so only last .<extension> is used. */
+/* Checks if application/x-<extension> has any handlers. */
 static inline unsigned char *
 try_extension_type(unsigned char *extension)
 {
+	/* Trim the extension so only last .<extension> is used. */
+	unsigned char *trimmed = strrchr(extension, '.');
 	struct mime_handler *handler;
 	unsigned char *content_type;
-	unsigned char *trimmed = strrchr(extension, '.');
 
 	if (!trimmed)
 		return NULL;
@@ -52,6 +54,37 @@ try_extension_type(unsigned char *extension)
 	}
 
 	mem_free(content_type);
+	return NULL;
+}
+
+static inline unsigned char *
+try_encoding_type(unsigned char *extension)
+{
+	enum stream_encoding encoding = guess_encoding(extension);
+	unsigned char **extensions;
+	int extensionlen;
+
+	/* Yes yes this is a bit dull, having to do the dirty work but
+	 * that's life. */
+	if (encoding == ENCODING_NONE) return NULL;
+	extensions = listext_encoded(encoding);
+	extensionlen = strlen(extension);
+
+	for (; extensions && *extensions; extensions++) {
+		int len = strlen(*extensions);
+		unsigned char *snip = extension + extensionlen - len;
+		unsigned char *ctype;
+
+		if (extensionlen <= len && strncmp(snip + 1, *extensions, len))
+			continue;
+
+		len = *snip;
+		*snip = '\0';
+		ctype = get_content_type_backends(extension);
+		*snip = len;
+		return ctype;
+	}
+
 	return NULL;
 }
 
@@ -87,9 +120,9 @@ get_content_type(unsigned char *head, unsigned char *url)
 	if (extension) {
 		unsigned char *ctype = get_content_type_backends(extension);
 
-		/* Check if application/x-<extension> has any handlers. */
-		if (!ctype)
-			ctype = try_extension_type(extension);
+		if (!ctype) ctype = try_encoding_type(extension);
+
+		if (!ctype) ctype = try_extension_type(extension);
 
 		mem_free(extension);
 

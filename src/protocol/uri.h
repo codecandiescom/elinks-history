1c4075d88e8d7b85043b616e8ca72f3aec41487e
Petr Baudis <pasky@ucw.cz>
2003-07-23 15:11:13 +0000

    Rearranged the declarations so that they cluster a little more naturally.

--- src/protocol/uri.h
+++ src/protocol/uri.h
@@ -1,4 +1,4 @@
-/* $Id: uri.h,v 1.24 2003/07/23 00:47:11 pasky Exp $ */
+/* $Id: uri.h,v 1.25 2003/07/23 15:11:13 pasky Exp $ */
 
 #ifndef EL__PROTOCOL_URI_H
 #define EL__PROTOCOL_URI_H
@@ -30,6 +30,7 @@ struct uri {
 	unsigned char *post;
 };
 
+
 /* Initializes the members of the uri struct, as they are encountered. If
  * an uri component is recognized both it's length and starting point is
  * set. It is possible that the component start is set but that the length
@@ -37,18 +38,7 @@ struct uri {
 /* Returns the length of the parsed uri or 0 if some error was found. */
 int parse_uri(struct uri *uri, unsigned char *uristring);
 
-/* Returns either the uri's port number if available or the protocol's
- * default port. For user protocols this is 0. */
-int get_uri_port(struct uri *uri);
 
-static inline int
-end_of_dir(unsigned char c)
-{
-	return c == POST_CHAR || c == '#' || c == ';' || c == '?';
-}
-
-
-/* For use in get_uri_string(). */
 enum uri_component {
 	URI_PROTOCOL	= (1 << 0),
 	URI_USER	= (1 << 1),
@@ -70,23 +60,37 @@ struct string *add_uri_to_string(struct string *string, struct uri *uri, enum ur
  * there's no struct uri around. */
 struct string *add_string_uri_to_string(struct string *string, unsigned char *uristring, enum uri_component components);
 
+struct string *add_string_uri_filename_to_string(struct string *string, unsigned char *);
+
 /* Returns the new URI string or NULL upon an error. */
 unsigned char *get_uri_string(struct uri *uri, enum uri_component components);
+
+/* Returns either the uri's port number if available or the protocol's
+ * default port. For user protocols this is 0. */
+int get_uri_port(struct uri *uri);
+
+
+void encode_uri_string(struct string *, unsigned char *);
+#if 0
+void decode_uri_string(unsigned char *);
+#endif
+
+
+/* Returns allocated string containing the biggest possible extension.
+ * If url is 'jabadaba.1.foo.gz' the returned extension is '1.foo.gz' */
+unsigned char *get_extension_from_url(unsigned char *url);
 
 
 unsigned char *join_urls(unsigned char *, unsigned char *);
 unsigned char *translate_url(unsigned char *, unsigned char *);
 unsigned char *extract_position(unsigned char *);
 unsigned char *extract_proxy(unsigned char *);
-struct string *add_string_uri_filename_to_string(struct string *string, unsigned char *);
 
-/* Returns allocated string containing the biggest possible extension.
- * If url is 'jabadaba.1.foo.gz' the returned extension is '1.foo.gz' */
-unsigned char *get_extension_from_url(unsigned char *url);
 
-void encode_uri_string(struct string *, unsigned char *);
-#if 0
-void decode_uri_string(unsigned char *);
-#endif
+static inline int
+end_of_dir(unsigned char c)
+{
+	return c == POST_CHAR || c == '#' || c == ';' || c == '?';
+}
 
 #endif

74b6ae9739cc3ca39c93d778d7af494d7b7e674a
Laurent Monin <zas@norz.org>
2003-07-24 13:38:49 +0000

    Cleanup, optimization and assertions. Type casting to (int) is useless, so removed.

--- src/util/base64.c
+++ src/util/base64.c
@@ -1,5 +1,5 @@
 /* Base64 encoder implementation. */
-/* $Id: base64.c,v 1.5 2002/12/07 20:05:57 pasky Exp $ */
+/* $Id: base64.c,v 1.6 2003/07/24 13:38:49 zas Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -10,36 +10,43 @@
 #include "elinks.h"
 
 #include "util/base64.h"
+#include "util/error.h"
 #include "util/memory.h"
 
 unsigned char base64_chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
 unsigned char *
-base64_encode(unsigned char *in)
+base64_encode(unsigned char *str)
 {
-	unsigned char *out, *outstr;
-	int inlen = strlen(in);
+	register unsigned char *in = str;
+	register unsigned char *out;
+	unsigned char *outstr;
+	int inlen;
 
-	outstr = out = mem_alloc(((inlen / 3) + 1) * 4 + 1 );
-	if (!outstr) return NULL;
+	assert(str && *str);
+
+	inlen = strlen(in);
+	out = outstr = mem_alloc((inlen / 3) * 4 + 4 + 1);
+	if (!out) return NULL;
 
 	while (inlen >= 3) {
-		*out++ = base64_chars[(int) (*in >> 2) ];
-		*out++ = base64_chars[(int) ((*in << 4 | *(in + 1) >> 4) & 63) ];
-		*out++ = base64_chars[(int) ((*(in + 1) << 2 | *(in + 2) >> 6) & 63) ];
-		*out++ = base64_chars[(int) (*(in + 2) & 63) ];
-		inlen -= 3; in += 3;
+		*out++ = base64_chars[ (*in >> 2) ];
+		*out++ = base64_chars[ ((*in << 4 | *(++in) >> 4) & 63) ];
+		*out++ = base64_chars[ ((*in << 2 | *(++in) >> 6) & 63) ];
+		*out++ = base64_chars[ (*in & 63) ];
+		in++;
+		inlen -= 3;
 	}
 	if (inlen == 1) {
-		*out++ = base64_chars[(int) (*in >> 2) ];
-		*out++ = base64_chars[(int) (*in << 4 & 63) ];
+		*out++ = base64_chars[ (*in >> 2) ];
+		*out++ = base64_chars[ (*in << 4 & 63) ];
 		*out++ = '=';
 		*out++ = '=';
 	}
 	if (inlen == 2) {
-		*out++ = base64_chars[(int) (*in >> 2) ];
-		*out++ = base64_chars[(int) ((*in << 4 | *(in + 1) >> 4) & 63) ];
-		*out++ = base64_chars[(int) ((*(in + 1) << 2) & 63) ];
+		*out++ = base64_chars[ (*in >> 2) ];
+		*out++ = base64_chars[ ((*in << 4 | *(++in) >> 4) & 63) ];
+		*out++ = base64_chars[ ((*in << 2) & 63) ];
 		*out++ = '=';
 	}
 	*out = 0;

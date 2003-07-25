a11a5c1199f7b4291451475c5cb2e6ec08466471
Jonas Fonseca <fonseca@diku.dk>
2003-07-25 17:16:01 +0000

    Updated commenting of struct uri.

--- src/protocol/uri.h
+++ src/protocol/uri.h
@@ -1,4 +1,4 @@
-/* $Id: uri.h,v 1.29 2003/07/25 16:23:51 jonas Exp $ */
+/* $Id: uri.h,v 1.30 2003/07/25 17:16:01 jonas Exp $ */
 
 #ifndef EL__PROTOCOL_URI_H
 #define EL__PROTOCOL_URI_H
@@ -12,19 +12,19 @@
  * start of a field is set but that the length is zero so instead of testing
  * *uri-><fieldname> always use uri-><fieldname>len.
  *
- * XXX Lots of places in the code assume that the string members point into the
+ * XXX: Lots of places in the code assume that the string members point into the
  * same string. That means if you need to use a NUL terminated uri field either
  * temporary modify the string, allocated a copy or change the function used to
  * take a length parameter. */
-/* TODO We should probably add path+query members instead of data. */
+/* TODO: We should probably add path+query members instead of data. */
 struct uri {
 	/* The start of the uri (and thus start of the protocol string). */
 	unsigned char *string;
 
-	/* The internal protocol type. Can _never_ be PROTOCOL_UNKNOWN. */
+	/* The internal type of protocol. Can _never_ be PROTOCOL_UNKNOWN. */
 	int protocol; /* -> enum protocol (to workaround dependency nightmare) */
 
-	/* The protocollen should only be usable if @type is either
+	/* @protocollen should only be usable if @protocol is either
 	 * PROTOCOL_USER or an uri string should be composed. */
 	int protocollen;
 
@@ -40,15 +40,20 @@ struct uri {
 	unsigned char *port;
 	int portlen;
 
+	/* @data can contain both the path and query uri fields.
+	 * It can never be NULL but can have zero length. */
 	unsigned char *data;
 	int datalen;
 
+	/* @post can contain some special encoded form data, used internally
+	 * to make form data handling more effecient. The data is marked by
+	 * POST_CHAR in the uri string. */
 	unsigned char *post;
 };
 
 
-/* Initializes the members of the uri struct, as they are encountered. If
- * an uri component is recognized both it's length and starting point is
+/* Initializes the members of the uri struct, as they are encountered.
+ * If an uri component is recognized both it's length and starting point is
  * set. */
 /* Returns 1 if parsing went well or 0 if some error was found. */
 int parse_uri(struct uri *uri, unsigned char *uristring);

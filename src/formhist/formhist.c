c7b2f2ac7ba3cc2e5373fdb6546e0d4a0948c16e
Laurent Monin <zas@norz.org>
2003-08-22 10:38:50 +0000

    Minor cleanup and bugfixes (potential memleaks).

--- src/formhist/formhist.c
+++ src/formhist/formhist.c
@@ -1,5 +1,5 @@
 /* Implementation of a login manager for HTML forms */
-/* $Id: formhist.c,v 1.24 2003/08/22 10:11:04 zas Exp $ */
+/* $Id: formhist.c,v 1.25 2003/08/22 10:38:50 zas Exp $ */
 
 /* TODO: Remember multiple login for the same form
  * TODO: Password manager GUI (here?) */
@@ -24,6 +24,7 @@
 #include "util/string.h"
 #include "viewer/text/form.h"
 
+#define FORMHIST_FILENAME		"password"
 
 INIT_LIST_HEAD(saved_forms);
 
@@ -37,7 +38,7 @@ load_saved_forms(void)
        	unsigned char *file;
 	FILE *f;
 
-	file = straconcat(elinks_home, "password", NULL);
+	file = straconcat(elinks_home, FORMHIST_FILENAME, NULL);
 	if (!file) return 0;
 
 	f = fopen(file, "r");
@@ -58,10 +59,11 @@ load_saved_forms(void)
 			return 0;
 		}
 
-		init_list(*form);
-		init_list(*form->submit);
 		form->url = stracpy(tmp);
 		if (!form->url) goto fail;
+
+		init_list(*form);
+		init_list(*form->submit);
 
 		while (safe_fgets(tmp, MAX_STR_LEN, f)) {
 			struct submitted_value *sv;
@@ -118,16 +120,17 @@ unsigned char *
 get_form_history_value(unsigned char *url, unsigned char *name)
 {
 	struct formsmem_data *form;
-	struct submitted_value *sv;
 
 	if (!loaded && !load_saved_forms()) return NULL;
 
 	foreach (form, saved_forms) {
-		if (strcmp(form->url, url)) continue;
+		if (!strcmp(form->url, url)) {
+			struct submitted_value *sv;
 
-		foreach (sv, *form->submit)
-			if (!strcmp(sv->name, name))
-				return sv->value;
+			foreach (sv, *form->submit)
+				if (!strcmp(sv->name, name))
+					return sv->value;
+		}
 	}
 
 	return NULL;
@@ -141,11 +144,12 @@ int
 form_already_saved(unsigned char *url, struct list_head *submit)
 {
 	struct formsmem_data *form;
-	struct submitted_value *sv, *savedsv;
 
 	if (!loaded && !load_saved_forms()) return 0;
 
 	foreach (form, saved_forms) {
+		struct submitted_value *sv, *savedsv;
+
 		if (strcmp(form->url, url)) continue;
 
 		savedsv = (struct submitted_value *) form->submit->next;
@@ -186,18 +190,15 @@ remember_form(struct formsmem_data *fmem_data)
 	if (!form) return 0;
 
 	form->submit = mem_alloc(sizeof(struct list_head));
-	if (!form->submit) {
-		mem_free(form);
-		return 0;
-	}
+	if (!form->submit) goto fail;
 
-	init_list(*form);
-	init_list(*form->submit);
 	form->url = stracpy(fmem_data->url);
 	if (!form->url) goto fail;
 
-	/* FIXME: i don't like "password" as name for this file. --Zas */
-	file = straconcat(elinks_home, "password", NULL);
+	init_list(*form);
+	init_list(*form->submit);
+
+	file = straconcat(elinks_home, FORMHIST_FILENAME, NULL);
 	if (!file) goto fail;
 
 	ssi = secure_open(file, 0177);
@@ -206,7 +207,7 @@ remember_form(struct formsmem_data *fmem_data)
 
 	/* We're going to save just <INPUT TYPE="text"> and
 	 * <INPUT TYPE="password"> */
-	foreach (sv, *fmem_data->submit)
+	foreach (sv, *fmem_data->submit) {
 		if ((sv->type == FC_TEXT) || (sv->type == FC_PASSWORD)) {
 			struct submitted_value *sv2;
 
@@ -228,12 +229,14 @@ remember_form(struct formsmem_data *fmem_data)
 
 			add_to_list_bottom(*form->submit, sv2);
 		}
+	}
 
 	add_to_list(saved_forms, form);
 
 	/* Write the list to password file */
 	foreach (tmpform, saved_forms) {
 		secure_fprintf(ssi, "%s\n", tmpform->url);
+
 		foreachback (sv, *tmpform->submit) {
 			unsigned char *encvalue = "";
 
@@ -248,6 +251,7 @@ remember_form(struct formsmem_data *fmem_data)
 
 			if (*encvalue) mem_free(encvalue);
 		}
+
 		secure_fputc(ssi, '\n');
 	}
 
@@ -261,35 +265,6 @@ fail:
 	return 0;
 }
 
-void free_form_in_list(struct formsmem_data* form);
-
-void
-done_form_history(void)
-{
-	struct formsmem_data *form;
-
-	foreach(form, saved_forms) {
-		free_form_in_list(form);
-	}
-	free_list(saved_forms);
-}
-
-void
-free_form(struct formsmem_data *form)
-{
-	struct submitted_value *sv;
-
-	if (form->url) mem_free(form->url);
-
-	foreachback (sv, *form->submit) {
-		if (sv->name) mem_free(sv->name);
-		if (sv->value) mem_free(sv->value);
-	}
-	free_list(*form->submit);
-        if (form->submit) mem_free(form->submit);
-        mem_free(form);
-}
-
 void
 free_form_in_list(struct formsmem_data *form)
 {
@@ -301,8 +276,28 @@ free_form_in_list(struct formsmem_data *form)
 		if (sv->name) mem_free(sv->name);
 		if (sv->value) mem_free(sv->value);
 	}
-	free_list(*form->submit);
-	if (form->submit) mem_free(form->submit);
+	if (form->submit) {
+		free_list(*form->submit);
+		mem_free(form->submit);
+	}
+}
+
+void
+done_form_history(void)
+{
+	struct formsmem_data *form;
+
+	foreach(form, saved_forms)
+		free_form_in_list(form);
+
+	free_list(saved_forms);
+}
+
+void
+free_form(struct formsmem_data *form)
+{
+	free_form_in_list(form);
+	mem_free(form);
 }
 
 struct list_head *

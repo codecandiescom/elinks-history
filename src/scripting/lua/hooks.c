4b73e3442bd523fc6b939f1016b9a631b98c282f
Jonas Fonseca <fonseca@diku.dk>
2003-09-22 23:02:36 +0000

    script_hook_goto_url(): Cleanup.

--- src/scripting/lua/hooks.c
+++ src/scripting/lua/hooks.c
@@ -1,5 +1,5 @@
 /* Lua scripting hooks */
-/* $Id: hooks.c,v 1.18 2003/09/22 21:54:47 jonas Exp $ */
+/* $Id: hooks.c,v 1.19 2003/09/22 23:02:36 jonas Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -24,6 +24,12 @@
  * generalization of scripting and moving everything Lua-related to lua/.
  * --pasky */
 
+static inline int
+str_event_code(unsigned char **retval, unsigned char *value)
+{
+	*retval = value;
+	return !!value;
+}
 
 /* Triggerred when user entered something into the goto URL dialog. */
 /* Returns NULL if the original @url should be followed, or dynamically
@@ -36,45 +42,44 @@ script_hook_goto_url(va_list ap)
 	unsigned char **returl = va_arg(ap, unsigned char **);
 	struct session *ses = va_arg(ap, struct session *);
 	unsigned char *url = va_arg(ap, unsigned char *);
-	int err;
-
-	*returl = NULL;
 
 	lua_getglobal(L, "goto_url_hook");
 	if (lua_isnil(L, -1)) {
 		lua_pop(L, 1);
-		return 0;
+		return str_event_code(returl, NULL);
 	}
 
 	lua_pushstring(L, url);
+	url = NULL;
+
 	if (!have_location(ses)) {
 		lua_pushnil(L);
 	} else {
 		lua_pushstring(L, cur_loc(ses)->vs.url);
 	}
 
-	if (prepare_lua(ses)) {
-		*returl = stracpy("");
-		return *returl ? 1 : 0;
-	}
-	err = lua_call(L, 2, 1);
-	finish_lua();
-	if (err) {
-		*returl = stracpy("");
-		return *returl ? 1 : 0;
+	if (!prepare_lua(ses)) {
+		int err = lua_call(L, 2, 1);
+
+		finish_lua();
+
+		if (err) {
+			url = "";
+
+		} else if (lua_isstring(L, -1)) {
+			url = (unsigned char *) lua_tostring(L, -1);
+
+		} else if (!lua_isnil(L, -1)) {
+			alert_lua_error("goto_url_hook must return a string or nil");
+
+		} else {
+			url = "";
+		}
+
+		lua_pop(L, 1);
 	}
 
-	if (lua_isstring(L, -1)) {
-		*returl = stracpy((unsigned char *) lua_tostring(L, -1));
-	} else if (!lua_isnil(L, -1)) {
-		alert_lua_error("goto_url_hook must return a string or nil");
-	} else {
-		*returl = stracpy("");
-	}
-
-	lua_pop(L, 1);
-
-	return *returl ? 1 : 0;
+	return str_event_code(returl, url ? stracpy(url) : NULL);
 }
 
 

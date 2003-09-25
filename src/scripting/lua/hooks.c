0a65f1322d9a2bcc44c87d3de92fda83388e4253
Jonas Fonseca <fonseca@diku.dk>
2003-09-25 00:41:45 +0000

    Rename variables and change hooks function for goto-url and follow-url to act like the original ones.

--- src/scripting/lua/hooks.c
+++ src/scripting/lua/hooks.c
@@ -1,5 +1,5 @@
 /* Lua scripting hooks */
-/* $Id: hooks.c,v 1.28 2003/09/23 14:03:06 pasky Exp $ */
+/* $Id: hooks.c,v 1.29 2003/09/25 00:41:45 jonas Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -40,7 +40,7 @@ static enum evhook_status
 script_hook_goto_url(va_list ap)
 {
 	lua_State *L = lua_state;
-	unsigned char **returl = va_arg(ap, unsigned char **);
+	unsigned char **new_url = va_arg(ap, unsigned char **);
 	struct session *ses = va_arg(ap, struct session *);
 	unsigned char *url = va_arg(ap, unsigned char *);
 	unsigned char *value = NULL;
@@ -48,7 +48,7 @@ script_hook_goto_url(va_list ap)
 	lua_getglobal(L, "goto_url_hook");
 	if (lua_isnil(L, -1)) {
 		lua_pop(L, 1);
-		return str_event_code(returl, NULL);
+		return str_event_code(new_url, url);
 	}
 
 	lua_pushstring(L, url);
@@ -63,18 +63,20 @@ script_hook_goto_url(va_list ap)
 		int err = lua_call(L, 2, 1);
 
 		finish_lua();
-		if (err) return str_event_code(returl, NULL);
+		if (err) return str_event_code(new_url, url);
 
 		if (lua_isstring(L, -1)) {
 			value = stracpy((unsigned char *) lua_tostring(L, -1));
 		} else if (!lua_isnil(L, -1)) {
 			alert_lua_error("goto_url_hook must return a string or nil");
+		} else {
+			value = url;
 		}
 
 		lua_pop(L, 1);
 	}
 
-	return str_event_code(returl, value);
+	return str_event_code(new_url, value);
 }
 
 
@@ -88,7 +90,7 @@ static enum evhook_status
 script_hook_follow_url(va_list ap)
 {
 	lua_State *L = lua_state;
-	unsigned char **returl = va_arg(ap, unsigned char **);
+	unsigned char **new_url = va_arg(ap, unsigned char **);
 	struct session *ses = va_arg(ap, struct session *);
 	unsigned char *url = va_arg(ap, unsigned char *);
 	unsigned char *value = NULL;
@@ -96,7 +98,7 @@ script_hook_follow_url(va_list ap)
 	lua_getglobal(L, "follow_url_hook");
 	if (lua_isnil(L, -1)) {
 		lua_pop(L, 1);
-		return str_event_code(returl, NULL);
+		return str_event_code(new_url, url);
 	}
 
 	lua_pushstring(L, url);
@@ -105,18 +107,20 @@ script_hook_follow_url(va_list ap)
 		int err = lua_call(L, 1, 1);
 
 		finish_lua();
-		if (err) return str_event_code(returl, NULL);
+		if (err) return str_event_code(new_url, url);
 
 		if (lua_isstring(L, -1)) {
 			value = stracpy((unsigned char *) lua_tostring(L, -1));
 		} else if (!lua_isnil(L, -1)) {
 			alert_lua_error("follow_url_hook must return a string or nil");
+		} else {
+			value = url;
 		}
 
 		lua_pop(L, 1);
 	}
 
-	return str_event_code(returl, value);
+	return str_event_code(new_url, value);
 }
 
 
@@ -131,31 +135,31 @@ static enum evhook_status
 script_hook_pre_format_html(va_list ap)
 {
 	lua_State *L = lua_state;
-	unsigned char **retval = va_arg(ap, unsigned char **);
+	unsigned char **new_html_src = va_arg(ap, unsigned char **);
 	struct session *ses = va_arg(ap, struct session *);
 	unsigned char *url = va_arg(ap, unsigned char *);
-	unsigned char *html = va_arg(ap, unsigned char *);
-	int *len = va_arg(ap, int *);
+	unsigned char *html_src = va_arg(ap, unsigned char *);
+	int *html_len = va_arg(ap, int *);
 	unsigned char *value = NULL;
 
 	lua_getglobal(L, "pre_format_html_hook");
 	if (lua_isnil(L, -1)) {
 		lua_pop(L, 1);
-		return str_event_code(retval, NULL);
+		return str_event_code(new_html_src, NULL);
 	}
 
 	lua_pushstring(L, url);
-	lua_pushlstring(L, html, *len);
+	lua_pushlstring(L, html_src, *html_len);
 
 	if (!prepare_lua(ses)) {
 		int err = lua_call(L, 2, 1);
 
 		finish_lua();
-		if (err) return str_event_code(retval, NULL);
+		if (err) return str_event_code(new_html_src, NULL);
 
 		if (lua_isstring(L, -1)) {
-			*len = lua_strlen(L, -1);
-			*retval = memacpy((unsigned char *) lua_tostring(L, -1), *len);
+			*html_len = lua_strlen(L, -1);
+			value = memacpy((unsigned char *) lua_tostring(L, -1), *html_len);
 		} else if (!lua_isnil(L, -1)) {
 			alert_lua_error("pre_format_html_hook must return a string or nil");
 		}
@@ -163,7 +167,7 @@ script_hook_pre_format_html(va_list ap)
 		lua_pop(L, 1);
 	}
 
-	return str_event_code(retval, value);
+	return str_event_code(new_html_src, value);
 }
 
 
@@ -185,14 +189,14 @@ static enum evhook_status
 script_hook_get_proxy(va_list ap)
 {
 	lua_State *L = lua_state;
-	unsigned char **retval = va_arg(ap, unsigned char **);
+	unsigned char **new_proxy_url = va_arg(ap, unsigned char **);
 	unsigned char *url = va_arg(ap, unsigned char *);
 	unsigned char *value = NULL;
 
 	lua_getglobal(L, "proxy_for_hook");
 	if (lua_isnil(L, -1)) {
 		lua_pop(L, 1);
-		return str_event_code(retval, NULL);
+		return str_event_code(new_proxy_url, NULL);
 	}
 
 	lua_pushstring(L, url);
@@ -200,7 +204,7 @@ script_hook_get_proxy(va_list ap)
 		int err = lua_call(L, 1, 1);
 
 		finish_lua();
-		if (err) return str_event_code(retval, NULL);
+		if (err) return str_event_code(new_proxy_url, NULL);
 
 		if (lua_isstring(L, -1)) {
 			value = stracpy((unsigned char *)lua_tostring(L, -1));
@@ -211,7 +215,7 @@ script_hook_get_proxy(va_list ap)
 		lua_pop(L, 1);
 	}
 
-	return str_event_code(retval, value);
+	return str_event_code(new_proxy_url, value);
 }
 
 

bd9131456c2841442f156873cb774842799f3502
Jonas Fonseca <fonseca@diku.dk>
2003-10-18 00:14:40 +0000

    Clear the bold bit. Fixes color weirdness.

--- src/terminal/color.c
+++ src/terminal/color.c
@@ -1,5 +1,5 @@
 /* Terminal color composing. */
-/* $Id: color.c,v 1.59 2003/10/17 23:52:18 jonas Exp $ */
+/* $Id: color.c,v 1.60 2003/10/18 00:14:40 jonas Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -227,7 +227,7 @@ set_term_color16(struct screen_char *schar, enum color_flags flags,
 #ifdef USE_256_COLORS
 	/* With 256 color support we use memcmp() when comparing color in
 	 * terminal/screen.c:add_char*() so we need to clear this byte. */
-	TERM_COLOR_FOREGROUND(schar->color) = fg;
+	TERM_COLOR_FOREGROUND(schar->color) = (fg & TERM_COLOR_MASK);
 	TERM_COLOR_BACKGROUND(schar->color) = bg;
 #else
 	schar->color[0] = (bg << 4 | fg);

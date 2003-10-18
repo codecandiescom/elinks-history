9f316bc1a89e1ab7d411b9c436ed54fef6ae0fee
Jonas Fonseca <fonseca@diku.dk>
2003-10-18 19:46:50 +0000

    Add configure fixup for broken autotool on FreeBSD. Fixes "ltconfig: you must specify a host type if you use `--no-verify'" reported by Dmitry Pinchukov <dpinch@viart.com.ua> and me.

--- autogen.sh
+++ autogen.sh
@@ -19,4 +19,20 @@ autoconf
 echo config.cache...
 rm -f config.cache
 
+# On at least some FreeBSD boxes the libtool installation will cause configure
+# to bail with the error
+#
+#	ltconfig: you must specify a host type if you use `--no-verify'
+#
+# Whether it is autoconf or libtool who is to blame I don't know. --jonas
+case "`libtool --version`" in
+	*1.3.4-freebsd-ports*)
+		echo "fixing broken libtool"
+		mv configure configure-
+		cat configure- | sed -e 's/--no-verify//' > configure
+		rm configure-
+		chmod +x configure
+	;;
+esac
+		
 echo done

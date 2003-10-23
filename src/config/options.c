497051fb8672c4a5c5fbb2164dc003a47ad332cc
Petr Baudis <pasky@ucw.cz>
2003-10-23 08:42:02 +0000

    As long as we assert right things.

--- src/config/options.c
+++ src/config/options.c
@@ -1,5 +1,5 @@
 /* Options variables manipulation core */
-/* $Id: options.c,v 1.336 2003/10/23 08:32:49 pasky Exp $ */
+/* $Id: options.c,v 1.337 2003/10/23 08:42:02 pasky Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -130,7 +130,7 @@ get_opt_rec(struct option *tree, unsigned char *name_)
 	if (tree && tree->flags & OPT_AUTOCREATE && !no_autocreate) {
 		struct option *template = get_opt_rec(tree, "_template_");
 
-		assertm(!template, "Requested %s should be autocreated but "
+		assertm(template, "Requested %s should be autocreated but "
 			"%.*s._template_ is missing!", name_, sep - name_,
 			name_);
 		if_assert_failed {

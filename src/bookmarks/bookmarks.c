a8316a2fbb7f7674bbb63afaada833b294a5f680
Petr Baudis <pasky@ucw.cz>
2003-10-25 12:02:10 +0000

    Call unregister_option_info() from done_bookmarks().

--- src/bookmarks/bookmarks.c
+++ src/bookmarks/bookmarks.c
@@ -1,5 +1,5 @@
 /* Internal bookmarks support */
-/* $Id: bookmarks.c,v 1.83 2003/10/25 10:42:47 pasky Exp $ */
+/* $Id: bookmarks.c,v 1.84 2003/10/25 12:02:10 pasky Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -38,32 +38,32 @@ int bookmarks_dirty = 0;
 
 /* Life functions */
 
+static struct option_info bookmark_options_info[] = {
+	INIT_OPT_TREE("", N_("Bookmarks"),
+		"bookmarks", 0,
+		N_("Bookmark options.")),
+
+#ifdef HAVE_LIBEXPAT
+	INIT_OPT_INT("bookmarks", N_("File format"),
+		"file_format", 0, 0, 1, 0,
+		N_("File format for bookmarks (affects both reading and saving):\n"
+		"0 is the default ELinks (Links 0.9x compatible) format\n"
+		"1 is XBEL universal XML bookmarks format (NO NATIONAL CHARS SUPPORT!)")),
+#else
+	INIT_OPT_INT("bookmarks", N_("File format"),
+		"file_format", 0, 0, 1, 0,
+		N_("File format for bookmarks (affects both reading and saving):\n"
+		"0 is the default ELinks (Links 0.9x compatible) format\n"
+		"1 is XBEL universal XML bookmarks format (NO NATIONAL CHARS SUPPORT!)"
+		"  (DISABLED)")),
+#endif
+
+	NULL_OPTION_INFO
+};
+
 void
 init_bookmarks(void)
 {
-	struct option_info bookmark_options_info[] = {
-		INIT_OPT_TREE("", N_("Bookmarks"),
-			"bookmarks", 0,
-			N_("Bookmark options.")),
-
-#ifdef HAVE_LIBEXPAT
-		INIT_OPT_INT("bookmarks", N_("File format"),
-			"file_format", 0, 0, 1, 0,
-			N_("File format for bookmarks (affects both reading and saving):\n"
-			"0 is the default ELinks (Links 0.9x compatible) format\n"
-			"1 is XBEL universal XML bookmarks format (NO NATIONAL CHARS SUPPORT!)")),
-#else
-		INIT_OPT_INT("bookmarks", N_("File format"),
-			"file_format", 0, 0, 1, 0,
-			N_("File format for bookmarks (affects both reading and saving):\n"
-			"0 is the default ELinks (Links 0.9x compatible) format\n"
-			"1 is XBEL universal XML bookmarks format (NO NATIONAL CHARS SUPPORT!)"
-			"  (DISABLED)")),
-#endif
-
-		NULL_OPTION_INFO
-	};
-
 	register_option_info(bookmark_options_info, config_options);
 
 	read_bookmarks();
@@ -95,6 +95,8 @@ done_bookmarks(void)
 	free_bookmarks(&bookmarks, &bookmark_box_items);
 	if (bm_last_searched_name) mem_free(bm_last_searched_name);
 	if (bm_last_searched_url) mem_free(bm_last_searched_url);
+
+	unregister_option_info(bookmark_options_info, config_options);
 }
 
 

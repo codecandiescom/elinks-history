26825ad2aaff1bc43eed576c9b56d2d35271c76a
Petr Baudis <pasky@ucw.cz>
2003-10-27 02:19:13 +0000

    Moved GPM mouse handling from os_dep.c to unix.c.

--- src/osdep/osdep.c
+++ src/osdep/osdep.c
@@ -1,5 +1,5 @@
 /* Features which vary with the OS */
-/* $Id: osdep.c,v 1.107 2003/10/27 02:08:40 pasky Exp $ */
+/* $Id: osdep.c,v 1.108 2003/10/27 02:19:13 pasky Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -29,10 +29,6 @@
 
 #if defined(HAVE_LIBGPM) && defined(HAVE_GPM_H)
 #define USE_GPM
-#endif
-
-#ifdef USE_GPM
-#include <gpm.h>
 #endif
 
 #ifdef HAVE_X11
@@ -555,97 +551,6 @@ elinks_cfmakeraw(struct termios *t)
 
 #if defined(USE_GPM) && defined(USE_MOUSE)
 
-struct gpm_mouse_spec {
-	int h;
-	void (*fn)(void *, unsigned char *, int);
-	void *data;
-};
-
-static void
-gpm_mouse_in(struct gpm_mouse_spec *gms)
-{
-	Gpm_Event gev;
-	struct term_event ev;
-
-	if (Gpm_GetEvent(&gev) <= 0) {
-		set_handlers(gms->h, NULL, NULL, NULL, NULL);
-		return;
-	}
-
-	ev.ev = EV_MOUSE;
-	ev.x = gev.x - 1;
-	ev.y = gev.y - 1;
-	if (gev.buttons & GPM_B_LEFT)
-		ev.b = B_LEFT;
-	else if (gev.buttons & GPM_B_MIDDLE)
-		ev.b = B_MIDDLE;
-	else if (gev.buttons & GPM_B_RIGHT)
-		ev.b = B_RIGHT;
-	else
-		return;
-
-	if (gev.type & GPM_DOWN)
-		ev.b |= B_DOWN;
-	else if (gev.type & GPM_UP)
-		ev.b |= B_UP;
-	else if (gev.type & GPM_DRAG)
-		ev.b |= B_DRAG;
-	else
-		return;
-
-	gms->fn(gms->data, (char *)&ev, sizeof(struct term_event));
-}
-
-int
-init_mouse(int cons)
-{
-	Gpm_Connect conn;
-
-	conn.eventMask = ~GPM_MOVE;
-	conn.defaultMask = GPM_MOVE;
-	conn.minMod = 0;
-	conn.maxMod = 0;
-
-	return Gpm_Open(&conn, cons);
-}
-
-int
-done_mouse(void)
-{
-	return Gpm_Close();
-}
-
-void *
-handle_mouse(int cons, void (*fn)(void *, unsigned char *, int),
-	     void *data)
-{
-	int h;
-	struct gpm_mouse_spec *gms;
-
-	h = init_mouse(cons, 0);
-	if (h < 0) return NULL;
-
-	gms = mem_alloc(sizeof(struct gpm_mouse_spec));
-	if (!gms) return NULL;
-	gms->h = h;
-	gms->fn = fn;
-	gms->data = data;
-	set_handlers(h, (void (*)(void *))gpm_mouse_in, NULL, NULL, gms);
-
-	return gms;
-}
-
-void
-unhandle_mouse(void *h)
-{
-	struct gpm_mouse_spec *gms = h;
-
-	if (!gms) return;
-
-	set_handlers(gms->h, NULL, NULL, NULL, NULL);
-	mem_free(gms);
-	done_mouse();
-}
 
 #elif !defined(USING_OS2_MOUSE)
 

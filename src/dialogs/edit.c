efdbff5e07c4bbe55351e47d6b19f5727cd03d2b
Jonas Fonseca <fonseca@diku.dk>
2003-11-09 11:16:33 +0000

    Move widget layouting to separate function. We now use not AL_NONE to figure out when to trim width.

--- src/dialogs/edit.c
+++ src/dialogs/edit.c
@@ -1,5 +1,5 @@
 /* Generic support for edit/search historyitem/bookmark dialog */
-/* $Id: edit.c,v 1.70 2003/11/09 03:55:39 jonas Exp $ */
+/* $Id: edit.c,v 1.71 2003/11/09 11:16:33 jonas Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -30,6 +30,29 @@ my_cancel_dialog(struct dialog_data *dlg_data, struct widget_data *widget_data)
 	return cancel_dialog(dlg_data, widget_data);
 }
 
+static inline void
+layout_widgets(struct terminal *term, struct widget_data *wdata, int widgets,
+	       int x, int *y, int w, int *rw)
+{
+	for (; widgets > 0; widgets--, wdata++) {
+		switch (wdata->widget->type) {
+		case WIDGET_FIELD:
+			dlg_format_field(term, wdata, x, y, w, rw, AL_LEFT);
+			(*y)++;
+			break;
+
+		/* We assume that the first button is part of the ending dialog
+		 * buttons. */
+		case WIDGET_BUTTON:
+			dlg_format_buttons(term, wdata, widgets,
+					   x, y, w, rw, AL_CENTER);
+			return;
+		default:
+			internal("Bad widget type or widget not supported.");
+		}
+	}
+}
+
 
 /* Called to setup the edit dialog */
 static void
@@ -40,25 +63,19 @@ add_dialog_layouter(struct dialog_data *dlg_data)
 	int rw = 0;
 	int y = -1, x = 0;
 
-	dlg_format_field(NULL, dlg_data->widgets_data, x, &y, w, &rw, AL_LEFT);
-	y++;
-	dlg_format_field(NULL, dlg_data->widgets_data + 1, x, &y, w, &rw, AL_LEFT);
-	y++;
-	dlg_format_buttons(NULL, dlg_data->widgets_data + 2, 2, x, &y, w, &rw, AL_CENTER);
+	layout_widgets(NULL, dlg_data->widgets_data, dlg_data->n,
+		       x, &y, w, &rw);
 
 	/* Update the width to respond to the required minimum width */
-	if (dlg_data->dlg->align == AL_BLOCK) w = rw;
+	if (dlg_data->dlg->align != AL_NONE) w = rw;
 
 	draw_dialog(dlg_data, w, y, AL_CENTER);
 
 	y = dlg_data->y + DIALOG_TB;
 	x = dlg_data->x + DIALOG_LB;
 
-	dlg_format_field(term, dlg_data->widgets_data, x, &y, w, NULL, AL_LEFT);
-	y++;
-	dlg_format_field(term, dlg_data->widgets_data + 1, x, &y, w, NULL, AL_LEFT);
-	y++;
-	dlg_format_buttons(term, dlg_data->widgets_data + 2, 3, x, &y, w, NULL, AL_CENTER);
+	layout_widgets(term, dlg_data->widgets_data, dlg_data->n,
+		       x, &y, w, NULL);
 }
 
 

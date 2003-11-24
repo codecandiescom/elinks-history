27aa3f748a0ab2d65218ebabdba07be10482a585
Petr Baudis <pasky@ucw.cz>
2003-11-24 22:58:32 +0000

    Introduced release_vs(), which unlocks view_state and destroy it if it becomes unused.

--- src/viewer/text/vs.h
+++ src/viewer/text/vs.h
@@ -1,7 +1,9 @@
-/* $Id: vs.h,v 1.9 2003/11/24 22:56:02 pasky Exp $ */
+/* $Id: vs.h,v 1.10 2003/11/24 22:58:32 pasky Exp $ */
 
 #ifndef EL__VIEWER_TEXT_VS_H
 #define EL__VIEWER_TEXT_VS_H
+
+#include "util/object.h"
 
 /* Crossdeps are evil. */
 struct document_view;
@@ -27,6 +29,12 @@ struct view_state {
 
 void init_vs(struct view_state *, unsigned char *, int);
 void destroy_vs(struct view_state *);
+#define release_vs(vs) \
+do { \
+	object_unlock(vs); \
+	if (!is_object_used(vs) destroy_vs(vs); \
+} while (0)
+
 void copy_vs(struct view_state *, struct view_state *);
 void check_vs(struct document_view *);
 

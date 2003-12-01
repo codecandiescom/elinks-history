fe8bf0d603d9bdb355907cbfb136cd4c89362b0d
Petr Baudis <pasky@ucw.cz>
2003-12-01 14:21:49 +0000

    lowlevel/ttime.* -> util/ttime.*

--- src/protocol/http/date.h
+++ src/protocol/http/date.h
@@ -1,9 +1,9 @@
-/* $Id: date.h,v 1.3 2003/09/21 12:02:52 zas Exp $ */
+/* $Id: date.h,v 1.4 2003/12/01 14:21:49 pasky Exp $ */
 
 #ifndef EL__PROTOCOL_HTTP_DATE_H
 #define EL__PROTOCOL_HTTP_DATE_H
 
-#include "lowlevel/ttime.h"
+#include "util/ttime.h"
 
 ttime parse_http_date(const unsigned char *);
 

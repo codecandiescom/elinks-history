5b4b4d1afb5e402c24f18dc21212a81e465a76bd
Petr Baudis <pasky@ucw.cz>
2003-12-01 14:02:28 +0000

    Substitute includes by necessary struct declarations.

--- src/viewer/dump/dump.h
+++ src/viewer/dump/dump.h
@@ -1,15 +1,15 @@
-/* $Id: dump.h,v 1.8 2003/10/31 22:18:16 pasky Exp $ */
+/* $Id: dump.h,v 1.9 2003/12/01 14:02:28 pasky Exp $ */
 
 #ifndef EL__VIEWER_DUMP_DUMP_H
 #define EL__VIEWER_DUMP_DUMP_H
 
-#include "sched/connection.h"
+struct download;
 
 void dump_end(struct download *, void *);
 void dump_start(unsigned char *);
 
-#include "document/html/renderer.h"
-#include "util/string.h"
+struct string;
+struct document;
 
 /* Adds the content of the document to the string line by line. */
 struct string *

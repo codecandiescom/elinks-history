e58433b602cfe32d2724f4afe6b5a6875bbdb705
Jonas Fonseca <fonseca@diku.dk>
2004-01-16 20:26:22 +0000

    Only dump link titles which are not empty.

--- src/viewer/dump/dump.c
+++ src/viewer/dump/dump.c
@@ -1,5 +1,5 @@
 /* Support for dumping to the file on startup (w/o bfu) */
-/* $Id: dump.c,v 1.80 2004/01/05 13:33:54 pasky Exp $ */
+/* $Id: dump.c,v 1.81 2004/01/16 20:26:22 jonas Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -316,7 +316,7 @@ fail:
 			if (!l->where)
 				continue;
 
-			if (l->title)
+			if (l->title && *l->title)
 				snprintf(buf, D_BUF, "%4d. %s\n\t%s\n",
 					 x + 1, l->title, l->where);
 			else

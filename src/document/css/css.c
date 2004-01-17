bd1d7bff9b5fd7ba7c9b18dd05e020f8837ccf64
Petr Baudis <pasky@ucw.cz>
2004-01-17 15:32:01 +0000

    Moved value parsing to separate functions.

--- src/document/css/css.c
+++ src/document/css/css.c
@@ -1,5 +1,5 @@
 /* CSS micro-engine */
-/* $Id: css.c,v 1.14 2004/01/17 15:25:56 pasky Exp $ */
+/* $Id: css.c,v 1.15 2004/01/17 15:32:01 pasky Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -36,6 +36,137 @@ static enum css_decl_valtype prop2valtype[CSS_DP_LAST] = {
 };
 
 
+typedef int (*css_value_parser_t)(union css_decl_value *value,
+				  unsigned char **string);
+
+static int
+css_parse_color_value(union css_decl_value *value, unsigned char **string)
+{
+	int pos;
+
+	/* Long live spaghetti code! Well, whatever, someone will come
+	 * by and clean this up. I hope. Er... of course someone will!
+	 * --pasky */
+
+	/* TODO: Generic parser for "functions" in the declarations.
+	 * --pasky */
+
+	if (!strncasecmp(*string, "rgb(", 4)) {
+		/* RGB function */
+		unsigned char *nstring;
+		int part;
+
+		/* FIXME: We should handle the % values as floats. */
+
+		(*string) += 4;
+		skip_whitespace(*string);
+		part = strtol(*string, (char **) &nstring, 10);
+		if (*string == nstring) {
+			return 0;
+		}
+		*string = nstring;
+		skip_whitespace(*string);
+		if (**string == '%') {
+			part *= 255; part /= 100;
+			(*string)++;
+		}
+		if (part > 255) part = 255;
+		value->color |= part << 16;
+		skip_whitespace(*string);
+		if (**string != ',') {
+			return 0;
+		}
+		(*string)++;
+
+		skip_whitespace(*string);
+		part = strtol(*string, (char **) &nstring, 10);
+		if (*string == nstring) {
+			return 0;
+		}
+		*string = nstring;
+		skip_whitespace(*string);
+		if (**string == '%') {
+			part *= 255; part /= 100;
+			(*string)++;
+		}
+		if (part > 255) part = 255;
+		value->color |= part << 8;
+		skip_whitespace(*string);
+		if (**string != ',') {
+			return 0;
+		}
+		(*string)++;
+
+		skip_whitespace(*string);
+		part = strtol(*string, (char **) &nstring, 10);
+		if (*string == nstring) {
+			return 0;
+		}
+		*string = nstring;
+		skip_whitespace(*string);
+		if (**string == '%') {
+			part *= 255; part /= 100;
+			(*string)++;
+		}
+		if (part > 255) part = 255;
+		value->color |= part;
+		skip_whitespace(*string);
+		if (**string != ')') {
+			return 0;
+		}
+		(*string)++;
+
+		return 1;
+	}
+
+	/* Just a color value we already know how to parse. */
+	
+	pos = strcspn(*string, ",; \t\r\n");
+	if (decode_color(*string, pos, &value->color) < 0) {
+		return 0;
+	}
+	string += pos;
+	return 1;
+}
+
+static int
+css_parse_font_attribute_value(union css_decl_value *value,
+				unsigned char **string)
+{
+	unsigned char *nstring;
+	int weight;
+
+	skip_whitespace(*string);
+	if (!strlcasecmp(*string, -1, "bold", 4)) {
+		(*string) += 4;
+		value->font_attribute |= AT_BOLD;
+		return 1;
+	}
+
+	/* TODO: Comma separated list of weights?! */
+	weight = strtol(*string, (char **) &nstring, 10);
+	if (*string == nstring) {
+		return 0;
+	}
+
+	*string = nstring;
+	/* The font weight(s) have values between 100 to 900.  These
+	 * values form an ordered sequence, where each number indicates
+	 * a weight that is at least as dark as its predecessor.
+	 *
+	 * normal -> Same as '400'.  bold Same as '700'.
+	 */
+	int_bounds(&weight, 100, 900);
+	if (weight >= 700) value->font_attribute |= AT_BOLD;
+	return 1;
+}
+
+static css_value_parser_t css_value_parsers[CSS_DV_LAST] = {
+	/* CSS_DV_NONE */		NULL,
+	/* CSS_DV_COLOR */		css_parse_color_value,
+	/* CSS_DV_FONT_ATTRIBUTE */	css_parse_font_attribute_value,
+};
+
 /* This function takes a value of a specified type from the given string and
  * converts it to a reasonable {struct css_property}-ready form. */
 /* It returns positive integer upon success, zero upon parse error, and moves
@@ -44,129 +175,15 @@ static int
 css_parse_value(enum css_decl_valtype valtype, union css_decl_value *value,
 		unsigned char **string)
 {
-	assert(string && valtype != CSS_DV_NONE && valtype < CSS_DV_LAST);
+	assert(string && value && valtype < CSS_DV_LAST);
+	assert(css_value_parsers[valtype]);
 
 	/* Skip the leading whitespaces. */
 	skip_whitespace(*string);
 
-	if (valtype == CSS_DV_COLOR) {
-		int pos;
-
-		/* Long live spaghetti code! Well, whatever, someone will come
-		 * by and clean this up. I hope. Er... of course someone will!
-		 * --pasky */
-
-		/* TODO: Generic parser for "functions" in the declarations.
-		 * --pasky */
-
-		if (!strncasecmp(*string, "rgb(", 4)) {
-			/* RGB function */
-			unsigned char *nstring;
-			int part;
-
-			/* FIXME: We should handle the % values as floats. */
-
-			(*string) += 4;
-			skip_whitespace(*string);
-			part = strtol(*string, (char **) &nstring, 10);
-			if (*string == nstring) {
-				return 0;
-			}
-			*string = nstring;
-			skip_whitespace(*string);
-			if (**string == '%') {
-				part *= 255; part /= 100;
-				(*string)++;
-			}
-			if (part > 255) part = 255;
-			value->color |= part << 16;
-			skip_whitespace(*string);
-			if (**string != ',') {
-				return 0;
-			}
-			(*string)++;
-
-			skip_whitespace(*string);
-			part = strtol(*string, (char **) &nstring, 10);
-			if (*string == nstring) {
-				return 0;
-			}
-			*string = nstring;
-			skip_whitespace(*string);
-			if (**string == '%') {
-				part *= 255; part /= 100;
-				(*string)++;
-			}
-			if (part > 255) part = 255;
-			value->color |= part << 8;
-			skip_whitespace(*string);
-			if (**string != ',') {
-				return 0;
-			}
-			(*string)++;
-
-			skip_whitespace(*string);
-			part = strtol(*string, (char **) &nstring, 10);
-			if (*string == nstring) {
-				return 0;
-			}
-			*string = nstring;
-			skip_whitespace(*string);
-			if (**string == '%') {
-				part *= 255; part /= 100;
-				(*string)++;
-			}
-			if (part > 255) part = 255;
-			value->color |= part;
-			skip_whitespace(*string);
-			if (**string != ')') {
-				return 0;
-			}
-			(*string)++;
-
-			return 1;
-		}
-
-		/* Just a color value we already know how to parse. */
-		
-		pos = strcspn(*string, ",; \t\r\n");
-		if (decode_color(*string, pos, &value->color) < 0) {
-			return 0;
-		}
-		string += pos;
-		return 1;
-	} else if (valtype == CSS_DV_FONT_ATTRIBUTE) {
-		unsigned char *nstring;
-		int weight;
-
-		skip_whitespace(*string);
-		if (!strlcasecmp(*string, -1, "bold", 4)) {
-			(*string) += 4;
-			value->font_attribute |= AT_BOLD;
-			return 1;
-		}
-
-		/* TODO: Comma separated list of weights?! */
-		weight = strtol(*string, (char **) &nstring, 10);
-		if (*string == nstring) {
-			return 0;
-		}
-
-		*string = nstring;
-		/* The font weight(s) have values between 100 to 900.  These
-		 * values form an ordered sequence, where each number indicates
-		 * a weight that is at least as dark as its predecessor.
-		 *
-		 * normal -> Same as '400'.  bold Same as '700'.
-		 */
-		int_bounds(&weight, 100, 900);
-		if (weight >= 700) value->font_attribute |= AT_BOLD;
-		return 1;
-	}
-
-	INTERNAL("Uh-oh. I the %d am not supposed to be here.", valtype);
-	return 0;
+	return css_value_parsers[valtype](value, string);
 }
+
 
 struct css_property_info {
 	unsigned char *name;

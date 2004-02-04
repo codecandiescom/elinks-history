2e71e1dc83e00f793f29063cabdaf661d8249882
Petr Baudis <pasky@ucw.cz>
2004-02-04 12:51:24 +0000

    Trailing whitespaces police strikes.

--- src/dialogs/exmode.h
+++ src/dialogs/exmode.h
@@ -1,11 +1,11 @@
-/* $Id: exmode.h,v 1.2 2004/01/26 05:50:17 jonas Exp $ */
+/* $Id: exmode.h,v 1.3 2004/02/04 12:51:24 pasky Exp $ */
 
 #ifndef EL__DIALOGS_EXMODE_H
 #define EL__DIALOGS_EXMODE_H
 
 struct session;
 struct input_history;
- 
+
 extern struct input_history exmode_history;
 
 void exmode_start(struct session *ses);

f3a2c4723cc11400d26e22e66cd056f1c72bbd35
Jonas Fonseca <fonseca@diku.dk>
2004-04-03 13:51:58 +0000

    Re-rename cache entry variables to cached to improve grepability.

--- src/cache/dialogs.c
+++ src/cache/dialogs.c
@@ -1,5 +1,5 @@
 /* Cache-related dialogs */
-/* $Id: dialogs.c,v 1.53 2004/04/03 13:11:58 jonas Exp $ */
+/* $Id: dialogs.c,v 1.54 2004/04/03 13:51:58 jonas Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -50,66 +50,66 @@ static unsigned char *
 get_cache_entry_info(struct listbox_item *item, struct terminal *term,
 		     enum listbox_info listbox_info)
 {
-	struct cache_entry *cache = item->udata;
+	struct cache_entry *cached = item->udata;
 	struct string msg;
 
 	if (listbox_info == LISTBOX_URI)
-		return get_uri_string(cache->uri, ~0);
+		return get_uri_string(cached->uri, ~0);
 
 	if (!init_string(&msg)) return NULL;
 
 	add_to_string(&msg, _("URL", term));
 	add_to_string(&msg, ": ");
-	add_uri_to_string(&msg, cache->uri, ~(URI_PASSWORD | URI_POST));
+	add_uri_to_string(&msg, cached->uri, ~(URI_PASSWORD | URI_POST));
 
-	if (cache->redirect) {
+	if (cached->redirect) {
 		add_format_to_string(&msg, "\n%s: %s", _("Redirect", term),
-						cache->redirect);
+						cached->redirect);
 
-		if (cache->redirect_get) {
+		if (cached->redirect_get) {
 			add_to_string(&msg, " (GET)");
 		}
 	}
 
-	add_format_to_string(&msg, "\n%s: %d", _("Size", term), cache->length);
+	add_format_to_string(&msg, "\n%s: %d", _("Size", term), cached->length);
 	add_format_to_string(&msg, "\n%s: %d", _("Loaded size", term),
-						cache->data_size);
-	if (cache->last_modified) {
+						cached->data_size);
+	if (cached->last_modified) {
 		add_format_to_string(&msg, "\n%s: %s", _("Last modified", term),
-				     cache->last_modified);
+				     cached->last_modified);
 	}
-	if (cache->etag) {
+	if (cached->etag) {
 		add_format_to_string(&msg, "\n%s: %s", "ETag",
-						cache->etag);
+						cached->etag);
 	}
-	if (cache->ssl_info) {
+	if (cached->ssl_info) {
 		add_format_to_string(&msg, "\n%s: %s", _("SSL Cipher", term),
-						cache->ssl_info);
+						cached->ssl_info);
 	}
-	if (cache->encoding_info) {
+	if (cached->encoding_info) {
 		add_format_to_string(&msg, "\n%s: %s", _("Encoding", term),
-						cache->encoding_info);
+						cached->encoding_info);
 	}
 
-	if (cache->incomplete || !cache->valid) {
+	if (cached->incomplete || !cached->valid) {
 		add_char_to_string(&msg, '\n');
 		add_to_string(&msg, _("Flags", term));
 		add_to_string(&msg, ": ");
-		if (cache->incomplete) {
+		if (cached->incomplete) {
 			add_to_string(&msg, _("incomplete", term));
 			add_char_to_string(&msg, ' ');
 		}
-		if (!cache->valid) add_to_string(&msg, _("invalid", term));
+		if (!cached->valid) add_to_string(&msg, _("invalid", term));
 	}
 
 #ifdef CONFIG_DEBUG
-	add_format_to_string(&msg, "\n%s: %d", "Refcount", cache->refcount);
+	add_format_to_string(&msg, "\n%s: %d", "Refcount", cached->refcount);
 	add_format_to_string(&msg, "\n%s: %d", _("ID tag", term),
-						cache->id_tag);
+						cached->id_tag);
 
-	if (cache->head && *cache->head) {
+	if (cached->head && *cached->head) {
 		add_format_to_string(&msg, "\n%s:\n\n%s", _("Header", term),
-				     cache->head);
+				     cached->head);
 	}
 #endif
 
@@ -125,11 +125,11 @@ can_delete_cache_entry(struct listbox_item *item)
 static void
 delete_cache_entry_item(struct listbox_item *item, int last)
 {
-	struct cache_entry *cache_entry = item->udata;
+	struct cache_entry *cached = item->udata;
 
-	assert(!is_object_used(cache_entry));
+	assert(!is_object_used(cached));
 
-	delete_cache_entry(cache_entry);
+	delete_cache_entry(cached);
 }
 
 static struct listbox_ops cache_entry_listbox_ops = {

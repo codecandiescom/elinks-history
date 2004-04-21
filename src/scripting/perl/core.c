e9dd49a381cd799a6c89e90d3c401c14bf8b46b8
Laurent Monin <zas@norz.org>
2004-04-21 08:59:29 +0000

    Fix usage of PERL_SYS_INIT3() and PERL_SYS_TERM() macros, prevent unused variables warnings, cleanup + comments.

--- src/scripting/perl/core.c
+++ src/scripting/perl/core.c
@@ -1,5 +1,5 @@
 /* Perl scripting engine */
-/* $Id: core.c,v 1.3 2004/04/16 09:23:40 zas Exp $ */
+/* $Id: core.c,v 1.4 2004/04/21 08:59:29 zas Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -18,7 +18,10 @@
 #define PERL_HOOKS_FILENAME	"hooks.pl"
 
 PerlInterpreter *my_perl;
+
+#ifdef PERL_SYS_INIT3
 extern char **environ;
+#endif
 
 static char *
 get_global_hook_file(void)
@@ -56,26 +59,42 @@ static void
 cleanup_perl(struct module *module)
 {
 	precleanup_perl(module);
+#ifdef PERL_SYS_TERM
 	PERL_SYS_TERM();
+#endif
 }
 
 static void
 init_perl(struct module *module)
 {
-	char *hook_global = get_global_hook_file();
-	char *hook_local = get_local_hook_file();
-	char *global_argv[] = { "", hook_global};
-	char *local_argv[] = { "", hook_local};
-/*	char *my_argv[] = { "" };	*/
-/*	int my_arc = 1;			*/
-	int err = 1;
+/* FIXME: it seems that some systems like OS/2 requires PERL_SYS_INIT3
+ * and PERL_SYS_TERM to open/close the same block, at least regarding some
+ * ml messages.
+ * 
+ * Is passing @environ strictly needed ? --Zas */
+
+#ifdef PERL_SYS_INIT3	/* this macro may not be defined, it depends on system. */
+	char *my_argv[] = { NULL };
+	int my_argc = 0;
+
+	my_argv[my_argc++] = "";	/* hack to prevent unused variables warnings. */
 
 	PERL_SYS_INIT3(&my_argc, &my_argv, &environ);
+#endif
+
 	my_perl = perl_alloc();
 	if (my_perl) {
+		char *hook_global = get_global_hook_file();
+		char *hook_local = get_local_hook_file();
+		char *global_argv[] = { "", hook_global};
+		char *local_argv[] = { "", hook_local};
+		int err = 1;
+
 		perl_construct(my_perl);
-		if (hook_local) err = perl_parse(my_perl, NULL, 2, local_argv, NULL);
-		else if (hook_global) err = perl_parse(my_perl, NULL, 2, global_argv, NULL);
+		if (hook_local)
+			err = perl_parse(my_perl, NULL, 2, local_argv, NULL);
+		else if (hook_global)
+			err = perl_parse(my_perl, NULL, 2, global_argv, NULL);
 		PL_exit_flags |= PERL_EXIT_DESTRUCT_END;
 		if (!err) err = perl_run(my_perl);
 		if (err) precleanup_perl(module);

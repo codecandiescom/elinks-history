73e39a7e28a4db5dd6b0a7d8fa00eac7c347a245
Laurent Monin <zas@norz.org>
2004-04-26 12:51:29 +0000

    Perl-specific code was moved to appropriate local functions, @err was renamed to non-misleading @count, dSP and PUSHMARK are needed even if no parameter is passed (hook_quit case, conform to perlcall doc), tidyup.

--- src/scripting/perl/hooks.c
+++ src/scripting/perl/hooks.c
@@ -1,5 +1,5 @@
 /* Perl scripting hooks */
-/* $Id: hooks.c,v 1.7 2004/04/25 05:58:17 witekfl Exp $ */
+/* $Id: hooks.c,v 1.8 2004/04/26 12:51:29 zas Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -22,37 +22,32 @@
 /* The events that will trigger the functions below and what they are expected
  * to do is explained in doc/events.txt */
 
-static enum evhook_status
-script_hook_goto_url(va_list ap, void *data)
+static inline void
+do_script_hook_goto_url(struct session *ses, unsigned char **url)
 {
-	if (!my_perl) return EHS_NEXT;
-	{
-		unsigned char **url = va_arg(ap, unsigned char **);
-		struct session *ses = va_arg(ap, struct session *);
-		unsigned char *new_url = NULL;
-		STRLEN n_a;
-		int err;
-		dSP;
+	int count;
+	dSP;	/* Keep in variables declaration block. */
 
-		if (*url == NULL) return EHS_NEXT;
-		ENTER;
-		SAVETMPS;
-		PUSHMARK(SP);
+	ENTER;
+	SAVETMPS;
 
-		my_XPUSHs(*url, strlen(*url));
+	PUSHMARK(SP);
+	my_XPUSHs(*url, strlen(*url));
+	if (!have_location(ses)) {
+		XPUSHs(sv_2mortal(newSV(0)));
+	} else {
+		unsigned char *uri = struri(cur_loc(ses)->vs.uri);
 
-		if (!have_location(ses)) {
-			XPUSHs(sv_2mortal(newSV(0)));
-		} else {
-			unsigned char *uri = struri(cur_loc(ses)->vs.uri);
+		my_XPUSHs(uri, strlen(uri));
+	}
+	PUTBACK;
 
-			my_XPUSHs(uri, strlen(uri));
-		}
-		PUTBACK;
-		err = call_pv("goto_url_hook", G_EVAL | G_SCALAR);
-		if (SvTRUE(ERRSV)) err = 0;
-		SPAGAIN;
-		if (err == 1) new_url = POPpx;
+	count = call_pv("goto_url_hook", G_EVAL | G_SCALAR);
+	if (SvTRUE(ERRSV)) count = 0;	/* FIXME: error message ? */
+	SPAGAIN;
+	if (count == 1) {
+		STRLEN n_a;	/* Used by POPpx macro. */
+		unsigned char *new_url = POPpx;
 
 		if (new_url) {
 			unsigned char *n = memacpy(new_url, n_a);
@@ -62,139 +57,178 @@ script_hook_goto_url(va_list ap, void *data)
 				*url = n;
 			}
 		}
-		PUTBACK;
-		FREETMPS;
-		LEAVE;
 	}
+
+	PUTBACK;
+	FREETMPS;
+	LEAVE;
+}
+
+static enum evhook_status
+script_hook_goto_url(va_list ap, void *data)
+{
+	unsigned char **url = va_arg(ap, unsigned char **);
+	struct session *ses = va_arg(ap, struct session *);
+
+	if (my_perl && *url)
+		do_script_hook_goto_url(ses, url);
+
 	return EHS_NEXT;
+}
+
+static inline void
+do_script_hook_follow_url(unsigned char **url)
+{
+	int count;
+	dSP;	/* Keep in variables declaration block. */
+
+	ENTER;
+	SAVETMPS;
+
+	PUSHMARK(SP);
+	my_XPUSHs(*url, strlen(*url));
+	PUTBACK;
+
+	count = call_pv("follow_url_hook", G_EVAL | G_SCALAR);
+	if (SvTRUE(ERRSV)) count = 0;	/* FIXME: error message ? */
+	SPAGAIN;
+	if (count == 1) {
+		STRLEN n_a;	/* Used by POPpx macro. */
+		unsigned char *new_url = POPpx;
+
+		if (new_url) {
+			unsigned char *n = memacpy(new_url, n_a);
+
+			if (n) {
+				mem_free(*url);
+				*url = n;
+			}
+		}
+	}
+
+	PUTBACK;
+	FREETMPS;
+	LEAVE;
 }
 
 static enum evhook_status
 script_hook_follow_url(va_list ap, void *data)
 {
-	if (!my_perl) return EHS_NEXT;
-	{
-		unsigned char **url = va_arg(ap, unsigned char **);
-		unsigned char *new_url = NULL;
-		STRLEN n_a;
-		int err;
-		dSP;
+	unsigned char **url = va_arg(ap, unsigned char **);
 
-		if (*url == NULL) return EHS_NEXT;
+	if (my_perl && *url)
+		do_script_hook_follow_url(url);
 
-		ENTER;
-		SAVETMPS;
-		PUSHMARK(SP);
-
-		my_XPUSHs(*url, strlen(*url));
-		PUTBACK;
-		err = call_pv("follow_url_hook", G_EVAL | G_SCALAR);
-		if (SvTRUE(ERRSV)) err = 0;
-		SPAGAIN;
-		if (err == 1) new_url = POPpx;
-
-		if (new_url) {
-			unsigned char *n = memacpy(new_url, n_a);
-
-			if (n) {
-				mem_free(*url);
-				*url = n;
-			}
-		}
-		PUTBACK;
-		FREETMPS;
-		LEAVE;
-	}
 	return EHS_NEXT;
 }
 
-static enum evhook_status
-script_hook_pre_format_html(va_list ap, void *data)
+static inline void
+do_script_hook_pre_format_html(unsigned char *url, unsigned char **html,
+			       int *html_len)
 {
-	if (!my_perl) return EHS_NEXT;
-	{
-		unsigned char **html = va_arg(ap, unsigned char **);
-		int *html_len = va_arg(ap, int *);
-		unsigned char *url;
-		unsigned char *new_html = NULL;
-		struct session *ses;
-		STRLEN n_a;
-		int err;
-		dSP;
+	int count;
+	dSP;	/* Keep in variables declaration block. */
 
-		ses = va_arg(ap, struct session *);
-		url = va_arg(ap, unsigned char *);
+	ENTER;
+	SAVETMPS;
 
-		if (*html == NULL || *html_len == 0) return EHS_NEXT;
+	PUSHMARK(SP);
+	my_XPUSHs(url, strlen(url));
+	my_XPUSHs(*html, *html_len);
+	PUTBACK;
 
-		ENTER;
-		SAVETMPS;
-		PUSHMARK(SP);
-
-		my_XPUSHs(url, strlen(url));
-		my_XPUSHs(*html, *html_len);
-		PUTBACK;
-		err = call_pv("pre_format_html_hook", G_EVAL | G_SCALAR);
-		if (SvTRUE(ERRSV)) err = 0;
-		SPAGAIN;
-		if (err == 1) new_html = POPpx;
+	count = call_pv("pre_format_html_hook", G_EVAL | G_SCALAR);
+	if (SvTRUE(ERRSV)) count = 0;	/* FIXME: error message ? */
+	SPAGAIN;
+	if (count == 1) {
+		STRLEN n_a;	/* Used by POPpx macro. */
+		unsigned char *new_html = POPpx;
 
 		if (new_html) {
 			*html_len = n_a;
 			*html = memacpy(new_html, *html_len);
 		}
-		PUTBACK;
-		FREETMPS;
-		LEAVE;
 	}
+
+	PUTBACK;
+	FREETMPS;
+	LEAVE;
+}
+
+static enum evhook_status
+script_hook_pre_format_html(va_list ap, void *data)
+{
+	unsigned char **html = va_arg(ap, unsigned char **);
+	int *html_len = va_arg(ap, int *);
+	struct session *ses = va_arg(ap, struct session *);
+	unsigned char *url = va_arg(ap, unsigned char *);
+
+	if (my_perl && ses && url && *html && *html_len)
+		do_script_hook_pre_format_html(url, html, html_len);
+
 	return EHS_NEXT;
 }
 
-/* The Lua function can return:
- *  - "PROXY:PORT" to use the specified proxy
- *  - ""           to not use any proxy
- *  - nil          to use the default proxies */
-static enum evhook_status
-script_hook_get_proxy(va_list ap, void *data)
+static inline void
+do_script_hook_get_proxy(unsigned char **new_proxy_url, unsigned char *url)
 {
-	if (!my_perl) return EHS_NEXT;
-	{
-		unsigned char **new_proxy_url = va_arg(ap, unsigned char **);
-		unsigned char *url = va_arg(ap, unsigned char *);
-		unsigned char *new_url = NULL;
-		STRLEN n_a;
-		int err;
-		dSP;
+	int count;
+	dSP;	/* Keep in variables declaration block. */
 
-		if (*new_proxy_url == NULL) return EHS_NEXT;
+	ENTER;
+	SAVETMPS;
 
-		ENTER;
-		SAVETMPS;
-		PUSHMARK(SP);
+	PUSHMARK(SP);
+	my_XPUSHs(url, strlen(url));
+	PUTBACK;
 
-		my_XPUSHs(url, strlen(url));
-		PUTBACK;
-		err = call_pv("proxy_for_hook", G_EVAL | G_SCALAR);
-		if (SvTRUE(ERRSV)) err = 0;
-		SPAGAIN;
-		if (err == 1) new_url = POPpx;
+	count = call_pv("proxy_for_hook", G_EVAL | G_SCALAR);
+	if (SvTRUE(ERRSV)) count = 0;	/* FIXME: error message ? */
+	SPAGAIN;
+	if (count == 1) {
+		STRLEN n_a;	/* Used by POPpx macro. */
+		unsigned char *new_url = POPpx;
 
 		if (new_url) {
 			*new_proxy_url = memacpy(new_url, n_a);
 		} else {
 			*new_proxy_url = NULL;
 		}
-		PUTBACK;
-		FREETMPS;
-		LEAVE;
 	}
+
+	PUTBACK;
+	FREETMPS;
+	LEAVE;
+}
+
+static enum evhook_status
+script_hook_get_proxy(va_list ap, void *data)
+{
+	unsigned char **new_proxy_url = va_arg(ap, unsigned char **);
+	unsigned char *url = va_arg(ap, unsigned char *);
+
+	if (my_perl && new_proxy_url && *new_proxy_url && url)
+		do_script_hook_get_proxy(new_proxy_url, url);
+
 	return EHS_NEXT;
+}
+
+static inline void
+do_script_hook_quit(void)
+{
+	dSP;
+
+	PUSHMARK(SP);
+
+	call_pv("quit_hook", G_EVAL | G_DISCARD | G_NOARGS);
 }
 
 static enum evhook_status
 script_hook_quit(va_list ap, void *data)
 {
-	if (my_perl) call_pv("quit_hook", G_EVAL | G_DISCARD | G_NOARGS);
+	if (my_perl)
+		do_script_hook_quit();
+
 	return EHS_NEXT;
 }
 

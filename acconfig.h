9756bf548ae22431356d9a4996d5954b62a0893d
Jonas Fonseca <fonseca@diku.dk>
2004-04-30 01:06:46 +0000

    Add EL_CONFIG_DEPENDS() macro for handling scripting and SSL CONFIG_* setup.

--- acconfig.h
+++ acconfig.h
@@ -1,25 +1,5 @@
 
 @BOTTOM@
-/* Define to 1 if we should use scripting */
-#undef CONFIG_SCRIPTING
-
-/* Define to 1 if we should use Guile */
-#undef CONFIG_GUILE
-
-/* Define to 1 if we should use Perl */
-#undef CONFIG_PERL
-
-/* Define to 1 if we should use Lua */
-#undef CONFIG_LUA
-
-/* Define to 1 if we should use SSL */
-#undef CONFIG_SSL
-
-/* Define to 1 if we should use OpenSSL */
-#undef CONFIG_OPENSSL
-
-/* Define to 1 if we should use GNUTLS */
-#undef CONFIG_GNUTLS
 
 /* Directory containing default config */
 #undef CONFDIR

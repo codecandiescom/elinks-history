86a10f7d34dd8880a2630477026063dcd9cd50e3
Jonas Fonseca <fonseca@diku.dk>
2004-04-30 14:30:59 +0000

    Since we now rely on autoconf 2.13 lets also do these *DIR defines the right way.

--- acconfig.h
+++ acconfig.h
@@ -1,13 +1,4 @@
 
 @BOTTOM@
 
-/* Directory containing default config */
-#undef CONFDIR
-
-/* Directory containing locales */
-#undef LOCALEDIR
-
-/* Directory containing libraries */
-#undef LIBDIR
-
 #include "../feature.h"

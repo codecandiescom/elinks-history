b8bc80d8a4549d707f2d38a7d03528bdbda9428a
Jonas Fonseca <fonseca@diku.dk>
2004-05-09 23:04:40 +0000

    Make finger protocol support optional. Maybe it should be disabled by default since I don't think so maybe people use it and it more serves as a protocol implementation reference.

--- src/protocol/finger.h
+++ src/protocol/finger.h
@@ -1,10 +1,14 @@
-/* $Id: finger.h,v 1.6 2004/05/07 17:27:46 jonas Exp $ */
+/* $Id: finger.h,v 1.7 2004/05/09 23:04:40 jonas Exp $ */
 
 #ifndef EL__PROTOCOL_FINGER_H
 #define EL__PROTOCOL_FINGER_H
 
 #include "protocol/protocol.h"
 
+#ifdef CONFIG_FINGER
 extern protocol_handler finger_protocol_handler;
+#else
+#define finger_protocol_handler NULL
+#endif
 
 #endif

a6cd961064c734c60d6936d3cf28f4d48bad70ab
Jonas Fonseca <fonseca@diku.dk>
2004-05-28 11:57:14 +0000

    Revert bad things.

--- autogen.sh
+++ autogen.sh
@@ -1,33 +1,17 @@
 #!/bin/sh
 
-export LANG=
-export LC_CTYPE=
-export LC_NUMERIC=
-export LC_TIME=
-export LC_COLLATE=
-export LC_MONETARY=
-export LC_MESSAGES=
-export LC_PAPER=
-export LC_NAME=
-export LC_ADDRESS=
-export LC_TELEPHONE=
-export LC_MEASUREMENT=
-export LC_IDENTIFICATION=
-export LC_ALL=
-
-
 echo acinclude.m4...
 echo "dnl This is automatically generated from m4/ files! Do not modify!" > acinclude.m4
 cat m4/*.m4 >> acinclude.m4
 
 echo aclocal...
-aclocal14
+aclocal
 
 echo autoheader...
 autoheader
 
 echo automake...
-automake14 -a -c
+automake -a -c
 
 echo autoconf...
 autoconf

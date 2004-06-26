b47a921cc9019bb1d245f82c5d3197733ae7882a
Jonas Fonseca <fonseca@diku.dk>
2004-06-26 14:35:38 +0000

    Use stack allocated strings.

--- src/cookies/parsetst.c
+++ src/cookies/parsetst.c
@@ -1,5 +1,5 @@
 /* Tool for testing of cookies string parser */
-/* $Id: parsetst.c,v 1.6 2004/01/01 15:36:18 jonas Exp $ */
+/* $Id: parsetst.c,v 1.7 2004/06/26 14:35:38 jonas Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -18,7 +18,7 @@ int
 main(int argc, char *argv[])
 {
 	struct cookie_str cstr;
-	unsigned char name[1024], value[1024];
+	unsigned char name[1024], value[1024], string[1024];
 
 	printf("This thing is for testing of cookies name-value pair parser.\n"
 	       "You probably do not want to mess with it :).\n");
@@ -26,14 +26,14 @@ main(int argc, char *argv[])
 	while (1) {
 		printf("Enter string (empty==quit): "); fflush(stdout);
 
-		cstr.str = malloc(1024);
-		fgets(cstr.str, 1024, stdin);
+		fgets(string, 1024, stdin);
 
-		cstr.str[strlen(cstr.str) - 1] = '\0'; /* Strip newline. */
-		if (!*cstr.str) return 0;
+		string[strlen(string) - 1] = '\0'; /* Strip newline. */
+		if (!*string) return 0;
 
+		cstr.str = string;
 		if (!parse_cookie_str(&cstr)) {
-			printf("ERROR while parsing '%s'!\n", cstr.str);
+			printf("ERROR while parsing '%s'!\n", string);
 			continue;
 		}
 
@@ -42,8 +42,7 @@ main(int argc, char *argv[])
 		memcpy(value, cstr.val_start, cstr.val_end - cstr.val_start);
 		value[cstr.val_end - cstr.val_start] = '\0';
 
-		printf("'%s' -> '%s' :: '%s'\n", cstr.str, name, value);
-		free(cstr.str);
+		printf("'%s' -> '%s' :: '%s'\n", string, name, value);
 	}
 }
 

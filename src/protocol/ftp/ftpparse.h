3eb99b792dfb10b53db46f21477acb832d5830c6
Laurent Monin <zas@norz.org>
2004-07-03 23:37:20 +0000

    Improve and reorder comments.

--- src/protocol/ftp/ftpparse.h
+++ src/protocol/ftp/ftpparse.h
@@ -1,22 +1,8 @@
 /* FTP directory parsing */
-/* $Id: ftpparse.h,v 1.7 2004/07/03 23:29:49 zas Exp $ */
+/* $Id: ftpparse.h,v 1.8 2004/07/03 23:37:20 zas Exp $ */
 
 #ifndef EL__PROTOCOL_FTP_FTPPARSE_H
 #define EL__PROTOCOL_FTP_FTPPARSE_H
-
-/*
-ftpparse(&fp,buf,len) tries to parse one line of LIST output.
-
-The line is an array of len characters stored in buf.
-It should not include the terminating CR LF; so buf[len] is typically CR.
-
-If ftpparse() can't find a filename, it returns 0.
-
-If ftpparse() can find a filename, it fills in fp and returns 1.
-fp is a struct ftpparse, defined below.
-The name is an array of fp.namelen characters stored in fp.name;
-fp.name points somewhere within buf.
-*/
 
 struct ftpparse {
 	unsigned char *name;	/* not necessarily 0-terminated */
@@ -40,19 +26,31 @@ struct ftpparse {
 #define FTPPARSE_SIZE_BINARY 1	/* size is the number of octets in TYPE I */
 #define FTPPARSE_SIZE_ASCII 2	/* size is the number of octets in TYPE A */
 
-#define FTPPARSE_MTIME_UNKNOWN 0 /* Must be zero to permit memset() on struct ftpparse. */
-#define FTPPARSE_MTIME_LOCAL 1	/* time is correct */
+/* When a time zone is unknown, it is assumed to be GMT. You may want
+ * to use localtime() for LOCAL times, along with an indication that the
+ * time is correct in the local time zone, and gmtime() for REMOTE* times.
+ */
+#define FTPPARSE_MTIME_UNKNOWN 0 	/* Must be zero to permit memset() on struct ftpparse. */
+#define FTPPARSE_MTIME_LOCAL 1		/* time is correct */
 #define FTPPARSE_MTIME_REMOTEMINUTE 2	/* time zone and secs are unknown */
 #define FTPPARSE_MTIME_REMOTEDAY 3	/* time zone and time of day are unknown */
-/*
-When a time zone is unknown, it is assumed to be GMT. You may want
-to use localtime() for LOCAL times, along with an indication that the
-time is correct in the local time zone, and gmtime() for REMOTE* times.
-*/
 
 #define FTPPARSE_ID_UNKNOWN 0	/* Must be zero to permit memset() on struct ftpparse. */
 #define FTPPARSE_ID_FULL 1	/* unique identifier for files on this FTP server */
 
-extern int ftpparse(struct ftpparse *, unsigned char *, int);
+/* ftpparse(struct ftpparse *fp, unsigned char *buf, int len) tries to parse one line
+ * of LIST output.
+ *
+ * The line is an array of @len characters stored in @buf.
+ * It should not include the terminating CR LF; so @buf[@len] is typically CR.
+ *
+ * If ftpparse() can't find a filename, it returns 0.
+ *
+ * If ftpparse() can find a filename, it fills in @fp and returns 1.
+ *
+ * The name is an array of @fp.namelen characters stored in @fp.name;
+ * @fp.name points somewhere within @buf.
+ */
+int ftpparse(struct ftpparse *, unsigned char *, int);
 
 #endif

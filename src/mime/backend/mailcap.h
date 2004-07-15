d4ff282a14cb41ca61e792788c9dcfe57a7bd6ef
Jonas Fonseca <fonseca@diku.dk>
2004-07-15 00:43:06 +0000

    Remove unneeded guards.

--- src/mime/backend/mailcap.h
+++ src/mime/backend/mailcap.h
@@ -1,9 +1,7 @@
-/* $Id: mailcap.h,v 1.4 2004/01/01 15:47:25 jonas Exp $ */
+/* $Id: mailcap.h,v 1.5 2004/07/15 00:43:06 jonas Exp $ */
 
 #ifndef EL__MIME_BACKEND_MAILCAP_H
 #define EL__MIME_BACKEND_MAILCAP_H
-
-#ifdef CONFIG_MAILCAP
 
 #include "mime/backend/common.h"
 #include "modules/module.h"
@@ -11,5 +9,4 @@
 extern struct mime_backend mailcap_mime_backend;
 extern struct module mailcap_mime_module;
 
-#endif
 #endif
--- src/mime/backend/mimetypes.h
+++ src/mime/backend/mimetypes.h
@@ -1,9 +1,7 @@
-/* $Id: mimetypes.h,v 1.3 2004/01/01 15:47:25 jonas Exp $ */
+/* $Id: mimetypes.h,v 1.4 2004/07/15 00:43:06 jonas Exp $ */
 
 #ifndef EL__MIME_BACKEND_MIMETYPES_H
 #define EL__MIME_BACKEND_MIMETYPES_H
-
-#ifdef CONFIG_MIMETYPES
 
 #include "mime/backend/common.h"
 #include "modules/module.h"
@@ -11,5 +9,4 @@
 extern struct mime_backend mimetypes_mime_backend;
 extern struct module mimetypes_mime_module;
 
-#endif
 #endif

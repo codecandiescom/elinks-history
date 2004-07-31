d6ad73bd3c06b790f917acbd661ab7c1571aa339
Miciah Dashiel Butler Masters <miciah@myrealbox.com>
2004-07-31 05:30:07 +0000

    In decode_terminal_mouse_escape_sequence, remove an extra space.

--- src/terminal/kbd.c
+++ src/terminal/kbd.c
@@ -1,5 +1,5 @@
 /* Support for keyboard interface */
-/* $Id: kbd.c,v 1.104 2004/07/30 22:22:43 jonas Exp $ */
+/* $Id: kbd.c,v 1.105 2004/07/31 05:30:07 miciah Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -674,7 +674,7 @@ decode_terminal_mouse_escape_sequence(struct itrm *itrm, struct term_event *ev,
 
 		switch ((itrm->kqueue[el] - ' ') ^ xterm_button) { /* Every event changes only one bit */
 		    case TW_BUTT_LEFT:   mouse->button = B_LEFT | ( (xterm_button & TW_BUTT_LEFT) ? B_UP : B_DOWN ); break;
-		    case TW_BUTT_MIDDLE: mouse->button = B_MIDDLE | ( (xterm_button & TW_BUTT_MIDDLE) ? B_UP :  B_DOWN ); break;
+		    case TW_BUTT_MIDDLE: mouse->button = B_MIDDLE | ( (xterm_button & TW_BUTT_MIDDLE) ? B_UP : B_DOWN ); break;
 		    case TW_BUTT_RIGHT:  mouse->button = B_RIGHT | ( (xterm_button & TW_BUTT_RIGHT) ? B_UP : B_DOWN ); break;
 		    case 0: ev->info.mouse.button = B_DRAG;
 		    /* default : Twin protocol error */

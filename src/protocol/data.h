32fed725979054ae489e74fbef98a25a34aff2b2
Jonas Fonseca <fonseca@diku.dk>
2004-08-18 14:11:51 +0000

    Conditionally declare the data_protocol_handler.

--- src/protocol/data.h
+++ src/protocol/data.h
@@ -1,10 +1,14 @@
-/* $Id: data.h,v 1.1 2004/08/14 03:32:32 jonas Exp $ */
+/* $Id: data.h,v 1.2 2004/08/18 14:11:51 jonas Exp $ */
 
 #ifndef EL__PROTOCOL_DATA_H
 #define EL__PROTOCOL_DATA_H
 
 #include "protocol/protocol.h"
 
+#ifdef CONFIG_DATA
 extern protocol_handler data_protocol_handler;
+#else
+#define data_protocol_handler NULL
+#endif
 
 #endif

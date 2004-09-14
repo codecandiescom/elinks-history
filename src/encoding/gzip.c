aed3d5b26aa929b878cdc6c662b9dba734112ef9
Jonas Fonseca <fonseca@diku.dk>
2004-09-14 07:21:49 +0000

    Implement gzip header skipping and switch to do all decoding in memory.
    Thanks goes out to lindi- for digging into the problem where
    I gave up and for sstar for enlightening us both.

--- src/encoding/gzip.c
+++ src/encoding/gzip.c
@@ -1,5 +1,5 @@
 /* Gzip encoding (ENCODING_GZIP) backend */
-/* $Id: gzip.c,v 1.6 2004/09/14 06:47:58 jonas Exp $ */
+/* $Id: gzip.c,v 1.7 2004/09/14 07:21:49 jonas Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -47,7 +47,107 @@ gzip_decode(struct stream_encoded *stream, unsigned char *data, int len,
 }
 
 
-#define GZIP_DECODE_BUFFER_FROM_TMP_FILE 1
+/* The following code for decoding gzip in memory is a mix of code from zlib's
+ * gzio.c file copyrighted 1995-2002 by Jean-loup Gailly and the costumized
+ * header extraction in the linux kernels lib/inflate.c file not copyrighted
+ * 1992 by Mark Adler. */
+
+static int gzip_header_magic[2] = { 0x1f, 0x8b };
+
+enum gzip_header_flag {
+	GZIP_ASCII_TEXT	 = 0x01, /* File probably ascii text (unused) */
+	GZIP_HEADER_CRC	 = 0x02, /* Header CRC present */
+	GZIP_EXTRA_FIELD = 0x04, /* Extra field present */
+	GZIP_ORIG_NAME	 = 0x08, /* Original file name present */
+	GZIP_COMMENT	 = 0x10, /* File comment present */
+	GZIP_RESERVED	 = 0xE0, /* bits 5..7: reserved */
+};
+
+/* Read a byte from a gz_stream; update next_in and avail_in. Return EOF for
+ * end of file. */
+static int
+get_gzip_byte(z_stream *stream)
+{
+	if (stream->avail_in == 0)
+		return EOF;
+
+	stream->avail_in--;
+
+	return *(stream->next_in)++;
+}
+
+#define skip_gzip_bytes(stream, bytes) \
+	do { int i = bytes; while (i-- > 0) get_gzip_byte(stream); } while (0)
+
+#define skip_gzip_string(stream) \
+	do { int i; while ((i = get_gzip_byte(stream)) != 0 && i != EOF) ; } while (0)
+
+/* Check the gzip header of a gz_stream opened for reading. Set the stream mode
+ * to transparent if the gzip magic header is not present; set s->err to
+ * Z_DATA_ERROR if the magic header is present but the rest of the header is
+ * incorrect. */
+static int
+skip_gzip_header(z_stream *stream)
+{
+	unsigned int len;
+	int method; /* method byte */
+	int flags;  /* flags byte */
+
+	/* Check the gzip magic header */
+	for (len = 0; len < 2; len++) {
+		int byte = get_gzip_byte(stream);
+
+		if (byte != gzip_header_magic[len]) {
+			if (len != 0) {
+				stream->avail_in++;
+				stream->next_in--;
+			}
+
+			if (byte != EOF) {
+				stream->avail_in++;
+				stream->next_in--;
+			}
+
+			return stream->avail_in != 0 ? Z_OK : Z_STREAM_END;
+		}
+	}
+
+	method = get_gzip_byte(stream);
+	flags = get_gzip_byte(stream);
+
+	if (method != Z_DEFLATED || (flags & GZIP_RESERVED) != 0)
+		return Z_DATA_ERROR;
+
+	/* Discard time, xflags and OS code: */
+	skip_gzip_bytes(stream, 6);
+
+	if (flags & GZIP_EXTRA_FIELD) {
+		/* Skip the extra field */
+		len  =  (unsigned int) get_gzip_byte(stream);
+		len += ((unsigned int) get_gzip_byte(stream)) << 8;
+
+		/* If EOF is encountered @len is garbage, but the loop below
+		 * will quit anyway. */
+		while (len-- > 0 && get_gzip_byte(stream) != EOF) ;
+	}
+
+	/* Skip the original file name */
+	if (flags & GZIP_ORIG_NAME)
+		skip_gzip_string(stream);
+
+	/* Skip the .gz file comment */
+	if (flags & GZIP_COMMENT)
+		skip_gzip_string(stream);
+
+	/* Skip the header CRC */
+	if (flags & GZIP_HEADER_CRC)
+		skip_gzip_bytes(stream, 2);
+
+	return Z_OK;
+}
+
+
+#define GZIP_DECODE_BUFFER_IN_MEMORY 1
 
 /* Freaking dammit. This is impossible for me to get working. */
 static unsigned char *
@@ -79,6 +179,12 @@ gzip_decode_buffer(unsigned char *data, int len, int *new_len)
 		stream.next_in = (char *)data;
 		stream.avail_in = len;
 
+		error = skip_gzip_header(&stream);
+		if (error != Z_OK) {
+			stream.next_in = (char *)data;
+			stream.avail_in = len;
+		}
+
 		do {
 			unsigned char *new_buffer;
 			size_t size = stream.total_out + MAX_STR_LEN;
@@ -98,6 +204,11 @@ gzip_decode_buffer(unsigned char *data, int len, int *new_len)
 
 			error = inflate(&stream, Z_NO_FLUSH);
 			if (error == Z_STREAM_END) {
+				/* Here gzio.c has some detection of
+				 * concatenated .gz files and will do a gzip
+				 * header skip and an inflateReset() call
+				 * before continuing. It partly uses CRC to
+				 * detect that. */
 				*new_len = stream.total_out;
 				error = Z_OK;
 				break;
@@ -182,6 +293,7 @@ gzip_decode_buffer(unsigned char *data, int len, int *new_len)
 	return buffer.source;
 #endif /* Which decoding buffer routine */
 }
+
 
 static void
 gzip_close(struct stream_encoded *stream)

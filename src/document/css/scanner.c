f34e4ac1fd3cacf65bea7de7368b1e1ce9632c3b
Petr Baudis <pasky@ucw.cz>
2004-09-21 16:03:49 +0000

    '>' is now CHAR_TOKEN.

--- src/document/css/scanner.c
+++ src/document/css/scanner.c
@@ -1,5 +1,5 @@
 /* CSS token scanner utilities */
-/* $Id: scanner.c,v 1.129 2004/09/21 14:14:28 jonas Exp $ */
+/* $Id: scanner.c,v 1.130 2004/09/21 16:03:49 pasky Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -49,7 +49,7 @@ static const struct scan_table_info css_scan_table_info[] = {
 	SCAN_TABLE_STRING("\\_",	 CSS_CHAR_IDENT | CSS_CHAR_IDENT_START),
 	/* This should contain mostly used char tokens like ':' and maybe a few
 	 * garbage chars that people might put in their CSS code */
-	SCAN_TABLE_STRING("({});:,*.",	 CSS_CHAR_TOKEN),
+	SCAN_TABLE_STRING("({});:,*.>",	 CSS_CHAR_TOKEN),
 	SCAN_TABLE_STRING("<!->",	 CSS_CHAR_SGML_MARKUP),
 
 	SCAN_TABLE_END,

ed40e2b0e7dd7614c80dd619e94d5b717ed57f7f
Petr Baudis <pasky@ucw.cz>
2004-09-24 20:34:11 +0000

    Simplify the #ifdef maze.

--- src/util/ttime.h
+++ src/util/ttime.h
@@ -1,21 +1,13 @@
-/* $Id: ttime.h,v 1.6 2003/12/01 13:55:41 pasky Exp $ */
+/* $Id: ttime.h,v 1.7 2004/09/24 20:34:11 pasky Exp $ */
 
 #ifndef EL__UTIL_TTIME_H
 #define EL__UTIL_TTIME_H
 
-#ifdef TIME_WITH_SYS_TIME
 #ifdef HAVE_SYS_TIME_H
 #include <sys/time.h>
 #endif
 #ifdef HAVE_TIME_H
 #include <time.h>
-#endif
-#else
-#if defined(TM_IN_SYS_TIME) && defined(HAVE_SYS_TIME_H)
-#include <sys/time.h>
-#elif defined(HAVE_TIME_H)
-#include <time.h>
-#endif
 #endif
 
 /* WARNING: may cause overflows since ttime holds values 1000 times

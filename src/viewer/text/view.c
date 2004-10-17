b10e6f5c393c5d0d88fcd6a816fc5e6e644c928c
Miciah Dashiel Butler Masters <miciah@myrealbox.com>
2004-10-17 21:31:05 +0000

    Revert; I wanted to go in a different direction...

--- src/viewer/text/view.c
+++ src/viewer/text/view.c
@@ -1,5 +1,5 @@
 /* HTML viewer (and much more) */
-/* $Id: view.c,v 1.634 2004/10/17 21:29:06 miciah Exp $ */
+/* $Id: view.c,v 1.635 2004/10/17 21:31:05 miciah Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -705,7 +705,7 @@ static enum frame_event_status
 try_form_action(struct session *ses, struct document_view *doc_view,
 		struct link *link, struct term_event *ev)
 {
-	enum frame_event_status status = FRAME_EVENT_IGNORED;
+	enum frame_event_status status;
 
 	if (link && link_is_textinput(link)) {
 		status = field_op(ses, doc_view, link, ev);
@@ -719,7 +719,7 @@ try_form_action(struct session *ses, struct document_view *doc_view,
 		}
 	}
 
-	return status;
+	return FRAME_EVENT_IGNORED;
 }
 
 static enum frame_event_status

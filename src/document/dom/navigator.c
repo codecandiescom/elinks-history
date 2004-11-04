a7e5b7e43edace44154b6a10c67a85ab2cc44963
Jonas Fonseca <fonseca@diku.dk>
2004-11-04 17:14:19 +0000

    push_dom_node(): Don't call done_dom_node(@node) when navigator depth is bigger than DOM_NAVIGATOR_MAX_DEPTH. It is the responsibility of the caller. Fixes crash on http://www.usdoj.gov/atr/cases/f1900/1914.htm caused by the current SGML parser naively expects closing tags.

--- src/document/dom/navigator.c
+++ src/document/dom/navigator.c
@@ -1,5 +1,5 @@
 /* The DOM tree navigation interface */
-/* $Id: navigator.c,v 1.6 2004/11/04 14:22:06 jonas Exp $ */
+/* $Id: navigator.c,v 1.7 2004/11/04 17:14:19 jonas Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -82,7 +82,6 @@ push_dom_node(struct dom_navigator *navigator, struct dom_node *node)
 	assert(0 < node->type && node->type < DOM_NODES);
 
 	if (navigator->depth > DOM_NAVIGATOR_MAX_DEPTH) {
-		done_dom_node(node);
 		return NULL;
 	}
 

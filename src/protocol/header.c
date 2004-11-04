cfaf48d7b64e871cd42a90b131c3751a681f474f
Laurent Monin <zas@norz.org>
2004-11-04 09:59:55 +0000

    Add comments and documentation related to bug 549.

--- src/protocol/header.c
+++ src/protocol/header.c
@@ -1,5 +1,5 @@
 /* Parser of HTTP headers */
-/* $Id: header.c,v 1.11 2004/07/04 13:06:05 jonas Exp $ */
+/* $Id: header.c,v 1.12 2004/11/04 09:59:55 zas Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -15,6 +15,104 @@
 #include "util/memory.h"
 #include "util/string.h"
 
+/*
+* RFC 2616                        HTTP/1.1                       June 1999
+*
+*
+*        OCTET          = <any 8-bit sequence of data>
+*        CHAR           = <any US-ASCII character (octets 0 - 127)>
+*        UPALPHA        = <any US-ASCII uppercase letter "A".."Z">
+*        LOALPHA        = <any US-ASCII lowercase letter "a".."z">
+*        ALPHA          = UPALPHA | LOALPHA
+*        DIGIT          = <any US-ASCII digit "0".."9">
+*        CTL            = <any US-ASCII control character
+*                         (octets 0 - 31) and DEL (127)>
+*        CR             = <US-ASCII CR, carriage return (13)>
+*        LF             = <US-ASCII LF, linefeed (10)>
+*        SP             = <US-ASCII SP, space (32)>
+*        HT             = <US-ASCII HT, horizontal-tab (9)>
+*        <">            = <US-ASCII double-quote mark (34)>
+*
+*    HTTP/1.1 defines the sequence CR LF as the end-of-line marker for all
+*    protocol elements except the entity-body (see appendix 19.3 for
+*    tolerant applications). The end-of-line marker within an entity-body
+*    is defined by its associated media type, as described in section 3.7.
+*
+*        CRLF           = CR LF
+*
+*    HTTP/1.1 header field values can be folded onto multiple lines if the
+*    continuation line begins with a space or horizontal tab. All linear
+*    white space, including folding, has the same semantics as SP. A
+*    recipient MAY replace any linear white space with a single SP before
+*    interpreting the field value or forwarding the message downstream.
+*
+*        LWS            = [CRLF] 1*( SP | HT )
+*
+*    The TEXT rule is only used for descriptive field contents and values
+*    that are not intended to be interpreted by the message parser. Words
+*    of *TEXT MAY contain characters from character sets other than ISO-
+*    8859-1 [22] only when encoded according to the rules of RFC 2047
+*    [14].
+*
+*        TEXT           = <any OCTET except CTLs,
+*                         but including LWS>
+*
+*    A CRLF is allowed in the definition of TEXT only as part of a header
+*    field continuation. It is expected that the folding LWS will be
+*    replaced with a single SP before interpretation of the TEXT value.
+*
+*    Hexadecimal numeric characters are used in several protocol elements.
+*
+*        HEX            = "A" | "B" | "C" | "D" | "E" | "F"
+*                       | "a" | "b" | "c" | "d" | "e" | "f" | DIGIT
+*
+*    Many HTTP/1.1 header field values consist of words separated by LWS
+*    or special characters. These special characters MUST be in a quoted
+*    string to be used within a parameter value (as defined in section
+*    3.6).
+*
+*        token          = 1*<any CHAR except CTLs or separators>
+*        separators     = "(" | ")" | "<" | ">" | "@"
+*                       | "," | ";" | ":" | "\" | <">
+*                       | "/" | "[" | "]" | "?" | "="
+*                       | "{" | "}" | SP | HT
+*
+*    Comments can be included in some HTTP header fields by surrounding
+*    the comment text with parentheses. Comments are only allowed in
+*    fields containing "comment" as part of their field value definition.
+*    In all other fields, parentheses are considered part of the field
+*    value.
+*
+*        comment        = "(" *( ctext | quoted-pair | comment ) ")"
+*        ctext          = <any TEXT excluding "(" and ")">
+*
+*    A string of text is parsed as a single word if it is quoted using
+*    double-quote marks.
+*
+*        quoted-string  = ( <"> *(qdtext | quoted-pair ) <"> )
+*        qdtext         = <any TEXT except <">>
+*
+*    The backslash character ("\") MAY be used as a single-character
+*    quoting mechanism only within quoted-string and comment constructs.
+*
+*        quoted-pair    = "\" CHAR
+*/
+
+/* FIXME: bug 549
+ *
+ * HTTP/1.1 header continuation lines are not honoured.
+ * DEL char is accepted in TEXT part.
+ * HT char is not accepted in TEXT part.
+ * LF alone do not mark end of line, CRLF is the correct termination.
+ * CR or LF are invalid in header line.
+ *
+ * Mozilla, IE, NS tolerate header value separator different from ':'
+ * Examples:
+ * name: value
+ * name value
+ * name :value
+ * name=value
+ */
 unsigned char *
 parse_header(unsigned char *head, unsigned char *item, unsigned char **ptr)
 {

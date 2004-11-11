5e271d979815c903a58958f90becda2fcf7d422f
Miciah Dashiel Butler Masters <miciah@myrealbox.com>
2004-11-11 19:05:30 +0000

    In del_from_cookie_list, drop the local variable tmp and instead operate directly on the passed pointer.

--- src/cookies/cookies.c
+++ src/cookies/cookies.c
@@ -1,5 +1,5 @@
 /* Internal cookies implementation */
-/* $Id: cookies.c,v 1.184 2004/11/11 19:04:39 miciah Exp $ */
+/* $Id: cookies.c,v 1.185 2004/11/11 19:05:30 miciah Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -426,10 +426,8 @@ set_cookie(struct uri *uri, unsigned char *str)
 static void
 del_from_cookie_list(struct cookie *cookie)
 {
-	struct cookie *tmp = cookie;
-
-	del_from_list(tmp);
-	free_cookie(tmp);
+	del_from_list(cookie);
+	free_cookie(cookie);
 }
 
 void

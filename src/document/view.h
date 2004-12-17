af23c406e18c1986659c0caceb0185718404e592
Miciah Dashiel Butler Masters <miciah@myrealbox.com>
2004-12-17 04:40:51 +0000

    Guess at the purpose of struct link_bg *link_bg and int link_bg_n in struct document_view.

--- src/document/view.h
+++ src/document/view.h
@@ -1,4 +1,4 @@
-/* $Id: view.h,v 1.25 2004/09/26 09:56:55 pasky Exp $ */
+/* $Id: view.h,v 1.26 2004/12/17 04:40:51 miciah Exp $ */
 
 #ifndef EL__DOCUMENT_VIEW_H
 #define EL__DOCUMENT_VIEW_H
@@ -25,9 +25,18 @@ struct document_view {
 	struct session *session;
 	struct document *document;
 	struct view_state *vs;
+
+	/* The elements of this array correspond to the points of the active
+	 * link. Each element stores the colour and attributes
+	 * that the corresponding point had before the link was selected.
+	 * The last element is an exception: it is the template character,
+	 * which holds the colour and attributes that the active link was
+	 * given upon being selected. */
+	/* XXX: The above is guesswork; please confirm or correct. -- Miciah */
 	struct link_bg *link_bg;
 
-	int link_bg_n;
+	int link_bg_n; /* number of elements in link_bg,
+			  not including the template  */
 	struct box box;	/* pos and size of window */
 	int last_x, last_y; /* last pos of window */
 	int depth;

e94a24c5eaed38977fc56b655dedd45e700279b3
Petr Baudis <pasky@ucw.cz>
2004-12-24 14:07:32 +0000

    Some meaningful Perl hooks by Apu aka Russ Rowan <rrowan@dandy.net>.

--- contrib/perl/hooks.pl
+++ contrib/perl/hooks.pl
@@ -1,26 +1,123 @@
+#!/usr/bin/perl -w
 use strict;
 
-sub goto_url_hook {
+################################################################################
+### goto_url_hook ##############################################################
+sub goto_url_hook
+{
 	my $url = shift;
 	my $current_url = shift;
+
+	# "bugmenot" dumb prefix
+	if ($url =~ '^bugmenot$' && $current_url)
+	{
+		my (undef, $target_url) = $current_url =~ /^(.*):\/\/(.*)/;
+		$url = 'http://bugmenot.com/view.php?url=' . $target_url;
+		return $url;
+	}
+
+	# random URL generator
+	if ($url =~ '^bored$' || $url =~ '^random$')
+	{
+		my $word; # you can say *that* again...
+		srand();
+		open FILE, '</usr/share/dict/words'
+			or open FILE, '</usr/dict/words'
+			or return 'http://google.com/webhp?hl=xx-bork';
+		rand($.) < 1 && ($word = $_) while <FILE>;
+		close FILE;
+		($word) = $word =~ /(.*)/;
+		$url = 'http://' . lc($word) . '.com';
+		return $url;
+	}
+
+	# Google search smart prefix
+	if ($url =~ '^(g|google|search|find)(| .*)$')
+	{
+		my ($search) = $url =~ /^[a-z]* (.*)/;
+		if ($search)
+		{
+			$url = 'http://google.com/search?q=' . $search . '&hl=xx-bork';
+		}
+		else
+		{
+			$url = 'http://google.com/webhp?hl=xx-bork';
+		}
+		return $url;
+	}
+
+	# Google Groups smart prefix
+	if ($url =~ '^(gg|groups|gr|news|usenet)(| .*)$')
+	{
+		my ($search) = $url =~ /^[a-z]* (.*)/;
+		if ($search)
+		{
+			$url = 'http://google.com/groups?q=' . $search . '&hl=xx-bork';
+		}
+		else
+		{
+			$url = 'http://google.com/groups?hl=xx-bork';
+		}
+		return $url;
+	}
+
 	return $url;
 }
 
-sub follow_url_hook {
+################################################################################
+### follow_url_hook ############################################################
+sub follow_url_hook
+{
 	my $url = shift;
+
+	# Bork! Bork! Bork!
+	if ($url =~ 'google.com')
+	{
+		if ($url =~ '^http://(|www\.|search\.)google\.com(|/search)(|/)$')
+		{
+			$url = 'http://google.com/webhp?hl=xx-bork';
+		}
+		elsif ($url =~ '^http://(|www\.)groups\.google\.com(|/groups)(|/)$'
+			|| $url =~ '^http://(|www\.|search\.)google\.com/groups(|/)$')
+		{
+			$url = 'http://google.com/groups?hl=xx-bork';
+		}
+		return $url;
+	}
+
 	return $url;
 }
 
-sub pre_format_html_hook {
+################################################################################
+### pre_format_html_hook #######################################################
+sub pre_format_html_hook
+{
 	my $url = shift;
 	my $html = shift;
+
+	# /. sanitation
+	if ($url =~ 'slashdot.org')
+	{
+#		$html =~ s/^<!-- Advertisement code. -->.*<!-- end ad code -->$/<br>/sm;
+		return $html;
+	}
+
 	return $html;
 }
 
-sub proxy_for_hook {
+################################################################################
+### proxy_for_hook #############################################################
+sub proxy_for_hook
+{
 	my $url = shift;
+
 	return $url;
 }
 
-sub quit_hook {
+################################################################################
+### quit_hook ##################################################################
+sub quit_hook
+{
+	# words of wisdom from ELinks the Sage
+	system('fortune -sa 2>/dev/null');
 }

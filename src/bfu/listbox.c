b6b699db2925f1d7dc8837168f4167f7ab3df751
Laurent Monin <zas@norz.org>
2004-12-29 14:25:44 +0000

    Use = instead of memcpy(). Safer and more efficient, idea by Pasky.

--- src/bfu/listbox.c
+++ src/bfu/listbox.c
@@ -1,5 +1,5 @@
 /* Listbox widget implementation. */
-/* $Id: listbox.c,v 1.183 2004/11/23 13:08:29 zas Exp $ */
+/* $Id: listbox.c,v 1.184 2004/12/29 14:25:44 zas Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -512,7 +512,7 @@ init_listbox(struct dialog_data *dlg_data, struct widget_data *widget_data)
 
 	/* Try to restore the position from last time */
 	if (!list_empty(browser->root.child) && browser->box_data.items) {
-		memcpy(box, &browser->box_data, sizeof(struct listbox_data));
+		*box = browser->box_data;
 
 		traverse_listbox_items_list(browser->root.child.next, box, 0, 0,
 					    check_old_state, box);

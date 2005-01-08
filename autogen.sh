5e3a740e89bf27ea525525afa08f55803455cd12
Jonas Fonseca <fonseca@diku.dk>
2005-01-08 21:45:28 +0000

    Who needs acinclude.m4 for generating aclocal.m4 when you can tell aclocal where to get the .m4 files.

--- autogen.sh
+++ autogen.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 
-echo acinclude.m4...
-echo "dnl This is automatically generated from m4/ files! Do not modify!" > acinclude.m4
-cat config/m4/*.m4 >> acinclude.m4
-
 echo aclocal...
-aclocal
+aclocal -I config/m4
 
 echo autoheader...
 autoheader

a4ce32ce7398e0e3d792459bb24a1fb6e1534248
Petr Baudis <pasky@ucw.cz>
2005-02-04 21:49:26 +0000

    Declare @cookie_queries as extern in the .h file. It was probably utterly broken until now. Also fixes compilation with outrageous CFLAGS like some of the Gentoo people like to have (http://bugs.gentoo.org/show_bug.cgi?id=80226).

--- src/cookies/dialogs.h
+++ src/cookies/dialogs.h
@@ -1,4 +1,4 @@
-/* $Id: dialogs.h,v 1.5 2004/11/19 15:53:54 jonas Exp $ */
+/* $Id: dialogs.h,v 1.6 2005/02/04 21:49:26 pasky Exp $ */
 
 #ifndef EL__COOKIES_DIALOGS_H
 #define EL__COOKIES_DIALOGS_H
@@ -8,7 +8,7 @@
 #include "terminal/terminal.h"
 #include "util/lists.h"
 
-struct list_head cookie_queries;
+extern struct list_head cookie_queries;
 
 void accept_cookie_dialog(struct session *ses, void *data);
 extern struct hierbox_browser cookie_browser;

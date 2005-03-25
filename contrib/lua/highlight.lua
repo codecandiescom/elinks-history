8c945dfe2e4d08f140cc835435de707cc029e3f4
Miciah Dashiel Butler Masters <miciah@myrealbox.com>
2005-03-25 19:02:48 +0000

    Simplify by returning from the loop once the code has been highlighted.

--- contrib/lua/highlight.lua
+++ contrib/lua/highlight.lua
@@ -1,5 +1,5 @@
 -- Source-code highlighting hook
--- $Id: highlight.lua,v 1.4 2005/03/25 19:00:46 miciah Exp $
+-- $Id: highlight.lua,v 1.5 2005/03/25 19:02:48 miciah Exp $
 
 highlight_file_types = {
     patch = "%.patch$",
@@ -11,19 +11,17 @@ highlight_file_types = {
 }
 
 function highlight (url, html)
-    local ret=nil
-
     for language,pattern in pairs(highlight_file_types) do
         if string.find (url, pattern) then
             local tmp = tmpname ()
             writeto (tmp) write (html) writeto()
             html = pipe_read ("(code2html -l "..language.." "..tmp.." - ) 2>/dev/null")
             os.remove(tmp)
-            ret = 1
+            return html,nil
         end
     end
 
-    return (ret and html),nil
+    return nil,nil
 end
 
 table.insert(pre_format_html_hooks, highlight)

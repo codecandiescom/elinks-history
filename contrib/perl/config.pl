7dfa8cf554c8eb4179b1075fa79d7a51bfc793cc
Russ Rowan <rrowan@dandy.net>
2005-03-27 22:20:27 +0000

    And here's config.pl.

---
+++ contrib/perl/config.pl
@@ -0,0 +1,11 @@
+bork:		nah
+collapse:	nope
+fortune:	elinks
+googlebeta:	hell no
+gotosearch:	not yet
+ipv6:		no
+language:	english
+news:		bbc
+search:		google
+usenet:		deja news
+weather:	weather underground

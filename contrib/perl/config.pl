a0a2aa8f0c9d3261544969c4a07c08b224a597c5
Russ Rowan <rrowan@dandy.net>
2005-03-27 22:35:46 +0000

    Properly comment config.pl so it's readable.

--- contrib/perl/config.pl
+++ contrib/perl/config.pl
@@ -1,11 +1,21 @@
-bork:		nah
-collapse:	nope
-fortune:	elinks
-googlebeta:	hell no
-gotosearch:	not yet
-ipv6:		no
-language:	english
-news:		bbc
-search:		google
-usenet:		deja news
-weather:	weather underground
+# Configuration file for hooks.pl - salt to taste and place in ~/.elinks
+# $Id: config.pl,v 1.2 2005/03/27 22:35:46 rrowan Exp $
+
+bork:       nah                   # BORKify Google?
+collapse:   nope                  # Collapse all XBEL bookmark folders on exit?
+fortune:    elinks                # *fortune*, *elinks* tip, or *none* on quit?
+googlebeta: hell no               # I miss DejaNews...
+gotosearch: not yet               # Don't use this yet.  It's broken.
+ipv6:       no                    # IPV4 or 6 address blocks with "ip" prefix?
+language:   english               # "bf nl en" still works, but now "bf nl" does too
+news:       bbc                   # Agency to use for "news" and "n" prefixes
+search:     google                # Engine for (search|find|www|web|s|f|go) prefixes
+usenet:     deja news             # *google* or *standard* view for news:// URLs
+weather:    weather underground   # Server for "weather" and "w" prefixes
+
+# news:    bbc, msnbc, cnn, fox, google, yahoo, reuters, eff, wired,
+#          slashdot, newsforge, usnews, newsci, discover, sciam
+# search:  elgoog, google, yahoo, ask jeeves, a9, altavista, msn, dmoz,
+#          dogpile, mamma, webcrawler, netscape, lycos, hotbot, excite
+# weather: weather underground, google, yahoo, cnn, accuweather,
+#          ask jeeves

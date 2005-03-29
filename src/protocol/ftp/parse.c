b9df9a492c70ac9a8f58ccaa67b5d65258e65e65
Jonas Fonseca <fonseca@diku.dk>
2005-03-29 01:04:26 +0000

    Fix spacing.

--- src/protocol/ftp/parse.c
+++ src/protocol/ftp/parse.c
@@ -1,5 +1,5 @@
 /* Parsing of FTP `ls' directory output. */
-/* $Id: parse.c,v 1.23 2005/03/29 01:03:47 jonas Exp $ */
+/* $Id: parse.c,v 1.24 2005/03/29 01:04:26 jonas Exp $ */
 
 /* Parts of this file was part of GNU Wget
  * Copyright (C) 1995, 1996, 1997, 2000, 2001 Free Software Foundation, Inc. */
@@ -647,7 +647,7 @@ parse_ftp_winnt_response(struct ftp_file_info *info, unsigned char *src, int len
 		return NULL;
 
 	/* Store the time-stamp. */
-	info->mtime = mktime (&mtime);
+	info->mtime = mktime(&mtime);
 
 	skip_nonspace_end(src, end);
 	skip_space_end(src, end);

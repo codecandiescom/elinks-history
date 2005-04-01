41432eefd4197d1fc15e6c2aa2773abd0b62d785
Laurent Monin <zas@norz.org>
2005-04-01 17:29:41 +0000

    Just declare struct module.

--- src/scripting/scripting.h
+++ src/scripting/scripting.h
@@ -1,11 +1,11 @@
-/* $Id: scripting.h,v 1.10 2004/04/29 23:32:18 jonas Exp $ */
+/* $Id: scripting.h,v 1.11 2005/04/01 17:29:41 zas Exp $ */
 
 #ifndef EL__SCRIPTING_SCRIPTING_H
 #define EL__SCRIPTING_SCRIPTING_H
 
 #ifdef CONFIG_SCRIPTING
 
-#include "modules/module.h"
+struct module;
 
 extern struct module scripting_module;
 

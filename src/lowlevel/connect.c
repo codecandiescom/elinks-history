321b5703ef9554c8dde2ffd42ebd71ceef183147
Jonas Fonseca <fonseca@diku.dk>
2005-04-11 23:50:30 +0000

    Fix last patch.

--- src/lowlevel/connect.c
+++ src/lowlevel/connect.c
@@ -1,5 +1,5 @@
 /* Sockets-o-matic */
-/* $Id: connect.c,v 1.155 2005/04/11 23:32:57 jonas Exp $ */
+/* $Id: connect.c,v 1.156 2005/04/11 23:50:30 jonas Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -569,10 +569,10 @@ struct write_buffer {
 	unsigned char data[1]; /* must be at end of struct */
 };
 
-int
+static int
 generic_write(struct connection_socket *socket, unsigned char *data, int len)
 {
-	return safe_write(socket->fd, wb->data + wb->pos, wb->len - wb->pos);
+	return safe_write(socket->fd, data, len);
 }
 
 static void
@@ -608,7 +608,7 @@ write_select(struct connection_socket *socket)
 #endif
 	{
 		assert(wb->len - wb->pos > 0);
-		wr = generic_write(socket->fd, wb->data + wb->pos, wb->len - wb->pos);
+		wr = generic_write(socket, wb->data + wb->pos, wb->len - wb->pos);
 		if (wr <= 0) {
 			socket->retry(socket->conn, wr ? -errno : S_CANT_WRITE);
 			return;

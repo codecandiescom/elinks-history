ce2bf5f96467756fe2cbd9af80789a2965a39a36
Jonas Fonseca <fonseca@diku.dk>
2005-04-12 00:41:41 +0000

    Remove unneeded includes.

--- src/ssl/connect.h
+++ src/ssl/connect.h
@@ -1,12 +1,11 @@
-/* $Id: connect.h,v 1.18 2005/04/12 00:40:17 jonas Exp $ */
+/* $Id: connect.h,v 1.19 2005/04/12 00:41:41 jonas Exp $ */
 
 #ifndef EL__SSL_CONNECT_H
 #define EL__SSL_CONNECT_H
 
 #ifdef CONFIG_SSL
 
-#include "lowlevel/connect.h"
-#include "sched/connection.h"
+struct connection_socket;
 
 int ssl_connect(struct connection_socket *socket);
 int ssl_write(struct connection_socket *socket, unsigned char *data, int len);

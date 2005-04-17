bede34516eb28db1f3c2b10d2eb00f88eea3e1b5
Laurent Monin <zas@norz.org>
2005-04-17 18:08:00 +0000

    Add progress.{c,h}.

---
+++ src/sched/progress.c
@@ -0,0 +1,10 @@
+/* Downloads progression stuff. */
+/* $Id: progress.c,v 1.1 2005/04/17 18:08:00 zas Exp $ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "elinks.h"
+
+#include "sched/progress.h"
---
+++ src/sched/progress.h
@@ -0,0 +1,6 @@
+/* $Id: progress.h,v 1.1 2005/04/17 18:08:00 zas Exp $ */
+
+#ifndef EL__SCHED_PROGRESS_H
+#define EL__SCHED_PROGRESS_H
+
+#endif

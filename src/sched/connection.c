b254103bd272ad13f19795384814d883af528bbb
Jonas Fonseca <fonseca@diku.dk>
2005-05-01 14:28:11 +0000

    Fix comment.

--- src/sched/connection.c
+++ src/sched/connection.c
@@ -1,5 +1,5 @@
 /* Connections management */
-/* $Id: connection.c,v 1.285 2005/04/27 22:12:06 jonas Exp $ */
+/* $Id: connection.c,v 1.286 2005/05/01 14:28:11 jonas Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -413,7 +413,7 @@ done_connection(struct connection *conn)
 	/* When removing the connection callbacks should always be aware of it
 	 * so they can unregister themselves. We do this by enforcing that the
 	 * connection is in a result state. If it is not already it is an
-	 * internal bug. This should never happen bug it does. ;) --jonas */
+	 * internal bug. This should never happen but it does. ;) --jonas */
 	if (!is_in_result_state(conn->state))
 		set_connection_state(conn, S_INTERNAL);
 

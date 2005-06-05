da122073f4e7cb09033045e449e73bbf90c94277
Witold Filipczyk <witekfl@poczta.gazeta.pl>
2005-06-05 14:22:08 +0000

    Killed warning.

--- src/scripting/python/python.c
+++ src/scripting/python/python.c
@@ -1,14 +1,15 @@
 /* Python module */
-/* $Id: python.c,v 1.1 2005/06/02 18:01:34 witekfl Exp $ */
+/* $Id: python.c,v 1.2 2005/06/05 14:22:08 witekfl Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
 
+#include "scripting/python/core.h"
+
 #include "elinks.h"
 
 #include "modules/module.h"
-#include "scripting/python/core.h"
 #include "scripting/python/hooks.h"
 
 

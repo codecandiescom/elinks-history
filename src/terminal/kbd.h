0623465ca93a0319706d4269259772864f4bb1de
Witold Filipczyk <witekfl@poczta.gazeta.pl>
2005-07-18 16:08:02 +0000

    Moved definition of struct itrm to header file. It will be used by sysmouse support.

--- src/terminal/kbd.h
+++ src/terminal/kbd.h
@@ -1,7 +1,44 @@
-/* $Id: kbd.h,v 1.10 2005/07/09 00:30:10 miciah Exp $ */
+/* $Id: kbd.h,v 1.11 2005/07/18 16:08:03 witekfl Exp $ */
 
 #ifndef EL__TERMINAL_KBD_H
 #define EL__TERMINAL_KBD_H
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#ifdef HAVE_TERMIOS_H
+#include <termios.h>
+#endif
+
+#include "main/timer.h"
+
+#define IN_BUF_SIZE	16
+
+struct itrm {
+	int std_in;
+	int std_out;
+	int sock_in;
+	int sock_out;
+	int ctl_in;
+
+	/* Input queue */
+	unsigned char kqueue[IN_BUF_SIZE];
+	int qlen;
+
+	/* Output queue */
+	unsigned char *ev_queue;
+	int eqlen;
+
+	timer_id_T timer;		/* ESC timeout timer */
+	struct termios t;		/* For restoring original attributes */
+	void *mouse_h;			/* Mouse handle */
+	unsigned char *orig_title;	/* For restoring window title */
+
+	unsigned int blocked:1;		/* Whether it was blocked */
+	unsigned int altscreen:1;	/* Whether to use alternate screen */
+	unsigned int touched_title:1;	/* Whether the term title was changed */
+};
 
 struct term_event_keyboard {
 	int key;

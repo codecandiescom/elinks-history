def231e0f128ae147f6e0de0b5ee1bdbebde800a
Russ Rowan <rrowan@dandy.net>
2005-07-31 09:52:35 +0000

    Added basic file detection for 'gotosearch'.

--- contrib/perl/hooks.pl
+++ contrib/perl/hooks.pl
@@ -1,5 +1,5 @@
 # Example ~/.elinks/hooks.pl
-# $Id: hooks.pl,v 1.108 2005/07/31 09:51:52 rrowan Exp $
+# $Id: hooks.pl,v 1.109 2005/07/31 09:52:35 rrowan Exp $
 #
 # This file is (c) Russ Rowan and Petr Baudis and GPL'd.
 #
@@ -931,6 +931,7 @@ Dialects: I<redneck>, I<jive>, I<cockney>, I<fudd>, I<bork>, I<moron>, I<piglati
 	# Anything not otherwise useful is a search
 	if ($current_url and loadrc("gotosearch") eq "yes")
 	{
+		return $url if $url =~ /^(\/|~)/;
 		if ($url =~ /^(([a-zA-Z]{3,}(|4|6):\/\/|(www|ftp)\.)|)[a-zA-Z0-9]+/       and
 		   ($url =~ /[a-zA-Z0-9-]+\.(com|org|net|edu|gov|int|mil)($|\/|\?)/        or
 			$url =~ /[a-zA-Z0-9-]+\.(biz|info|name|pro|aero|coop|museum)($|\/|\?)/ or

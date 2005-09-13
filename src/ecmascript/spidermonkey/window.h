1c2695011f9f3f9d915254231e9465ce7ebe4f2d
Petr Baudis <pasky@ucw.cz>
2005-09-13 21:52:02 +0000

    Fix wrong include name.

--- src/ecmascript/spidermonkey/window.h
+++ src/ecmascript/spidermonkey/window.h
@@ -1,9 +1,9 @@
-/* $Id: window.h,v 1.1 2005/09/13 17:35:23 pasky Exp $ */
+/* $Id: window.h,v 1.2 2005/09/13 21:52:02 pasky Exp $ */
 
 #ifndef EL__ECMASCRIPT_SPIDERMONKEY_WINDOW_H
 #define EL__ECMASCRIPT_SPIDERMONKEY_WINDOW_H
 
-#include "ecmascript/spidermonkey/base.h"
+#include "ecmascript/spidermonkey/util.h"
 
 extern const JSClass window_class;
 extern const JSPropertySpec window_props[];

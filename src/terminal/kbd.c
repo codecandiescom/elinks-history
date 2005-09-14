5df776a08c9b0c7dfd89aa605165f5bb7edd8b21
Laurent Monin <zas@norz.org>
2005-09-14 10:22:29 +0000

    Group all mouse-related code.

--- src/terminal/kbd.c
+++ src/terminal/kbd.c
@@ -1,5 +1,5 @@
 /* Support for keyboard interface */
-/* $Id: kbd.c,v 1.169 2005/09/14 10:04:54 zas Exp $ */
+/* $Id: kbd.c,v 1.170 2005/09/14 10:22:29 zas Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -40,12 +40,6 @@
 
 
 /* TODO: move stuff from here to itrm.{c,h} and mouse.{c,h} */
-
-
-#define TW_BUTT_LEFT	1
-#define TW_BUTT_MIDDLE	2
-#define TW_BUTT_RIGHT	4
-
 
 static struct itrm *ditrm = NULL;
 
@@ -148,66 +142,34 @@ kbd_ctrl_c(void)
 #define write_sequence(fd, seq) \
 	hard_write(fd, seq, sizeof(seq) - 1)
 
-#define INIT_TERMINAL_SEQ	"\033)0\0337"	/* Special Character and Line Drawing Set, Save Cursor */
+
+
+#ifdef CONFIG_MOUSE
+
 #define INIT_TWIN_MOUSE_SEQ	"\033[?9h"	/* Send MIT Mouse Row & Column on Button Press */
 #define INIT_XWIN_MOUSE_SEQ	"\033[?1000h"	/* Send Mouse X & Y on button press and release */
-#define INIT_ALT_SCREEN_SEQ	"\033[?47h"	/* Use Alternate Screen Buffer */
-
-static int mouse_enabled;
 
 static void
 send_mouse_init_sequence(int h)
 {
-#ifdef CONFIG_MOUSE
 	write_sequence(h, INIT_TWIN_MOUSE_SEQ);
 	write_sequence(h, INIT_XWIN_MOUSE_SEQ);
-#endif
 }
 
-static void
-send_init_sequence(int h, int altscreen)
-{
-	write_sequence(h, INIT_TERMINAL_SEQ);
-
-	/* If alternate screen is supported switch to it. */
-	if (altscreen) {
-		write_sequence(h, INIT_ALT_SCREEN_SEQ);
-	}
-	send_mouse_init_sequence(h);
-}
-
-#define DONE_CLS_SEQ		"\033[2J"	/* Erase in Display, Clear All */
-#define DONE_TERMINAL_SEQ	"\0338\r \b"	/* Restore Cursor (DECRC) + ??? */
 #define DONE_TWIN_MOUSE_SEQ	"\033[?9l"	/* Don't Send MIT Mouse Row & Column on Button Press */
 #define DONE_XWIN_MOUSE_SEQ	"\033[?1000l"	/* Don't Send Mouse X & Y on button press and release */
-#define DONE_ALT_SCREEN_SEQ	"\033[?47l"	/* Use Normal Screen Buffer */
 
 static void
 send_mouse_done_sequence(int h)
 {
-#ifdef CONFIG_MOUSE
 	/* This is a hack to make xterm + alternate screen working,
 	 * if we send only DONE_XWIN_MOUSE_SEQ, mouse is not totally
 	 * released it seems, in rxvt and xterm... --Zas */
 	write_sequence(h, DONE_TWIN_MOUSE_SEQ);
 	write_sequence(h, DONE_XWIN_MOUSE_SEQ);
-#endif
 }
 
-static void
-send_done_sequence(int h, int altscreen)
-{
-	write_sequence(h, DONE_CLS_SEQ);
-
-	send_mouse_done_sequence(h);
-
-	/* Switch from alternate screen. */
-	if (altscreen) {
-		write_sequence(h, DONE_ALT_SCREEN_SEQ);
-	}
-
-	write_sequence(h, DONE_TERMINAL_SEQ);
-}
+static int mouse_enabled;
 
 static void
 disable_mouse(void)
@@ -239,6 +201,151 @@ toggle_mouse(void)
 	else
 		enable_mouse();
 }
+
+static int
+decode_mouse_position(struct itrm *itrm, int from)
+{
+	int position;
+
+	position = (unsigned char) (itrm->in.queue.data[from]) - ' ' - 1
+		 + ((int) ((unsigned char) (itrm->in.queue.data[from + 1]) - ' ' - 1) << 7);
+
+	return (position & (1 << 13)) ? 0 : position;
+}
+
+#define get_mouse_x_position(itrm, esclen) decode_mouse_position(itrm, (esclen) + 1)
+#define get_mouse_y_position(itrm, esclen) decode_mouse_position(itrm, (esclen) + 3)
+
+#define TW_BUTT_LEFT	1
+#define TW_BUTT_MIDDLE	2
+#define TW_BUTT_RIGHT	4
+
+/* Returns length of the escape sequence or -1 if the caller needs to set up
+ * the ESC delay timer. */
+static int
+decode_terminal_mouse_escape_sequence(struct itrm *itrm, struct term_event *ev,
+				      int el, int v)
+{
+	static int xterm_button = -1;
+	struct term_event_mouse mouse;
+
+	if (itrm->in.queue.len - el < 3)
+		return -1;
+
+	if (v == 5) {
+		if (xterm_button == -1)
+			xterm_button = 0;
+
+		if (itrm->in.queue.len - el < 5)
+			return -1;
+
+		mouse.x = get_mouse_x_position(itrm, el);
+		mouse.y = get_mouse_y_position(itrm, el);
+
+		switch ((itrm->in.queue.data[el] - ' ') ^ xterm_button) { /* Every event changes only one bit */
+		case TW_BUTT_LEFT:
+			mouse.button = B_LEFT | ((xterm_button & TW_BUTT_LEFT) ? B_UP : B_DOWN);
+			break;
+		case TW_BUTT_MIDDLE:
+			mouse.button = B_MIDDLE | ((xterm_button & TW_BUTT_MIDDLE) ? B_UP : B_DOWN);
+			break;
+		case TW_BUTT_RIGHT:
+			mouse.button = B_RIGHT | ((xterm_button & TW_BUTT_RIGHT) ? B_UP : B_DOWN);
+			break;
+		case 0:
+			mouse.button = B_DRAG;
+			break;
+		default:
+			mouse.button = 0; /* shut up warning */
+		/* default : Twin protocol error */
+		}
+
+		xterm_button = itrm->in.queue.data[el] - ' ';
+		el += 5;
+
+	} else {
+		/* See terminal/mouse.h about details of the mouse reporting
+		 * protocol and {struct term_event_mouse->button} bitmask
+		 * structure. */
+		mouse.x = itrm->in.queue.data[el+1] - ' ' - 1;
+		mouse.y = itrm->in.queue.data[el+2] - ' ' - 1;
+
+		/* There are rumours arising from remnants of code dating to
+		 * the ancient Mikulas' times that bit 4 indicated B_DRAG.
+		 * However, I didn't find on what terminal it should be ever
+		 * supposed to work and it conflicts with wheels. So I removed
+		 * the last remnants of the code as well. --pasky */
+
+		mouse.button = (itrm->in.queue.data[el] & 7) | B_DOWN;
+		/* smartglasses1 - rxvt wheel: */
+		if (mouse.button == 3 && xterm_button != -1) {
+			mouse.button = xterm_button | B_UP;
+		}
+		/* xterm wheel: */
+		if ((itrm->in.queue.data[el] & 96) == 96) {
+			mouse.button = (itrm->in.queue.data[el] & 1) ? B_WHEEL_DOWN : B_WHEEL_UP;
+		}
+
+		xterm_button = -1;
+		/* XXX: Eterm/aterm uses rxvt-like reporting, but sends the
+		 * release sequence for wheel. rxvt itself sends only press
+		 * sequence. Since we can't reliably guess what we're talking
+		 * with from $TERM, we will rather support Eterm/aterm, as in
+		 * rxvt, at least each second wheel up move will work. */
+		if (mouse_action_is(&mouse, B_DOWN))
+			xterm_button = mouse_get_button(&mouse);
+
+		el += 3;
+	}
+
+	/* Postpone changing of the event type until all sanity
+	 * checks have been done. */
+	set_mouse_term_event(ev, mouse.x, mouse.y, mouse.button);
+
+	return el;
+}
+
+#endif /* CONFIG_MOUSE */
+
+
+#define INIT_TERMINAL_SEQ	"\033)0\0337"	/* Special Character and Line Drawing Set, Save Cursor */
+#define INIT_ALT_SCREEN_SEQ	"\033[?47h"	/* Use Alternate Screen Buffer */
+
+static void
+send_init_sequence(int h, int altscreen)
+{
+	write_sequence(h, INIT_TERMINAL_SEQ);
+
+	/* If alternate screen is supported switch to it. */
+	if (altscreen) {
+		write_sequence(h, INIT_ALT_SCREEN_SEQ);
+	}
+#ifdef CONFIG_MOUSE
+	send_mouse_init_sequence(h);
+#endif
+}
+
+#define DONE_CLS_SEQ		"\033[2J"	/* Erase in Display, Clear All */
+#define DONE_TERMINAL_SEQ	"\0338\r \b"	/* Restore Cursor (DECRC) + ??? */
+#define DONE_ALT_SCREEN_SEQ	"\033[?47l"	/* Use Normal Screen Buffer */
+
+static void
+send_done_sequence(int h, int altscreen)
+{
+	write_sequence(h, DONE_CLS_SEQ);
+
+#ifdef CONFIG_MOUSE
+	send_mouse_done_sequence(h);
+#endif
+
+	/* Switch from alternate screen. */
+	if (altscreen) {
+		write_sequence(h, DONE_ALT_SCREEN_SEQ);
+	}
+
+	write_sequence(h, DONE_TERMINAL_SEQ);
+}
+
 
 #undef write_sequence
 
@@ -340,7 +447,9 @@ handle_trm(int std_in, int std_out, int sock_in, int sock_out, int ctl_in,
 		if (ctl_in >= 0) setraw(ctl_in, &itrm->t);
 		send_init_sequence(std_out, itrm->altscreen);
 		handle_terminal_resize(ctl_in, resize_terminal);
+#ifdef CONFIG_MOUSE
 		enable_mouse();
+#endif
 	}
 
 	set_handlers(std_in, (select_handler_T) in_kbd,
@@ -436,7 +545,9 @@ free_trm(struct itrm *itrm)
 
 
 		unhandle_terminal_resize(itrm->in.ctl);
+#ifdef CONFIG_MOUSE
 		disable_mouse();
+#endif
 		send_done_sequence(itrm->out.std, itrm->altscreen);
 		tcsetattr(itrm->in.ctl, TCSANOW, &itrm->t);
 	}
@@ -700,107 +811,6 @@ get_esc_code(unsigned char *str, int len, unsigned char *code, int *num)
 #ifdef DEBUG_ITRM_QUEUE
 #include <ctype.h>	/* isprint() isspace() */
 #endif
-
-#ifdef CONFIG_MOUSE
-static int
-decode_mouse_position(struct itrm *itrm, int from)
-{
-	int position;
-
-	position = (unsigned char) (itrm->in.queue.data[from]) - ' ' - 1
-		 + ((int) ((unsigned char) (itrm->in.queue.data[from + 1]) - ' ' - 1) << 7);
-
-	return (position & (1 << 13)) ? 0 : position;
-}
-
-#define get_mouse_x_position(itrm, esclen) decode_mouse_position(itrm, (esclen) + 1)
-#define get_mouse_y_position(itrm, esclen) decode_mouse_position(itrm, (esclen) + 3)
-
-/* Returns length of the escape sequence or -1 if the caller needs to set up
- * the ESC delay timer. */
-static int
-decode_terminal_mouse_escape_sequence(struct itrm *itrm, struct term_event *ev,
-				      int el, int v)
-{
-	static int xterm_button = -1;
-	struct term_event_mouse mouse;
-
-	if (itrm->in.queue.len - el < 3)
-		return -1;
-
-	if (v == 5) {
-		if (xterm_button == -1)
-			xterm_button = 0;
-
-		if (itrm->in.queue.len - el < 5)
-			return -1;
-
-		mouse.x = get_mouse_x_position(itrm, el);
-		mouse.y = get_mouse_y_position(itrm, el);
-
-		switch ((itrm->in.queue.data[el] - ' ') ^ xterm_button) { /* Every event changes only one bit */
-		case TW_BUTT_LEFT:
-			mouse.button = B_LEFT | ((xterm_button & TW_BUTT_LEFT) ? B_UP : B_DOWN);
-			break;
-		case TW_BUTT_MIDDLE:
-			mouse.button = B_MIDDLE | ((xterm_button & TW_BUTT_MIDDLE) ? B_UP : B_DOWN);
-			break;
-		case TW_BUTT_RIGHT:
-			mouse.button = B_RIGHT | ((xterm_button & TW_BUTT_RIGHT) ? B_UP : B_DOWN);
-			break;
-		case 0:
-			mouse.button = B_DRAG;
-			break;
-		default:
-			mouse.button = 0; /* shut up warning */
-		/* default : Twin protocol error */
-		}
-
-		xterm_button = itrm->in.queue.data[el] - ' ';
-		el += 5;
-
-	} else {
-		/* See terminal/mouse.h about details of the mouse reporting
-		 * protocol and {struct term_event_mouse->button} bitmask
-		 * structure. */
-		mouse.x = itrm->in.queue.data[el+1] - ' ' - 1;
-		mouse.y = itrm->in.queue.data[el+2] - ' ' - 1;
-
-		/* There are rumours arising from remnants of code dating to
-		 * the ancient Mikulas' times that bit 4 indicated B_DRAG.
-		 * However, I didn't find on what terminal it should be ever
-		 * supposed to work and it conflicts with wheels. So I removed
-		 * the last remnants of the code as well. --pasky */
-
-		mouse.button = (itrm->in.queue.data[el] & 7) | B_DOWN;
-		/* smartglasses1 - rxvt wheel: */
-		if (mouse.button == 3 && xterm_button != -1) {
-			mouse.button = xterm_button | B_UP;
-		}
-		/* xterm wheel: */
-		if ((itrm->in.queue.data[el] & 96) == 96) {
-			mouse.button = (itrm->in.queue.data[el] & 1) ? B_WHEEL_DOWN : B_WHEEL_UP;
-		}
-
-		xterm_button = -1;
-		/* XXX: Eterm/aterm uses rxvt-like reporting, but sends the
-		 * release sequence for wheel. rxvt itself sends only press
-		 * sequence. Since we can't reliably guess what we're talking
-		 * with from $TERM, we will rather support Eterm/aterm, as in
-		 * rxvt, at least each second wheel up move will work. */
-		if (mouse_action_is(&mouse, B_DOWN))
-			xterm_button = mouse_get_button(&mouse);
-
-		el += 3;
-	}
-
-	/* Postpone changing of the event type until all sanity
-	 * checks have been done. */
-	set_mouse_term_event(ev, mouse.x, mouse.y, mouse.button);
-
-	return el;
-}
-#endif /* CONFIG_MOUSE */
 
 /* Returns length of the escape sequence or -1 if the caller needs to set up
  * the ESC delay timer. */
